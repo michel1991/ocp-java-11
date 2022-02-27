230992b4b1a180bbb3ce688772af463a8ec815cd
michel1991 <michelmvoumamvouma@gmail.com>
2022-02-27 22:20:15 +0100

    code: adding sample code for running a program in One line with packages without javac command we taping java command

---
+++ singleFile/Learning.java
@@ -0,0 +1,8 @@
+package singleFile;
+import java.util.*;
+public class Learning{
+   private ArrayList list;
+   public static void main(String[] args){
+    System.out.println("This works!");
+   }
+}
