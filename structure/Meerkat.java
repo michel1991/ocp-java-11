fb436204cf24338a34355099b97469f04236c1b9
michel1991 <michelmvoumamvouma@gmail.com>
2022-02-27 22:43:45 +0100

    code: sample code ordering elements in class

---
+++ structure/Meerkat.java
@@ -0,0 +1,10 @@
+package structure; // package must be first non-comment
+import java.util.*; // import must come after package
+public class Meerkat{ // then comes the class
+  double weight; // fields and methods can go either order
+ public double getWeight(){
+     return weight;
+  }
+
+double height; // another field - they don't nedd to be together
+}
