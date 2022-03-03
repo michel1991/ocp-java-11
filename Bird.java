6682e08a4b44ae97524081759086c14ece373a51
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-03 05:20:24 +0100

    code: executing instance initializer blocks, sample of code

---
+++ Bird.java
@@ -0,0 +1,12 @@
+public class Bird{
+   // first bloc
+   public static void main(String[] args){
+    // bloc 2
+     {
+       System.out.println("Feathers");
+     }
+  }
+   {
+     System.out.println("Snowy");
+   }
+}
