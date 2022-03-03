271925aa34b4e23834a8bbbcdbae9adbee5f0cc6
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-03 05:42:17 +0100

    style: adding the name of bloc

--- Bird.java
+++ Bird.java
@@ -1,12 +1,14 @@
 public class Bird{
-   // first bloc
+   // bloc 1 class definition
    public static void main(String[] args){
-    // bloc 2
+    // bloc 2 method declaration
      {
+      //  bloc 3 inner bloc
        System.out.println("Feathers");
      }
   }
    {
+     // bloc 4 instance initializer
      System.out.println("Snowy");
    }
 }
