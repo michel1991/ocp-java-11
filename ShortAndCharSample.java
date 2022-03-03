e1cfee25f473d35d6725d4a42566825e372e770e
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-03 14:01:41 +0100

    code: real world sample of code of short and char

---
+++ ShortAndCharSample.java
@@ -0,0 +1,8 @@
+public class ShortAndCharSample{
+  public static void main(String[] args){
+     short bird = 'd';
+     char mammal= (short)83;
+    System.out.println(bird);
+    System.out.println(mammal);
+  }
+}
