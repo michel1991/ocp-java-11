f01d519d6f68ddbf7e28e20150b5de16fe4157b8
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-04 06:45:30 +0100

    code: introducing var, sample of code

---
+++ SampleVarVariable.java
@@ -0,0 +1,11 @@
+public class SampleVarVariable{
+   public void whatTypeAmI(){
+    var name = "Hello";
+    var size = 7;
+    System.out.println(name+ " " + size);
+   }
+ public static void main(String[] args){
+    SampleVarVariable sample = new SampleVarVariable();
+    sample.whatTypeAmI();
+ }
+}
