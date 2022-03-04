a664fa98204a67fe08c3839b65e0c41f6865019b
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-04 07:38:46 +0100

    code: type inference, sample breaking declaration

---
+++ BreakingDeclaration.java
@@ -0,0 +1,11 @@
+public class BreakingDeclaration{
+  public void breakingDeclaration(){
+   var silly
+    = 1;
+   System.out.println("breaking declaration "+ silly);
+  }
+ public static void main(String[] args){
+  BreakingDeclaration sample = new BreakingDeclaration();
+   sample.breakingDeclaration();
+  }
+}
