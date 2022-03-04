1a6b2849f4cdb3cc2b5363f5b895aa13c529a5ed
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-04 05:53:49 +0100

    error-code: creating local variable, sample local variable in method with complex initialization using condition, adding initialization of variable that produce error and define invalid value when using the method

--- LocalVariableComplexInitializationWithError.java
+++ LocalVariableComplexInitializationWithError.java
@@ -4,14 +4,15 @@ public class LocalVariableComplexInitializationWithError{
        int otherAnswer;
        int onlyOneBranch;
        if(check){
+         onlyOneBranch = 1;
          answer = 1;
        } else{
         answer = 2;
        }
-      System.out.println(onlyOneBranch);
+      System.out.println(onlyOneBranch); // does not compile
    }
   public static void main(String[] args){
    LocalVariableComplexInitializationWithError my = new LocalVariableComplexInitializationWithError();
-   my.findAnswer(true);
+   my.findAnswer(false);
   }
 }
