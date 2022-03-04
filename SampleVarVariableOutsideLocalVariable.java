b3a50c03c523c1d223456a43e7c276fd4ee0571b
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-04 06:51:26 +0100

    code-error: introducing var, sample of code using the keyword var to declare type of field

---
+++ SampleVarVariableOutsideLocalVariable.java
@@ -0,0 +1,7 @@
+public class SampleVarVariableOutsideLocalVariable{
+  var tricky = "Hello";
+  public static void main(String[] args){
+   SampleVarVariableOutsideLocalVariable sample = new SampleVarVariableOutsideLocalVariable();
+   System.out.println(sample.tricky);
+  }
+}
