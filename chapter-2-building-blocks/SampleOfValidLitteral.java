b5a2e8bbfa5d302179df4abd502052bc0a052bef
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-05 16:24:17 +0100

    code: semple of numeric litteral with underscore must no to use at the begining and end of number, and the begining of the dot in decimal number, after the dot of decimal number

---
+++ chapter-2-building-blocks/SampleOfValidLitteral.java
@@ -0,0 +1,12 @@
+public class SampleOfValidLitteral{
+  public static void main(String[] args){
+    int a = 17_00_00;
+    int b = 3400_78;
+    int c = 65_98_98;
+    double d = 56.98_76;
+    System.out.println("a = " +  a);
+   System.out.println("b = " +  b);
+  System.out.println("c = " +  c); 
+  System.out.println("d = " +  d);
+  }
+}
