181ee5f93b9f740cde27bed797ea8d54dbcc2792
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-05 15:42:55 +0100

    code: java sample of type which in this case is primitive

---
+++ chapter-2-building-blocks/SampleJavaTypePrimitive.java
@@ -0,0 +1,13 @@
+public class SampleJavaTypePrimitive{
+  public static void main(String[] args){
+    int a = 1;
+    float b = 34.89F;
+    double c = 879.98;
+    char d = 'c';
+    System.out.println("sample of primitive");
+    System.out.println("int value: " +a);
+    System.out.println("float value: " + b);
+    System.out.println("double value: " +c);
+    System.out.println("char value: " +d);
+   }
+}
