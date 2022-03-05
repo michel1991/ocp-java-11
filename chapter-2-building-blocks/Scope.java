373ee9c787a03a48a8e8ee500fe35e1107d13ceb
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-05 06:07:04 +0100

    code: sample to think, how gabarge collection work, use the paper to draw reference and object

---
+++ chapter-2-building-blocks/Scope.java
@@ -0,0 +1,11 @@
+public class Scope{
+  public static void main(String[] args){
+    String one, two;
+    one = new String("a");
+    two = new String("b");
+    one = two;
+   String three = one;
+   one = null;
+   System.out.println("end scope bye");
+  }
+}
