5534ca784e70f9612eff6b1fe11ffb1272f4e3c5
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-05 18:35:30 +0100

    code: sample of code using reverse operator represent by sign(-) or (+), this operator is only using with one operand or variable

---
+++ chapter-3-operator/SampleUsingReverseOperator.java
@@ -0,0 +1,10 @@
+public class SampleUsingReverseOperator{
+  public static void main(String[] args){
+       double zooTemperature = 1.21;
+      System.out.println(zooTemperature);
+      zooTemperature = -zooTemperature;
+     System.out.println(zooTemperature);
+     zooTemperature = -(-zooTemperature);
+    System.out.println(zooTemperature);
+  }
+}
