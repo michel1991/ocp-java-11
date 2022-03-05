d2377871ab30e4b05878df840992b9f7649fbb1d
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-05 16:41:32 +0100

    code: assign value to java type called reference using another refernce they point to the same object

--- chapter-2-building-blocks/SampleJavaTypeCalledReference.java
+++ chapter-2-building-blocks/SampleJavaTypeCalledReference.java
@@ -1,8 +1,10 @@
 import java.util.Date;
 public class SampleJavaTypeCalledReference{
   public static void main(String[] args){
-    String greeting;
+    String greeting = "toto";
+    String anotherGreeting = greeting;
     Date today = new Date();
     System.out.println("today is " + today);
+    System.out.println("assign value to reference using another reference first=" + greeting + " second="+ anotherGreeting);
   }
 }
