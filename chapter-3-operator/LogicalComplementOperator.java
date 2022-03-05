2ef468e77eb7d7c2d7923d2d70ca13710e96e279
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-05 18:27:26 +0100

    code: sample of code about negation operator of boolean also called Logical Complement operator

---
+++ chapter-3-operator/LogicalComplementOperator.java
@@ -0,0 +1,8 @@
+public class LogicalComplementOperator{
+  public static void main(String[] args){
+    boolean isAnimalAsleep = false;
+    System.out.println(isAnimalAsleep);
+    isAnimalAsleep = !isAnimalAsleep;
+    System.out.println(isAnimalAsleep);
+  }
+}
