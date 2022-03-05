0e4934cfcd1947d42671e332a325e5b3941058ec
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-05 14:54:18 +0100

    code-error: sample balanced parentheses problem, because the number of brackets open is not the same of number of bracket close

---
+++ chapter-2-building-blocks/BalancedParenthesesProblem.java
@@ -0,0 +1,9 @@
+public class BalancedParenthesesProblem{
+  public void hello(){
+    System.out.println("");
+    // balanced parentheses problem because we does not close the first bracket
+    
+   public static void main(String[] args){
+      System.out.println("I am in main ");
+   }
+}
