98ef2af507f6290f5ee5449833aed4f52db8a550
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-06 04:58:35 +0100

    code: sample of code Numeric promotion, first rule If two values have different data types, Java will automatically promote one of the values to the larger of the two data types.

---
+++ chapter-3-operator/NumericPromotionConvertionToLarger.java
@@ -0,0 +1,8 @@
+public class NumericPromotionConvertionToLarger{
+  public static void main(String[] args){
+    int x = 1;
+   long y = 33;
+   var z = x + y;
+   System.out.println("long result addition from int plus long " + z); 
+  }
+}
