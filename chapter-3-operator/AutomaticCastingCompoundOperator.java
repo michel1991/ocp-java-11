4b2868f58cd0bab91d42b5e406447cb66ab46edd
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-06 21:38:27 +0100

    code: sample of code automatic casting for compound operator

---
+++ chapter-3-operator/AutomaticCastingCompoundOperator.java
@@ -0,0 +1,8 @@
+public class AutomaticCastingCompoundOperator{
+  public static void main(String[] args){
+    long goat = 10;
+    int sheep = 5;
+    sheep *= goat;
+   System.out.println("result : "+ sheep);
+  }
+}
