35ba552e4f5a9f446ff119f546c003dc6c80f969
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-06 02:27:30 +0100

    code: sample code of increment and decrement operator useful for the exam

---
+++ chapter-3-operator/IncrementAndDecrementOperator.java
@@ -0,0 +1,10 @@
+public class IncrementAndDecrementOperator{
+  public static void main(String[] args){
+     int parkAttendance = 0;
+     System.out.println("initial value parkAttendance "+ parkAttendance);
+     System.out.println("pre increment value "+ " "+  ++parkAttendance);
+     System.out.println("Park attendance nothing operator "+ parkAttendance);
+    System.out.println("post decrement "+ " " + parkAttendance--);
+     System.out.println("Park Attendance nothing operator "+ " " + parkAttendance);
+  }
+}
