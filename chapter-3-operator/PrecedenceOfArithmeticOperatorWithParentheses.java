453449f2af5959fdb2b9746872854e236da307f3
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-06 03:42:09 +0100

    code: changing the order of operation on the operand or variable by using parentheses

---
+++ chapter-3-operator/PrecedenceOfArithmeticOperatorWithParentheses.java
@@ -0,0 +1,6 @@
+public class PrecedenceOfArithmeticOperatorWithParentheses{
+  public static void main(String[] args){
+    int price = 2 * ((5 + 3) * 4 -8);
+    System.out.println("result of 2 * ((5 + 3) * 4 -8) = " + price);
+  }
+}
