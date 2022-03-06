d83aecd872f0b2e835f483554ca16aa8022cf6ac
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-06 03:21:24 +0100

    code: sample of code using operator precedence with arithmetic operator, arithmetic operator should use two operand or variable

---
+++ chapter-3-operator/PrecedenceOfArithmeticOperator.java
@@ -0,0 +1,6 @@
+public class PrecedenceOfArithmeticOperator{
+  public static void main(String[] args){
+    int price = 2 * 5 + 3 * 4 -8;
+    System.out.println("result of 2 * 5 + 3 * 4 -8 = " + price);
+  }
+}
