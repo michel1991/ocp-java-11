c13da6bc5a6654b232a32d5d35fecdbb3b8491a8
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-06 05:43:12 +0100

    code: numeric promotion using all rules

---
+++ chapter-3-operator/NumericPromotionAllRule.java
@@ -0,0 +1,9 @@
+public class NumericPromotionAllRule{
+  public static void main(String[] args){
+   short w = 14;
+   float x = 13;  
+   double y = 30;
+   var z = w * x / y;
+   System.out.println("numeric promotion all rule  " + z); 
+  }
+}
