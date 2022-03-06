f4efa7acb64af493cf011145afcd61e9c141c191
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-06 05:11:54 +0100

    code-error: numeric promotion trick, java suggest every float to be double we msut to specify that it is float with F or f, that example produce error incompatible types: possible lossy conversion from double to float

---
+++ chapter-3-operator/NumericPromotionConvertionFloatSupposingDouble.java
@@ -0,0 +1,8 @@
+public class NumericPromotionConvertionFloatSupposingDouble{
+  public static void main(String[] args){
+    double x = 39.21;
+   float y = 2.1;
+   var z = x + y;
+   System.out.println("java suggest float to be double  result addition from "); 
+  }
+}
