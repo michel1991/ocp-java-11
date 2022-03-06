9abe10e0ef4a6316a55f24318a67b24380849b84
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-06 22:08:40 +0100

    code: sample of code  assignment operator with return value

---
+++ chapter-3-operator/AssignmentOperatorReturnValue.java
@@ -0,0 +1,8 @@
+public class AssignmentOperatorReturnValue{
+  public static void main(String[] args){
+    long wolf = 5;
+    long coyote =  (wolf = 3);
+   System.out.println(wolf);
+   System.out.println(coyote);
+  }
+}
