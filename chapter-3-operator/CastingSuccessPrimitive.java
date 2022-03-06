d329380e03ec2a77362fa026e73e8408de912076
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-06 18:47:01 +0100

    code: sample of castin with successg, casting is done automatically useful for cast big type to smaller otherwise it is not usefull, apy attention

---
+++ chapter-3-operator/CastingSuccessPrimitive.java
@@ -0,0 +1,9 @@
+public class  CastingSuccessPrimitive{
+ public static void main(String[] args){
+     int fur  = (int) 5;
+     int hair = (short) 2;
+     String type = (String) "Bird";
+      short tail = (short) (4 + 10);
+     System.out.println("All done we success see the code");
+  }
+}
