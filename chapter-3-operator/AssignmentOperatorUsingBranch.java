00bfd84706a9a59ffacc1c6757a3cda9e2cd6763
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-06 22:14:06 +0100

    code: sample of code  assignment operator with branch (if) assignment operator is consider like expression be carefull

---
+++ chapter-3-operator/AssignmentOperatorUsingBranch.java
@@ -0,0 +1,7 @@
+public class AssignmentOperatorUsingBranch{
+  public static void main(String[] args){
+      boolean healthy = false;
+      if(healthy = true)
+        System.out.println("Good " + healthy);
+  }
+}
