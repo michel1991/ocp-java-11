7e138ae0aadbda55ae4e8dcfeaf7aa9402f29290
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-11 07:50:36 +0100

    code: using null with instanceof operator with Object class always return false

---
+++ chapter-3-operator/NullAndInstanceOf.java
@@ -0,0 +1,6 @@
+public class NullAndInstanceOf{
+  public static void main(String[] args){
+     boolean result = null instanceof Object;
+    System.out.println("result check if null it is instance of object always return false "+ result);
+  }
+}
