f7a6c041f1bc562927e28d3b4f8db42f26517485
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-17 01:36:59 +0100

    code: concatenation, sample example using three rule of concatenation

---
+++ chapter-5-core-java-apis/SampleExampleWithConcatenationRule.java
@@ -0,0 +1,9 @@
+public class SampleExampleWithConcatenationRule{
+  public static void main(String[] args){
+    System.out.println(1+2);
+    System.out.println("a" + "b");
+    System.out.println("a" + "b" + 3);
+    System.out.println(1 + 2 + "c");
+    System.out.println("c" + 1 + 2);
+  }
+}
