ba91d46b152d274034e11bced3d3bb004edcd50b
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-18 02:34:17 +0100

    code: sample using append method of stringbuilder with multiple type of signature we don't care about the conversion of the type it is already taked by the method

---
+++ chapter-5-core-java-apis/StringBuilderppendMethodWithMultipleSignature.java
@@ -0,0 +1,8 @@
+public class StringBuilderppendMethodWithMultipleSignature{
+  public static void main(String[] args){
+    StringBuilder sb = new StringBuilder().append(1).append('c');
+     sb.append("-").append(true);
+    System.out.println(sb);
+  }
+ 
+}
