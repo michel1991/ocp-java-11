4f3ecdfbcb6185fc976fd469a3566925f81bcf0c
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-18 21:22:58 +0100

    code: creating an array with reference variable, this example also show equality reference with array because the two arrays point to the same object, we also use equals method who does not verify the content of array but the reference

---
+++ chapter-5-core-java-apis/ArrayType.java
@@ -0,0 +1,9 @@
+public class ArrayType{
+   public static void main(String[] args){
+     String [] bugs = { "cricket", "beetle", "ladybug"};
+     String [] alias = bugs;
+    
+     System.out.println(bugs.equals(alias));
+     System.out.println(bugs.toString());
+  }
+}
