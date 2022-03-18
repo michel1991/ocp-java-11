d9d49cffc4db12f415bd746c93319d332e61e3bc
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-18 02:20:22 +0100

    code: sample of code using method charAt, indexOf, length, substring of stringbuilder, notice that substring does not change the stringbuilder because it is mutable

---
+++ chapter-5-core-java-apis/StringBuilderUsingCharAtIndexOfLengthSubstring.java
@@ -0,0 +1,9 @@
+public class StringBuilderUsingCharAtIndexOfLengthSubstring{
+  public static void main(String[] args){
+     StringBuilder sb = new StringBuilder("animals");
+     String sub = sb.substring(sb.indexOf("a"), sb.indexOf("al"));
+     int len = sb.length();
+     char ch = sb.charAt(6);
+     System.out.println(sub + " " + len + " " + ch);
+   }
+}
