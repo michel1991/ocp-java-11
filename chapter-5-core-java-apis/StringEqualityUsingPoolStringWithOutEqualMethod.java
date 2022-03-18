cc7c95028a99458ddac042a6a7e2d7f447d945ef
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-18 04:35:38 +0100

    code: sample of code that check equality value between two strings without equal method but using java string pool

---
+++ chapter-5-core-java-apis/StringEqualityUsingPoolStringWithOutEqualMethod.java
@@ -0,0 +1,7 @@
+public class StringEqualityUsingPoolStringWithOutEqualMethod{
+  public static void main(String[] args){
+   String x = "Hello World";
+   String y = "Hello World";
+   System.out.println(x == y);
+  }
+}
