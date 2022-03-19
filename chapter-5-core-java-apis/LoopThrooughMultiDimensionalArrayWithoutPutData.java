0cf5bf126049c1e9e795d6f14bb766a2a2914e17
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-19 07:56:47 +0100

    code: sample loop through int multidimensional array without put data into array, in this case java use the default value of primitive for each value into array

---
+++ chapter-5-core-java-apis/LoopThrooughMultiDimensionalArrayWithoutPutData.java
@@ -0,0 +1,12 @@
+public class LoopThrooughMultiDimensionalArrayWithoutPutData{
+   public static void main(String[] args){
+    int[][] twoD = new int[3][2];
+    for(int i = 0; i < twoD.length; i++){
+          for(int j = 0; j < twoD[i].length; j++)
+             System.out.print(twoD[i][j] + " ");
+          System.out.println();
+         
+     }
+  }
+
+}
