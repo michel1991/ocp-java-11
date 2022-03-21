77373f15779c5d5377857118ccc08d73437bfaac
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-21 06:11:00 +0100

    code-error: sample of code that demonstrate we cannot access to private member property or method of other class in the same package

---
+++ chapter-7-methods-encapsulation/pond/duck/BadDuckling.java
@@ -0,0 +1,8 @@
+package pond.duck;
+public class BadDuckling{
+   public void makeNoise(){
+    FatherDuck duck = new FatherDuck();
+    duck.quack();  // does not compile
+    System.out.println(duck.noise); // does not compile
+  }
+}
