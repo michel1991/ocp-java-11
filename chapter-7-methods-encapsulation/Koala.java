df90b5cc68601b3f7d6adc82f307095d3afe8136
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-22 06:42:45 +0100

    code: Designing static methods and fields(title). Code sample of how to use static keyword on members

---
+++ chapter-7-methods-encapsulation/Koala.java
@@ -0,0 +1,8 @@
+public class Koala{
+   public static int count = 0; // static variable
+  
+   public static void main(String[] args){
+      System.out.println(count);
+   }
+
+}
---
+++ chapter-7-methods-encapsulation/KoalaTester.java
@@ -0,0 +1,8 @@
+public class KoalaTester{
+   public static int count = 0; // static variable
+  
+   public static void main(String[] args){
+      Koala.main(new String[0]); // call static method
+   }
+
+}
