1058f4324e62c555bf9d9f4800a58688276bd52d
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-24 13:18:27 +0100

    code: pass-by-value vs pass by reference(title). Example that show that we can store returning value into variable.

---
+++ chapter-7-methods-encapsulation/ReturningValues.java
@@ -0,0 +1,19 @@
+public class ReturningValues{
+  public static int number(int number){
+     number++;
+     return number;
+  }
+
+  public static String letters(String letters){
+     letters += "d";
+     return letters;
+  }
+
+  public static void main(String[] args){
+    int number = 1; 
+    String letters = "abc";
+    number (number);
+    letters = letters(letters);
+     System.out.println(number + letters); // 1abcd
+  }
+}
