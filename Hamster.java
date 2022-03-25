8328e7e00e9a97b5aa5de492f72e5e934bd9aa22
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-25 15:18:05 +0100

    code: Calling overloaded constructors with this(). This example show how we can call another constructor with this(keyword) follow by list of parameters. The this() call must be the first statement in the constructor. The side effect of this is that there can be only one call to this() in any constructor

---
+++ Hamster.java
@@ -0,0 +1,13 @@
+public class Hamster{
+  private String color;
+  private int weight;
+  public Hamster(int weight){
+    this(weight, "brown");
+  }
+
+   public Hamster(int weight, String color){
+      this.weight = weight;
+      this.color = color;
+   }
+
+}
