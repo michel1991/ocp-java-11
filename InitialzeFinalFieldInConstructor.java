36d97e70a66072bf213181e791f3cd1bb5db8dae
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-25 18:01:29 +0100

    code: Constructor and final Fields (Title). Like other final variables, once the value is assigned, it cannot be changed.  This example show how we assign value into final instance variable using constructor. Instance final variable follow rule of static initializer (constant static final field).

---
+++ InitialzeFinalFieldInConstructor.java
@@ -0,0 +1,10 @@
+public class InitialzeFinalFieldInConstructor{
+  private final int volume;
+  private final String type;
+ 
+  public InitialzeFinalFieldInConstructor(){
+      this.volume = 10;
+      type = "happy";
+   }
+
+}
