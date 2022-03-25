cdb2fb5a6fef56259cdea92b156d2ee2d0c113fd
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-25 10:49:43 +0100

    code: Creating classes => Extending class (type). Example of how to extends class

---
+++ Lion.java
@@ -0,0 +1,17 @@
+public class Lion extends Animal{
+    public void setProperties(int age, String n){
+      setAge(age);
+      name = n;
+   }
+
+   public void roar(){
+      System.out.print(name + ", age " +  getAge() + " , says: Roar!");
+   }
+
+   public static void main(String[] args){
+      var lion = new Lion();
+     lion.setProperties(3, "kion");
+     lion.roar();
+   }
+
+}
