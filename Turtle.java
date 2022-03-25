dab94ce5de0fa88cecf2791362b26f93baf1fa21
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-25 13:56:00 +0100

    code: Declaring Constructors ==> creating constructor(Title). This example show how to overloading constructor. The overloading of constructor follow the same rule of method overloading

---
+++ Turtle.java
@@ -0,0 +1,19 @@
+public class Turtle{
+   private String name;
+
+    public Turtle(){
+      name = "John Doe";
+    }
+
+    public Turtle(int age){
+
+    }
+
+   public Turtle(long age){
+
+   }
+
+   public Turtle(String newName, String... favoriteFoods){
+       name = newName;
+   }
+}
