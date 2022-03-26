919401a91a551f6d470e93340e3bea9efa307bea
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-26 05:44:55 +0100

    code: Order of initialization ==> instance initialization(Title). Example show the instance initialization with inheritance but focus on entry program (main). The example is more complex beacuse we have multiple overloaded constructors. Pay attention that Parent class parent is initialize once (not twise)

---
+++ instanceInitialization/Okapi.java
@@ -0,0 +1,24 @@
+package instanceInitialization;
+
+public class Okapi extends GiraffeFamily{
+   static {
+      System.out.print("F");
+   }
+
+  public Okapi(int stripes){
+     super("sugar");
+     System.out.print("G");
+  }
+
+   {
+     System.out.print("H");
+   }
+
+   public static void main(String[] grass){
+     new Okapi(1);
+     System.out.println();
+     new Okapi(2);
+     
+   }
+
+}
