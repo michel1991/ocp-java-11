fb75a7f09518d536c7727b0d8f63c1640ba44a31
michel1991 <michelmvoumamvouma@gmail.com>
2022-03-27 19:23:19 +0200

    code-error: Overriding a Method (title). throws exception that is more large(broader)  of the parent. Notice that this exception is checked exceptions

---
+++ overridingMethod/GalapagosTortoise.java
@@ -0,0 +1,16 @@
+package overridingMethod;
+import java.io.*;
+
+public class GalapagosTortoise extends Reptile{
+
+ public void sleepInShell() throws FileNotFoundException{}
+ 
+ public void hideInShell() throws IllegalArgumentException{
+
+ }
+
+ public void exitShell() throws IOException{
+
+  }
+ 
+}
---
+++ overridingMethod/Reptile.java
@@ -0,0 +1,16 @@
+package overridingMethod;
+import java.io.*;
+
+public class Reptile{
+
+ protected void sleepInShell() throws IOException{}
+ 
+ protected void hideInShell() throws NumberFormatException{
+
+ }
+
+ protected void exitShell() throws FileNotFoundException{
+
+  }
+ 
+}
