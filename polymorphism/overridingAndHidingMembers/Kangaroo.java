2535b099677ce0acb01c5905ea2b4cbe4881fa61
michel1991 <michelmvoumamvouma@gmail.com>
2022-04-01 08:09:21 +0200

    code: Polymorphism ==> Overriding vs Hiding Members (title). Besides the location, the reference type can also determine the value you get when you are working with hidden members. In this example java uses the reference type for determine members properties to call. Since static methods can only be hidden, not overridden, Java uses the reference type to determine which version of members properties  should be called.

---
+++ polymorphism/overridingAndHidingMembers/Kangaroo.java
@@ -0,0 +1,20 @@
+package polymorphism.overridingAndHidingMembers;
+
+public class Kangaroo extends Marsupial{
+   protected int age = 6;
+
+  public static boolean isBiped(){
+     return true;
+  }
+
+   public static void main(String[] args){
+     Kangaroo joey = new Kangaroo();
+     Marsupial moey = joey;
+     System.out.println(joey.isBiped()); //true
+     System.out.println(moey.isBiped()); // false
+     System.out.println(joey.age); // 6
+    System.out.println(moey.age); //2
+   
+   }
+
+}
---
+++ polymorphism/overridingAndHidingMembers/Marsupial.java
@@ -0,0 +1,10 @@
+package polymorphism.overridingAndHidingMembers;
+
+class Marsupial{
+   protected int age = 2;
+
+  public static boolean isBiped(){
+      return false;
+  }
+
+}
