71012cfd878cd942f0a0d45f8735db3b6f9c7a87
michel1991 <michelmvoumamvouma@gmail.com>
2022-04-06 05:19:48 +0200

    code-error: Runtime Exception Classes ==> NullPointerException  (Title). Example of unchecked exception, who is NullPointerException  belong to runtime Exception

---
+++ uncheckedException/runtime/SampleNullPointerException.java
@@ -0,0 +1,9 @@
+package uncheckedException.runtime;
+
+ class SampleNullPointerException{
+   public static void main(String[] args){
+      String name;
+      System.out.println(name.length());
+   }
+
+}
