4c08d671e8b71e0bf34a3e7e967c669b4448c11e
michel1991 <michelmvoumamvouma@gmail.com>
2022-04-07 00:09:59 +0200

    code-error: Chaining catch Block (Title). Bad declaration of catch block between IllegalArgumentException and NumberFormatException because NumberFormatException is a child of IllegalArgumentException. The catch block of IllegalArgumentException must come at the last

---
+++ chainingCatchBlocks/ErrorIllegalArgumentAndNumberFormatException.java
@@ -0,0 +1,19 @@
+package chainingCatchBlocks;
+
+class ErrorIllegalArgumentAndNumberFormatException{
+  
+   public void visitSnakes(){
+       try{
+         
+       }catch(IllegalArgumentException e){
+            
+        } catch(NumberFormatException e ){
+           
+        }
+   }
+
+   public static void main(String[] args){
+       var  sample = new ErrorIllegalArgumentAndNumberFormatException();
+      sample.visitSnakes();
+   }
+}
