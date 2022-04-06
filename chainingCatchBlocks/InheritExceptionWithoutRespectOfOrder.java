a4db165f5d94808f6a7f83db6df48a1d540147c2
michel1991 <michelmvoumamvouma@gmail.com>
2022-04-06 23:12:47 +0200

    code-error: Chaining catch Block (Title). This example show that when we chaining catch block with inheritence we must to respect the order of declaration of catch. The superclass must come in the last position. This time, if the more specific ExhibitClosedForLunch exception is thrown, the catch block for ExhibitClosed runs—which means there is no way for the second catch block to ever run. Java correctly tells you there is an unreachable catch block.

---
+++ chainingCatchBlocks/InheritExceptionWithoutRespectOfOrder.java
@@ -0,0 +1,23 @@
+package chainingCatchBlocks;
+
+class InheritExceptionWithoutRespectOfOrder{
+
+    void seeAnimal(){
+      throw new ExhibitClosedForLunch();
+    }
+  
+   public void visitMonkeys(){
+       try{
+          seeAnimal();
+       }catch(ExhibitClosed e){
+            System.out.print("try back later ");
+        } catch(ExhibitClosedForLunch e ){
+           System.out.print("not today");
+        }
+   }
+
+   public static void main(String[] args){
+       BasicExample sample = new BasicExample();
+      sample.visitPorcupine();
+   }
+}
