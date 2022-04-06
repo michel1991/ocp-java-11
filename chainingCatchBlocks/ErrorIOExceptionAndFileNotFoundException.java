595a348d951431103ae78015b3d3d1cd9978c931
michel1991 <michelmvoumamvouma@gmail.com>
2022-04-07 00:16:43 +0200

    code-error: Chaining catch Block (Title). Bad declaration of catch block between IOException and FileNotFoundException because FileNotFoundException is a child of IOException. The catch block of IOException must come at the last

---
+++ chainingCatchBlocks/ErrorIOExceptionAndFileNotFoundException.java
@@ -0,0 +1,18 @@
+package chainingCatchBlocks;
+import java.io.*;
+class ErrorIOExceptionAndFileNotFoundException{
+  
+   public void visitSnakes(){
+       try{
+         
+       }catch(IOException e){
+            
+        } catch(FileNotFoundException e ){
+           
+        }
+   }
+
+   public static void main(String[] args){
+       
+   }
+}
