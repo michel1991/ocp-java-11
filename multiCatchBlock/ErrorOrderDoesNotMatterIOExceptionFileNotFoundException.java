305002ac328aebc8f6f34e90720682e5992f0bf1
michel1991 <michelmvoumamvouma@gmail.com>
2022-04-07 01:27:55 +0200

    code-error: Applying a multi-catch Block (Title). Adding throw exception.  Applying multi catch block with related class, in this example order does not matter, the code does not compile. The one difference between multi-catch blocks and chaining catch blocks is that order does not matter for a multi-catch block within a single catch expression.

--- multiCatchBlock/ErrorOrderDoesNotMatterIOExceptionFileNotFoundException.java
+++ multiCatchBlock/ErrorOrderDoesNotMatterIOExceptionFileNotFoundException.java
@@ -4,7 +4,7 @@ import java.io.*;
 class ErrorOrderDoesNotMatterIOExceptionFileNotFoundException{
   public static void main(String[] args){
       try{
-          
+          throw new IOException();          
       } catch(FileNotFoundException | IOException  e){
          
       }
