79191dad7f92479483a690dc7977f89a7f4b22bc
michel1991 <michelmvoumamvouma@gmail.com>
2022-04-08 04:08:22 +0200

    code: Throwing Additional Exceptions(Title). Complex example with nested try. You must well understand this example.  The correct answer is before_catch_finally_done. First on line 35, before_ is added. Line 36 throws a NullPointerException. Line 37 is skipped as Java goes straight to the catch block. Line 38 does catch the exception, and catch_ is added on line 39. Then line 40 throws a RuntimeException. The finally block runs after the catch regardless of whether an exception is thrown; it adds finally_ to result. At this point, we have completed the inner try statement that ran on lines 34–44. The outer catch block then sees an exception was thrown and catches it on line 45; it adds done to result. (Please refert to book line)

---
+++ throwingAdditionalExceptions/ComplexExampleNestedTry.java
@@ -0,0 +1,34 @@
+package throwingAdditionalExceptions;
+
+class ComplexExampleNestedTry{
+   public String exceptions(){
+        StringBuilder result = new StringBuilder();
+       String v = null;
+      try{
+          try{
+             result.append("before_");
+             v.length();
+             result.append("after_");
+
+          }catch(NullPointerException e){
+              result.append("catch_");
+              throw new RuntimeException();
+         } finally{
+             result.append("finally_");
+             throw new Exception();
+         }
+      }catch(Exception e){
+          result.append("done");
+       }
+
+      return result.toString();
+   }
+
+   public static void main(String... args){
+
+     var sample = new ComplexExampleNestedTry();
+     var result = sample.exceptions();
+     System.out.println(result);
+    }
+
+}
