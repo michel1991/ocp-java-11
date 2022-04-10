14d8291d1682cdb45369769c4192ee671a4d9ba4
michel1991 <michelmvoumamvouma@gmail.com>
2022-04-10 19:20:09 +0200

    code: Creating Simple Enums(Title). Sample of using values method on enum

--- creatingSimpleEnums/ExampleUsingEnum.java
+++ creatingSimpleEnums/ExampleUsingEnum.java
@@ -5,5 +5,16 @@ class ExampleUsingEnum{
     Season s  = Season.SUMMER;
     System.out.println(Season.SUMMER); // SUMMER
     System.out.println(s == Season.SUMMER); // true
+    System.out.println();
+
+    exampleValues();
   }
+
+
+  public static void exampleValues(){
+     for(Season season : Season.values()){
+        System.out.println(season.name() + " " + season.ordinal());
+     }
+  }
+
 }
