f8b34663479e6cac8b3ef56d4595c3684bf7533b
michel1991 <michelmvoumamvouma@gmail.com>
2022-04-11 06:20:19 +0200

    code: Adding Constructors, Field and Methods (Title). Adding members field, constructor, method to enum. Pay attention that we use final modifier before member instance, because enum values are shared by all processes in the JVM, it would be problematic if one of them could change the value inside an enum

--- creatingSimpleEnums/Season.java
+++ creatingSimpleEnums/Season.java
@@ -2,4 +2,15 @@ package creatingSimpleEnums;
 
 public enum Season{
  WINTER("Low"), SPRING("Medium"), SUMMER("High"), FALL("Medium");
+
+ private final String expectedVisitors;
+
+ private Season(String expectedVisitors){
+    this.expectedVisitors = expectedVisitors;
+
+ }
+
+  public void printExpectedVisitors(){
+   System.out.println(expectedVisitors);
+  }
 }
