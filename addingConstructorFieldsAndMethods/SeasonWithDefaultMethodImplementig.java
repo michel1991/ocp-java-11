dbcc51d86588f6e32adfd2c0778ffbdd6eb8e55f
michel1991 <michelmvoumamvouma@gmail.com>
2022-04-11 07:26:52 +0200

    code: Adding Constructors, Field and Methods (Title). Sample of enum with default method, we overridde the method with some value of enum

---
+++ addingConstructorFieldsAndMethods/SeasonWithDefaultMethodImplementig.java
@@ -0,0 +1,22 @@
+package addingConstructorFieldsAndMethods;
+
+public enum SeasonWithDefaultMethodImplementig{
+ WINTER{
+    public String getHours(){
+       return "10am-3pm";
+     }
+  },
+
+  SUMMER{
+     public String getHours(){ 
+       return "9am-7pm";
+     }
+   },
+
+  SPRING, FALL;
+
+  public String getHours(){
+    return "9am-5pm";
+  }
+
+}
