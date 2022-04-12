3ff0770f1edc2972ed1fb8de530358e603924c06
michel1991 <michelmvoumamvouma@gmail.com>
2022-04-12 07:01:55 +0200

    code: Writing a Local Class(Title). Example of writing a local class

---
+++ writingALocalClass/PrintNumbers.java
@@ -0,0 +1,21 @@
+package writingALocalClass;
+
+public class PrintNumbers{
+ private int length = 5;
+ public void calculate(){
+    final int width = 20;
+    class MyLocalClass{
+        public void multiply(){
+           System.out.print( length * width);
+        }
+    }
+    MyLocalClass local = new MyLocalClass();
+   local.multiply();
+ } 
+
+   public static void main(String[] args){
+      PrintNumbers outer = new PrintNumbers();
+      outer.calculate();
+   }
+
+}
