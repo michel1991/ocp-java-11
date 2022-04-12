658ca5bf397706c0231321e1a59833b0752486f2
michel1991 <michelmvoumamvouma@gmail.com>
2022-04-12 05:01:11 +0200

    code: Declaring an Inner Class(Title). Example of another synthax to instantiate inner class. Pay attention that inner class can not be static

--- declaringAnInnerClass/Outer.java
+++ declaringAnInnerClass/Outer.java
@@ -17,9 +17,19 @@ public class Outer{
      inner.go();
   }
 
+ public static void anotherWayToInstantiateInner(){
+    System.out.println();
+    System.out.println("Another way to instantiate inner ");
+    Outer outer = new Outer();
+    Inner inner = outer.new Inner();
+   inner.go();
+ }
+
  public static void main(String[] args){
    Outer outer = new Outer();
    outer.callInner();
+
+  anotherWayToInstantiateInner();
  }
 
  
