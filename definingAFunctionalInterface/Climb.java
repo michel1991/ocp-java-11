f8438914eadd78f9cde7154cd673e3d14fa0b5d4
michel1991 <michelmvoumamvouma@gmail.com>
2022-04-23 00:03:20 +0200

    code: Defining a Functional Interface (Title). adding Climb interface

---
+++ definingAFunctionalInterface/Climb.java
@@ -0,0 +1,17 @@
+package definingAFunctionalInterface;
+
+public interface Climb{
+   void reach();
+
+   default void fall(){
+   }
+
+   static int getBackUp(){
+     return 100;
+  }
+
+  private static boolean checkHeight(){
+    return true;
+  }
+  
+}
