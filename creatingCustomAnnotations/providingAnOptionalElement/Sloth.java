04b2a93004f5ae3c19874267817d452937763618
michel1991 <michelmvoumamvouma@gmail.com>
2022-04-24 01:45:27 +0200

    code: Creating Custom ==> Providing an Optional Element (Title). use optional element or property. Here we do not specify the optional element or attribute is omit

---
+++ creatingCustomAnnotations/providingAnOptionalElement/Sloth.java
@@ -0,0 +1,8 @@
+
+package creatingCustomAnnotations.providingAnOptionalElement;
+import creatingCustomAnnotations.specifyingARequiredElement.Exercise;
+
+@Exercise(hoursPerDay=0)
+public class Sloth{
+
+}
