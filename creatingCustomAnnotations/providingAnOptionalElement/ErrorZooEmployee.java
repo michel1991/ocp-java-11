6ed1b2df511332b75c8eed50a3a2f06913199b1b
michel1991 <michelmvoumamvouma@gmail.com>
2022-04-24 01:50:28 +0200

    code-error: Creating Custom ==> Providing an Optional Element (Title). In this version, the ErrorZooEmployee class does not compile because it defines a value that is incompatible with the int type of startHour. The compiler is doing its duty validating the type!

---
+++ creatingCustomAnnotations/providingAnOptionalElement/ErrorZooEmployee.java
@@ -0,0 +1,7 @@
+package creatingCustomAnnotations.specifyingARequiredElement;
+import creatingCustomAnnotations.specifyingARequiredElement.Exercise;
+
+@Exercise(hoursPerDay=7, startHour="8") 
+public class ErrorZooEmployee{
+
+}
