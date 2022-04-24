40ec6009f04a0229fe8f36d9ff31f3abf7055e76
michel1991 <michelmvoumamvouma@gmail.com>
2022-04-25 00:10:14 +0200

    code: Ignoring Warnings with @SuppressWarnings (Title). The first warning is because we are using a method SongBird.sing() that is deprecated. The second warning is triggered by the call to new ArrayList(), which does not define a generic type. An improved implementation would be to use new ArrayList<String>().

---
+++ ignoringWarningsWithSuppressWarnings/Nightingale.java
@@ -0,0 +1,19 @@
+package ignoringWarningsWithSuppressWarnings;
+import java.util.*;
+
+public class Nightingale{
+   public void wakeUp(){
+     SongBird.sing(10);
+   }
+
+  public void goToBed(){
+    SongBird.chirp(new ArrayList());
+   }
+
+   public static void main(String[] args){
+       var birdy = new Nightingale();
+       birdy.wakeUp();
+       birdy.goToBed();
+   }
+
+}
---
+++ ignoringWarningsWithSuppressWarnings/SongBird.java
@@ -0,0 +1,17 @@
+package ignoringWarningsWithSuppressWarnings;
+
+import java.util.*;
+
+class SongBird{
+ 
+@Deprecated
+ static void sing(int volume){
+      
+  }
+
+ static Object chirp(List<String> data){
+     return data.size();
+  }
+
+
+}
