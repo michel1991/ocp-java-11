0e0a4091ef4e446333e1640bc1d2427df03d80db
michel1991 <michelmvoumamvouma@gmail.com>
2022-04-24 16:26:54 +0200

    code: Understanding the TYPE_USE Value (Title). Example of how to use TYPE_USE

---
+++ understandingTheTYPEUSEValue/NetworkRepair.java
@@ -0,0 +1,20 @@
+package understandingTheTYPEUSEValue;
+
+import java.util.function.Predicate;
+
+public class NetworkRepair{
+  class OutSrc extends @Technical NetworkRepair{
+
+  }
+
+    public void repair(){
+      var repairSubclass = new @Technical NetworkRepair(){
+
+      };
+
+      var o = new @Technical NetworkRepair().new @Technical OutSrc();
+      int remaining = (@Technical int) 10.0;
+   }
+
+
+}
---
+++ understandingTheTYPEUSEValue/Technical.java
@@ -0,0 +1,9 @@
+package understandingTheTYPEUSEValue;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE_USE)
+@interface Technical{
+
+}
