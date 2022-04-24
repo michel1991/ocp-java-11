4b2240f19c02fadc1e7e789953b5e05c0bb2ade0
michel1991 <michelmvoumamvouma@gmail.com>
2022-04-24 16:42:53 +0200

    code: Storing Annotations with @Retention (Title). In this example, both annotations will retain the annotation information in their .class files, although only Swimmer will be available (via reflection) at runtime.

---
+++ storingAnnotationsWithRetention/Flier.java
@@ -0,0 +1,8 @@
+package storingAnnotationsWithRetention;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.CLASS)
+@interface Flier{
+
+}
