5c405ab7339034c9d8e5d32087f4c7a6933fb6c9
michel1991 <michelmvoumamvouma@gmail.com>
2022-04-24 17:37:11 +0200

    code-error: Supporting Duplicates with @Repeatable (Title).  Add the @Repeatable annotation. This code also does not compile, but this time because the @Repeatable annotation is not declared correctly. It requires a reference to a second annotation. That brings us to our next rule: to declare a @Repeatable annotation, you must define a containing annotation type value.

--- supportingDuplicatesWithRepeatable/Risk.java
+++ supportingDuplicatesWithRepeatable/Risk.java
@@ -1,5 +1,7 @@
 package supportingDuplicatesWithRepeatable;
+import java.lang.annotation.Repeatable;
 
+@Repeteable // does not compile
 public @interface Risk{
    String danger();
    int level() default 1;
