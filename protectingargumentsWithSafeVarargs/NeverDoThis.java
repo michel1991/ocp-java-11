2a21bbe9480d46c9e9bd29b4a441b353388497b8
michel1991 <michelmvoumamvouma@gmail.com>
2022-04-25 05:39:48 +0200

    code: Protecting Arguments with @SafeVarargs (Title). This code compiles, although it generates two compiler warnings. Both are related to type safety.

---
+++ protectingargumentsWithSafeVarargs/NeverDoThis.java
@@ -0,0 +1,17 @@
+package protectingargumentsWithSafeVarargs;
+import java.util.*;
+
+public class NeverDoThis{
+   final int thisIsUnsafe(List<Integer> ...carrot){  // carrot in  bold
+         Object[] stick = carrot;
+         stick[0] = Arrays.asList("nope!");
+        return carrot[0].get(0); // ClassCastException at runtime
+    }
+
+     public static void main(String[] a){
+       var carrot = new ArrayList<Integer>();
+       new NeverDoThis().thisIsUnsafe(carrot); // bold
+     
+     }
+
+}
