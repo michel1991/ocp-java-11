7da6563211e17b07b98793f84ed03fbffd1838af
michel1991 <michelmvoumamvouma@gmail.com>
2022-04-26 05:40:32 +0200

    code: Using Method References (Title). Not bad. There's a bit of redundancy, though. The lambda declares one parameter named s. However, it does nothing other than pass that parameter to another method. A method reference lets us remove that redundancy

---
+++ usingMethodReferences/Duckling.java
@@ -0,0 +1,9 @@
+package usingMethodReferences;
+
+public class Duckling{
+   public  static void makeSound(String sound){
+     LearnToSpeak learner = s -> System.out.println(s);
+     DuckHelper.teacher(sound, learner);
+   }
+
+}
