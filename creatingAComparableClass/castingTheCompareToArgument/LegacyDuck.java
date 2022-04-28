a44f0cb9aef35ec7d2f2791b732c6000a74ab336
michel1991 <michelmvoumamvouma@gmail.com>
2022-04-28 18:23:03 +0200

    code: Creating a Comparable Class ==> Casting the compareTo() Argument (Title). Since we don't specify a generic type for Comparable, Java assumes that we want an Object, which means that we have to cast to LegacyDuck before accessing instance variables on it.

---
+++ creatingAComparableClass/castingTheCompareToArgument/LegacyDuck.java
@@ -0,0 +1,10 @@
+package creatingAComparableClass.castingTheCompareToArgument;
+
+
+public class LegacyDuck implements Comparable{
+    private String name;
+    public int compareTo(Object obj){
+       LegacyDuck d = (LegacyDuck) obj; // cast because no generics
+       return name.compareTo(d.name);
+    }
+}
