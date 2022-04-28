19c38045875292ee5ef6772341e072bebce3a4dc
michel1991 <michelmvoumamvouma@gmail.com>
2022-04-28 18:10:42 +0200

    code: Creating a Comparable Class (Title). Another example of how to use Comparable interface

---
+++ creatingAComparableClass/Animal.java
@@ -0,0 +1,20 @@
+package creatingAComparableClass;
+
+public class Animal implements Comparable<Animal>{
+ private int d;
+ public int compareTo(Animal a){
+    return id - a.id; // sorts ascending by id
+  }
+  
+ public static void main(String[] args){
+     var a1 = new Animal();
+     var a2 = new Animal();
+     a1.id = 5;
+     a2.id = 7;
+
+     System.out.println(a1.compareTo(a2)); // -2
+    System.out.println(a1.compareTo(a1)); // 0
+    System.out.println(a2.compareTo(a1)); // 2
+  }
+   
+}
