f773b1808189af5ac535905ad7835376adee3066
michel1991 <michelmvoumamvouma@gmail.com>
2022-04-29 06:36:49 +0200

    code-error: Creating a Comparable Class ==> Sorting and Searching(Title). Earlier in the chapter, we talked about collections that require classes to implement Comparable. Unlike sorting, they don't check that you have actually implemented Comparable at compile time

---
+++ creatingAComparableClass/sortingAndSearching/UseTreeSet.java
@@ -0,0 +1,19 @@
+package creatingAComparableClass.sortingAndSearching;
+import creatingAComparableClass.comparingDataWithAComparator.Duck;
+import java.util.*;
+
+public class UseTreeSet{
+   static class Rabbit{
+      int id;
+   }
+
+  
+
+    public static void main(String[] args){
+        Set<Duck> ducks = new TreeSet<>();
+        ducks.add(new Duck("Puddles", 6));
+        Set<Rabbit> rabbits = new TreeSet<>();
+        rabbits.add(new Rabbit()); // ClassCastException
+  
+    }
+}
