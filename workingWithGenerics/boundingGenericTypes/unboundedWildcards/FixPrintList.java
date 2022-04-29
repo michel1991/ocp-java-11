e1a96cac16cfeeaef37f9c19be9bfefab0d850c3
michel1991 <michelmvoumamvouma@gmail.com>
2022-04-29 17:50:15 +0200

    fix:  Working with Generics ==> Bounding Generic Types => Unbounded Wildcards (Title). Print all type of object. The printList() method takes any type of list as a parameter. The keywords variable is of type List<String>. We have a match! List<String> is a list of anything. “Anything” just happens to be a String here.

---
+++ workingWithGenerics/boundingGenericTypes/unboundedWildcards/FixPrintList.java
@@ -0,0 +1,16 @@
+package workingWithGenerics.boundingGenericTypes.unboundedWildcards;
+import java.util.*;
+
+public class FixPrintList{
+   public static void printList(List<?> list){
+     for (Object x : list)
+      System.out.println(x);
+  }
+
+  public static void main(String[] args){
+      List<String> keywords = new ArrayList<>();
+      keywords.add("java");
+     printList(keywords); 
+  }
+
+}
