ccc0e3592a511da6125a12f5cf7ec4933e79e216
michel1991 <michelmvoumamvouma@gmail.com>
2022-04-29 04:50:26 +0200

    code: Creating a Comparable Class ==> Comparing Multiple Fields (Title). Adding Squirrel class. When writing a Comparator that compares multiple instance variables, the code gets a little messy.

---
+++ creatingAComparableClass/comparingMultipleFields/Squirrel.java
@@ -0,0 +1,21 @@
+package creatingAComparableClass.comparingMultipleFields;
+
+public class Squirrel{
+  private int weight;
+  private String species;
+
+  public Squirrel(int theWeight, String theSpecies){
+
+      weight = theWeight;
+      species = theSpecies;
+  }
+
+  public int getWeight(){
+     return weight;
+  }
+
+  public String getSpecies(){
+      return species;
+   }
+
+}
