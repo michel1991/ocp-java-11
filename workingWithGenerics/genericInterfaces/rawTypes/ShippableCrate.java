3d8690fb08cb8c5a0a16d4f8669ec73e0719d01c
michel1991 <michelmvoumamvouma@gmail.com>
2022-04-29 16:07:22 +0200

    code: Working with Generics ==>  Generic Interfaces ==> Raw Types (Title). The final way is to not use generics at all. This is the old way of writing code. It generates a compiler warning about Shippable being a raw type, but it does compile. Here the ship() method has an Object parameter since the generic type is not defined.

---
+++ workingWithGenerics/genericInterfaces/rawTypes/ShippableCrate.java
@@ -0,0 +1,8 @@
+package  workingWithGenerics.genericInterfaces.rawTypes;
+import  workingWithGenerics.genericInterfaces.Shippable;
+
+class ShippableCrate implements Shippable{
+  public void ship(Object t){
+
+  }
+}
