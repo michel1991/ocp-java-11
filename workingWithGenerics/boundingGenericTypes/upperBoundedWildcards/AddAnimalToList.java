304258c84fe1df4ab7584a29054de5cc78b71689
michel1991 <michelmvoumamvouma@gmail.com>
2022-04-29 19:09:12 +0200

    code-error:  Working with Generics ==> Bounding Generic Types =>  Upper‐Bounded Wildcards (Title). UNknow Object containing into list. The problem stems from the fact that Java doesn't know what type List<? extends Bird> really is. It could be List<Bird> or List<Sparrow> or some other generic type that hasn't even been written yet. Line 7 doesn't compile because we can't add a Sparrow to List<? extends Bird>, and line 8 doesn't compile because we can't add a Bird to List<Sparrow>. From Java's point of view, both scenarios are equally possible, so neither is allowed.

---
+++ workingWithGenerics/boundingGenericTypes/upperBoundedWildcards/AddAnimalToList.java
@@ -0,0 +1,15 @@
+package workingWithGenerics.boundingGenericTypes.upperBoundedWildcards;
+import java.util.*;
+
+public class AddAnimalToList{
+  
+  static class Sparrow extends Bird{}
+  static class Bird{}
+
+  public static void main(String[] args){
+     List<? extends Bird> birds = new ArrayList<Bird>();
+      birds.add (new Sparrow());  // does not compile
+      birds.add (new Bird());  // does not compile
+
+   }
+}
