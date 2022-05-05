268cf149433d6a38cb43c4f26670f0717a38a4d9
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-05 19:31:47 +0200

    code: Using Common Terminal Operations  ==> collect() (Title). Example of terminal operation collect, this is basic example with different  logic into  accumulator and combiner. Revert to book part for more detail

--- usingCommonTerminalOperations/CollectOperation.java
+++ usingCommonTerminalOperations/CollectOperation.java
@@ -1,6 +1,6 @@
 package usingCommonTerminalOperations;
 import java.util.stream.*;
-
+import java.util.TreeSet;
 
 public class CollectOperation{
 
@@ -9,11 +9,22 @@ public class CollectOperation{
         StringBuilder word = stream.collect(StringBuilder::new, StringBuilder::append, StringBuilder::append);
         System.out.println("basic example " + word);
   }
+  
+  static void differentLogicBetweenAccumulatorAndCombiner(){
+     Stream<String> stream = Stream.of("w", "o", "l", "f");
+     TreeSet<String> set = stream.collect(
+         TreeSet::new,
+         TreeSet::add,
+         TreeSet::addAll
+    ); 
 
+     System.out.println("differnt logic between accumulator and combiner " + set);
+  }
 
   public static void main(String[] args){
          
      basicExample();
+     differentLogicBetweenAccumulatorAndCombiner();
 
   }
 
