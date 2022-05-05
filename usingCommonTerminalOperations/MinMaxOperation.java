c8b8bc41711c60009bfe66f25b8068a37b5c07a1
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-05 16:30:56 +0200

    code: Using Common Terminal Operations  ==> min() and max() (Title). Example of terminal operation called min max

---
+++ usingCommonTerminalOperations/MinMaxOperation.java
@@ -0,0 +1,15 @@
+package usingCommonTerminalOperations;
+import java.util.stream.*;
+import java.util.*;
+
+public class MinMaxOperation{
+  public static void main(String[] args){
+     Stream<String> s = Stream.of("monkey", "ape", "bonobo");
+     Optional<String> min = s.min( (s1, s2) -> s1.length() - s2.length() );
+     min.ifPresent(System.out::println);
+
+     Optional<?> minEmpty = Stream.empty().min( (s1, s2) -> 0 );
+     System.out.println("Min empty stream ");
+     System.out.println(minEmpty.isPresent()); // false
+  }
+}
