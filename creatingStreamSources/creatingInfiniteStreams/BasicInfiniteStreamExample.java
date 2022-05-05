4d41c11a502595676720d7737def3411129759d1
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-05 15:32:47 +0200

    code: Creating Stream Sources  ==> Creating InFinite Streams (Title). overloaded of interate method of the stream

--- creatingStreamSources/creatingInfiniteStreams/BasicInfiniteStreamExample.java
+++ creatingStreamSources/creatingInfiniteStreams/BasicInfiniteStreamExample.java
@@ -7,6 +7,17 @@ public class BasicInfiniteStreamExample{
      Stream<Double> randoms = Stream.generate(Math::random);
      Stream<Integer> oddNumbers = Stream.iterate(1, n -> n + 2);  
    }
+   
+   static void overloadedIterate(){
+      Stream<Integer> oddNumberUnder100 =
+       Stream.iterate(
+         1, // seed
+         n -> n < 100 , // Predicate to specify when done
+         n -> n + 2  // UnaryOperator to get next value 
+      );
+
+  }
+
 
   public static void main(String[] args){
      firstExample();
