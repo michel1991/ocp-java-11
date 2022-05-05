a29fb4262d61c5609cd7c4455edfb781801c2b1c
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-05 18:33:53 +0200

    code: Using Common Terminal Operations  ==> reduce() (Title). Example of terminal operation reduce without identity, by default java remplace omit identity by optional

--- usingCommonTerminalOperations/ReduceOperation.java
+++ usingCommonTerminalOperations/ReduceOperation.java
@@ -1,6 +1,6 @@
 package usingCommonTerminalOperations;
 import java.util.stream.*;
-
+import java.util.function.*;
 
 public class ReduceOperation{
    static void withLambda(){ 
@@ -21,10 +21,23 @@ public class ReduceOperation{
       System.out.println("multiply:  "+ result ); 
    }
 
+
+   static void omitIdentity(){
+     BinaryOperator<Integer> op = (a, b) -> a * b;
+     Stream<Integer> empty = Stream.empty();
+     Stream<Integer> oneElement = Stream.of(3);
+     Stream<Integer> threeElements = Stream.of(3, 5, 6);
+
+     empty.reduce(op).ifPresent(System.out::println); // no output
+     oneElement.reduce(op).ifPresent(System.out::println); // 3
+     threeElements.reduce(op).ifPresent(System.out::println); // 90
+   }
+
   public static void main(String[] args){
-          withLambda();
-         withReferenceMethod();
+        withLambda();
+        withReferenceMethod();
         multiply();
+        omitIdentity();
   }
 
 }
