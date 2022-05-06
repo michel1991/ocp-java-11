9ac7f716c47f12666c2433d3b7f7f07c4e1e387c
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-06 06:48:14 +0200

    code: Putting Together the Pipeline ==> Peeking behind the Scenes  (Title). The correct answer is 11233455. As the first element passes through, 1 shows up in the peek() and print(). The second element makes it past limit() and peek(), but it gets caught  in filter(). The third and fifth elements behave like the first element. The fourth behaves like the second.

--- puttingTogetherThePipeline/PeekBehindTheScenes.java
+++ puttingTogetherThePipeline/PeekBehindTheScenes.java
@@ -11,9 +11,27 @@ class PeekBehindTheScenes{
       ;
 
    }
+
+
+   static void oddNumberWithPeek(){
+      var infinite = Stream.iterate(1, x -> x + 1);
+      infinite.limit(5)
+              .peek(System.out::print)
+              .filter(x -> x % 2 == 1)
+              .forEach(System.out::print)
+      ;
+
+   }
+
   public static void main(String[] args){
      System.out.println("odd number ");
       oddNumber();
+
+       System.out.println();
+       System.out.println("odd number with peek ");
+       oddNumberWithPeek();
+
+        System.out.println(); 
    }
 
 }
