2d78f3df3070050c903c16c4c65b2adb401d437e
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-07 14:51:19 +0200

    code: Collecting Results ==>  Collecting Using Grouping, Partitioning, and Mapping (Title). Partitioning by string length , we partitioning using 7 characters

--- collectingUsingBasicCollectors/PartitioningExample.java
+++ collectingUsingBasicCollectors/PartitioningExample.java
@@ -15,9 +15,23 @@ class PartitioningExample{
 
   }
 
+
+  static void partitioningByStringLengthSevenCharactersAllow(){
+      var ohMy = Stream.of("lions", "tigers", "bears");
+      Map<Boolean, List<String>> map = ohMy.collect(
+                       Collectors.partitioningBy(
+                             s -> s.length() <= 7
+                      )
+     );
+
+    System.out.println("partitioning by string length less than or equals to five (7) : " + map);
+
+  }
+
    public static void main(String[] args){
 
      partitioningByStringLength();
+     partitioningByStringLengthSevenCharactersAllow();
    }
 
 }
