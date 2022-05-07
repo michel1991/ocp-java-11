60d6ac8659b82d33ec68de164bcfb70933c7b645
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-07 12:56:07 +0200

    code: Collecting Results ==>  Collecting Using Basic Collectors (Title). Basic usage from stream to collection

--- collectingUsingBasicCollectors/BasicExample.java
+++ collectingUsingBasicCollectors/BasicExample.java
@@ -1,5 +1,6 @@
 package collectingUsingBasicCollectors;
 import java.util.stream.*;
+import java.util.*;
 
 class BasicExample{
    static void joiningDataIntoStream(){
@@ -16,11 +17,20 @@ class BasicExample{
      Double result = ohMy.collect(Collectors.averagingInt(String::length));
     System.out.println("average of string length " + result);
    
+  }
+
+  static void fromStreamToCollectionFilterAllStringStartsByT() {
+    var ohMy = Stream.of("lions", "tiggers", "bears");
+    TreeSet<String> result = ohMy.filter( s -> s.startsWith("t") )
+                                 .collect(Collectors.toCollection(TreeSet::new))
+     ;
+    System.out.println("from stream to Collection filter all string start by t " + result);
   }  
  
   public static void main(String[] args){
      joiningDataIntoStream();
      averageOfStringLength();
+     fromStreamToCollectionFilterAllStringStartsByT();
   }
 
 }
