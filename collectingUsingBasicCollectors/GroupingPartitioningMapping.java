64d170ea2f60b8cdfe2305f2e0cd72405c88a619
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-07 14:16:18 +0200

    code: Collecting Results ==>  Collecting Using Grouping, Partitioning, and Mapping (Title). Using Set to hold data during grounping element by string length

--- collectingUsingBasicCollectors/GroupingPartitioningMapping.java
+++ collectingUsingBasicCollectors/GroupingPartitioningMapping.java
@@ -8,11 +8,24 @@ class GroupingPartitioningMapping{
     Map<Integer, List<String>> map = ohMy.collect(
            Collectors.groupingBy(String::length)
     );
-   System.out.println("group by length " + map);
+   System.out.println("group by length using List to hold data:  " + map);
+  }
+
+   static void groupByLengthUsingSetThatHoldValue(){
+     var ohMy = Stream.of("lions", "tigers", "bears");
+    Map<Integer, Set<String>> map = ohMy.collect(
+           Collectors.groupingBy(
+               String::length,
+               Collectors.toSet()
+
+          )
+    );
+   System.out.println("group by length  using Set to hold data: " + map);
   }
 
   public static void main(String[] args){
      groupByLength();
+    groupByLengthUsingSetThatHoldValue();
   }
   
 }
