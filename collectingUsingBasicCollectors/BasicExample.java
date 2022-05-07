e0729d9803284f9ad81a71b518d414eba5aed0ba
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-07 13:18:45 +0200

    code: Collecting Results ==>  Collecting Using Basic Collectors (Title). Adding Collecting into maps

--- collectingUsingBasicCollectors/BasicExample.java
+++ collectingUsingBasicCollectors/BasicExample.java
@@ -1,6 +1,7 @@
 package collectingUsingBasicCollectors;
 import java.util.stream.*;
 import java.util.*;
+import java.util.function.*;
 
 class BasicExample{
    static void joiningDataIntoStream(){
@@ -25,12 +26,34 @@ class BasicExample{
                                  .collect(Collectors.toCollection(TreeSet::new))
      ;
     System.out.println("from stream to Collection filter all string start by t " + result);
+  }
+
+
+  static void basicCollectingIntoMap(){
+    var ohMy = Stream.of("lions", "tiggers", "bears");
+     Map<String,  Integer> map = ohMy.collect( Collectors.toMap(s-> s, String::length) );
+    System.out.println("basic example collecting into map " + map);
+
+  }
+
+  static void basicCollectingIntoMapWithIdentity(){
+    var ohMy = Stream.of("lions", "tiggers", "bears");
+     Map<String,  Integer> map = ohMy.collect( Collectors.toMap(Function.identity(), String::length) );
+    System.out.println("basic example collecting into map using function identity " + map);
+
   }  
  
   public static void main(String[] args){
      joiningDataIntoStream();
+     System.out.println();
      averageOfStringLength();
+      System.out.println();
      fromStreamToCollectionFilterAllStringStartsByT();
+      System.out.println();
+     basicCollectingIntoMap();
+    
+     System.out.println(); 
+     basicCollectingIntoMapWithIdentity();
   }
 
 }
