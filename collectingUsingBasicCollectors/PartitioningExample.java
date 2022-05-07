8e338bb47a0bdc412ab40776291f1a8715bde241
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-07 14:43:56 +0200

    code: Collecting Results ==>  Collecting Using Grouping, Partitioning, and Mapping (Title). Partitioning by string length

---
+++ collectingUsingBasicCollectors/PartitioningExample.java
@@ -0,0 +1,23 @@
+package collectingUsingBasicCollectors;
+import java.util.stream.*;
+import java.util.*;
+
+class PartitioningExample{
+  static void partitioningByStringLength(){
+      var ohMy = Stream.of("lions", "tigers", "bears");
+      Map<Boolean, List<String>> map = ohMy.collect(
+                       Collectors.partitioningBy(
+                             s -> s.length() <= 5
+                      )
+     );
+    
+    System.out.println("partitioning by string length less than or equals to five (5) : " + map);
+
+  }
+
+   public static void main(String[] args){
+
+     partitioningByStringLength();
+   }
+
+}
