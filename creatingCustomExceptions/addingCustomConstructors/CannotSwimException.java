34566c5ae6fd08ea16a29fc486f221239378a92a
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-08 10:24:10 +0200

    code: Creating Custom Exceptions ==> Adding Custom Constructors (Title). Basic example of how to create custom exception that extends Exception class

---
+++ creatingCustomExceptions/addingCustomConstructors/CannotSwimException.java
@@ -0,0 +1,16 @@
+package creatingCustomExceptions.addingCustomConstructors;
+
+public class CannotSwimException extends Exception{
+    public  CannotSwimException(){
+        super(); // Optional, compiler will insert automatically
+   }
+
+    public CannotSwimException(Exception e){
+       super(e);
+   }
+
+   public CannotSwimException(String message){ 
+       super(message);
+   }
+   
+}
