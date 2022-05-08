bbf52fbe82cacf9dd2f4be04dcde04cdca06c96e
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-08 13:13:20 +0200

    code: Constructing Try‐With‐Resources Statements (Title).  adding many declarations of resources in try-with-resources statement that means that the resource must be close in inverse order of the declaration

--- constructingTryWithResourcesStatements/CustomReaderMyFileReader.java
+++ constructingTryWithResourcesStatements/CustomReaderMyFileReader.java
@@ -1,11 +1,32 @@
 package constructingTryWithResourcesStatements;
 
 class CustomReaderMyFileReader{
-  public static void main(String[] args){
-     try( var bookReader = new MyFileReader("monkey") ) {
+
+  static void oneResourceDeclaration(){
+       try( var bookReader = new MyFileReader("monkey") ) {
           System.out.println("Try Block ");
-     }finally{
+      }finally{
         System.out.println("Finally Block");
-     } 
+     }
   }
+
+    static void manyResourceDeclaration(){ 
+       try( var bookReader = new MyFileReader("1");
+            var movieReader = new MyFileReader("2");
+            var tvReader = new MyFileReader("3");
+         ) {
+          System.out.println("Try Block ");
+      }finally{
+        System.out.println("Finally Block");
+     }
+  }
+
+  public static void main(String[] args){
+     System.out.println("One Resource Declaration ");
+     oneResourceDeclaration();
+     System.out.println(); 
+     System.out.println("Many Resource Declaration ==> close resource in inverse order of the declaration ");
+      manyResourceDeclaration();
+  }
+
 }
