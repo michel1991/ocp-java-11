035217f5301b615696871cd55239a759c21c7db1
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-09 04:54:32 +0200

    code: Working with Dates and Times ==>  Formatting Dates and Times  (Title). Example of formatting date/time with custom format

--- workingWithDatesAndTimes/FormattingBasicExample.java
+++ workingWithDatesAndTimes/FormattingBasicExample.java
@@ -13,7 +13,19 @@ class FormattingBasicExample{
      System.out.println(dt.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
   }
 
+  static void formatingUsingCustomFormat(){
+      LocalDate date = LocalDate.of(2020, Month.OCTOBER, 20);
+     LocalTime time = LocalTime.of(11, 12, 34);
+     LocalDateTime dt = LocalDateTime.of(date, time);
+     var f = DateTimeFormatter.ofPattern("MMMM dd, yyyy 'at' hh:mm");
+     System.out.println(dt.format(f));
+   }
+
    public static void main(String[] args){
-       basicExample();
+      System.out.println("Format using using predefined formats below "); 
+      basicExample();
+      System.out.println();
+      System.out.println("Custom format MMMM dd, yyyy 'at' hh:mm below ");
+      formatingUsingCustomFormat();
    }
 }
