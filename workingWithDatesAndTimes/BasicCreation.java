3a8e16237e0d2c0308bd49a7ac36da7872552706
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-09 04:09:21 +0200

    code: Working with Dates and Times ==> Creating Dates and Times & Using the of() Methods  (Title). Example of creation of localTime(time) using LocalTime.of method

--- workingWithDatesAndTimes/BasicCreation.java
+++ workingWithDatesAndTimes/BasicCreation.java
@@ -10,14 +10,31 @@ class BasicCreation{
      System.out.println(date2);
    }
 
+
+  static void createTimeWithOf(){
+     LocalTime time1 = LocalTime.of(6, 15);
+     LocalTime time2 =  LocalTime.of(6, 15, 30); // + seconds
+     LocalTime time3 =  LocalTime.of(6, 15, 30, 200); // + seconds + nanoseconds
+     System.out.println(time1);
+     System.out.println(time2);
+     System.out.println(time3);
+   }
+
+
   public static void main(String[] args){
     System.out.println(LocalDate.now());
    System.out.println(LocalTime.now());
    System.out.println(LocalDateTime.now());
    System.out.println(ZonedDateTime.now());
    System.out.println();
-   System.out.println("Creation using of method below");
+
+   System.out.println("LocalDate: Creation Date using of method below");
    createWithOf();
+   System.out.println();
+  
+   System.out.println("LocalTime: Creation Time using of method below");
+   createTimeWithOf();
+   System.out.println();
   }
 
 }
