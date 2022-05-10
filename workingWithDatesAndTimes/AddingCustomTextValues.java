f7131297b4647a3c24a83f07ad9f54935640c0f9
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-10 05:38:25 +0200

    code: Working with Dates and Times ==>  Formatting Dates and Times ==> Adding Custom Text Values (Title). More,  Escape the text by surrounding it with a pair of single quotes('). Without escaping the text values with single quotes, an exception will be thrown at runtime if the text cannot be interpreted as a date/time symbol.

--- workingWithDatesAndTimes/AddingCustomTextValues.java
+++ workingWithDatesAndTimes/AddingCustomTextValues.java
@@ -19,12 +19,28 @@ class AddingCustomTextValues{
 
   }
 
+  static void doubleQuoteExample(){
+    var dt = LocalDateTime.of(2020, Month.OCTOBER, 20, 6, 15, 30);
+    var g1 = DateTimeFormatter.ofPattern("MMMM dd', party''s at' hh:mm");
+    System.out.println(dt.format(g1));
+
+    var g2 = DateTimeFormatter.ofPattern("'System format, hh:mm: 'hh:mm");
+    System.out.println(dt.format(g2));
+
+    var g3 = DateTimeFormatter.ofPattern("'NEW!' yyyy', yaay!'");
+    System.out.println(dt.format(g3));
+   }
+
   public static void main(String[] args){
     basicExample();
     System.out.println();
 
     System.out.println("Escape the text by surrounding it with a pair of single quote, below");
     usingPairOfSingleQuoteToEscapeText();
+
+    System.out.println();
+    System.out.println("More Escape the text by surrounding it with a pair of single quote, below");
+    doubleQuoteExample();
   }
 
 }
