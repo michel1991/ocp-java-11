cde6decc7bb670b1fa75f81924709591be115a01
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-10 06:44:41 +0200

    code: Supporting Internationalization and Localization ==> Picking a Locale (Title). Create Locale using constructor

--- supportingInternationalizationAndLocalization/PickingALocale.java
+++ supportingInternationalizationAndLocalization/PickingALocale.java
@@ -12,11 +12,20 @@ class PickingALocale{
        System.out.println(Locale.GERMANY); // de_DE
    }
 
+
+    static void createWithConstructor(){
+      System.out.println("French language (fr) only : " +  new Locale("fr")); 
+       System.out.println(new Locale( "language and code country Hindi(hi) In India(IN): " + "hi", "IN")); 
+   }
+
  public static void main(String[] args){
-     System.out.println("Current locale");
+     System.out.println("Current locale Below");
      currentLocal();
      System.out.println();
-     System.out.println("German locale");
+     System.out.println("German locale Below");
      germanLanguage();
+     System.out.println();
+     System.out.println("create locale with constructor Below ");
+     createWithConstructor();
   }
 }
