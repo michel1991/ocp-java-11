785430c6286b5bd28fe6ca9a76628a9f8ca8717b
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-10 06:30:28 +0200

    code:Supporting Internationalization and Localization ==> Picking a Locale (Title). The first example selects the German language, which is spoken in many countries, including Austria ( de_AT) and Liechtenstein ( de_LI). The second example selects both German the language and Germany the country. While these examples may look similar, they are not the same. Only one includes a country code.

--- supportingInternationalizationAndLocalization/PickingALocale.java
+++ supportingInternationalizationAndLocalization/PickingALocale.java
@@ -7,7 +7,16 @@ class PickingALocale{
     System.out.println(locale);
   }
 
+  static void germanLanguage(){
+      System.out.println(Locale.GERMAN); // de
+       System.out.println(Locale.GERMANY); // de_DE
+   }
+
  public static void main(String[] args){
+     System.out.println("Current locale");
      currentLocal();
+     System.out.println();
+     System.out.println("German locale");
+     germanLanguage();
   }
 }
