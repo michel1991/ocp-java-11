14cee28423814c1e69fb7d53431147b5f045a893
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-11 07:06:40 +0200

    code:Supporting Internationalization and Localization ==> Parsing Numbers (Title). Parsing currency

--- supportingInternationalizationAndLocalization/ParsingNumbers.java
+++ supportingInternationalizationAndLocalization/ParsingNumbers.java
@@ -12,10 +12,21 @@ class ParsingNumbers{
       System.out.println("FRANCE : " + fr.parse(s));
    }
 
+   static void parsingCurrency() throws ParseException {
+      
+      String income = "$92,807.99"; //$92,807.99
+      var cf = NumberFormat.getCurrencyInstance(Locale.US);
+      double value = (Double) cf.parse(income);
+      System.out.println("from " + income + " to " +  value);
+   }
+
    public static void main(String[] args) throws ParseException{
-     System.out.println("Basic Parsing");
+     System.out.println("Basic Parsing Below");
      basicParseNumber();
      System.out.println();
-
+     
+     System.out.println("Parsing Currency Below");
+     parsingCurrency();
+     System.out.println();
    }
 }
