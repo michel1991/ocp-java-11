72fe27da627dce41bb6f5b0ea41fad1330946657
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-12 07:25:56 +0200

    code: Loading Properties with Resource Bundles ==> Creating a Resource Bundle (Title). Example to loop through locale file properties and reading the values

--- loadingPropertiesWithResourceBundles/CreatingAResourceBundle.java
+++ loadingPropertiesWithResourceBundles/CreatingAResourceBundle.java
@@ -7,10 +7,22 @@ class CreatingAResourceBundle{
       System.out.println(rb.getString("hello") + ", "+ rb.getString("open"));
   }
 
+  static void loopThroughtAllProperties(){
+    var us = new Locale("en", "US");
+     ResourceBundle rb = ResourceBundle.getBundle("loadingPropertiesWithResourceBundles.Zoo", us);
+     rb.keySet().stream()
+                .map(k -> k + ": "+ rb.getString(k))
+                .forEach(System.out::println)
+    ;
+  }
+
    public static void main(String[] args){
     var us = new Locale("en", "US");
     var france = new Locale("fr", "FR");
     printWelcomeMessage(us);
     printWelcomeMessage(france);
+    System.out.println();
+    System.out.println("loop throuh all keys from file Zoo_en propertie file");
+    loopThroughtAllProperties();
    }
 }
