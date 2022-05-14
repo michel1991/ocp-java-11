bbe9c14fcdfa62c39e7c6ef00b0a61c43cfa9fa0
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-14 09:55:13 +0200

    code-error: Failing to Compile with a Cyclic Dependency (Title): Example show dependency cyclic when using module

---
+++ butterflyModule/zoo/butterfly/Butterfly.java
@@ -0,0 +1,3 @@
+package zoo.butterfly;
+public class Butterfly{
+}
---
+++ caterpillarModule/zoo/caterpillar/CaterpillarLifecycle.java
@@ -0,0 +1,5 @@
+package zoo.caterpillar;
+import zoo.butterfly.Butterfly;
+public interface CaterpillarLifecycle{
+     Butterfly emergeCocoon();
+}
