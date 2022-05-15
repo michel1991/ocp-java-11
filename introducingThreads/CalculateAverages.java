650a005a16bf83e73bfe535311e74436077f6c36
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-15 02:25:10 +0200

    code: code: Introducing Threads ==>  Defining a Task with Runnable ==> Creating Runnable classes  (Title). Example of class that implements Runnable. It is use to pass data

---
+++ introducingThreads/CalculateAverages.java
@@ -0,0 +1,15 @@
+package introducingThreads;
+
+public class CalculateAverages implements Runnable{
+  private double[] scores;
+   public CalculateAverages(double[] scores){
+      this.scores = scores;
+   } 
+
+    @Override
+    public void run(){
+     // define work here that uses the scores object
+
+   }
+
+}
