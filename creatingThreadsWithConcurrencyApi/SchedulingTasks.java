9abf5f00cf16be1ddccc3ed833c26416ced841ab
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-16 05:43:31 +0200

    code: Creating Threads with the Concurrency API  ==> Scheduling Taks (Title). Template of how to schedule the task in the future using ScheduledExecutorService

---
+++ creatingThreadsWithConcurrencyApi/SchedulingTasks.java
@@ -0,0 +1,14 @@
+package creatingThreadsWithConcurrencyApi;
+import java.util.concurrent.*;
+
+public class SchedulingTasks{
+  static void basicSchedulingTask(){
+     ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+     Runnable task1 = () -> System.out.println("Hello Zoo");
+     Callable<String> task2 = () -> "Monkey";
+     ScheduledFuture<?> r1 = service.schedule(task1, 10, TimeUnit.SECONDS );
+    ScheduledFuture<?> r2 = service.schedule(task2, 8, TimeUnit.MINUTES );
+   }
+
+
+}
