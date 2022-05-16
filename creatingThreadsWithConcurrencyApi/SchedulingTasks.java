d62d3e43f7e8332b35f6ee679d4bc7198c927a14
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-16 05:50:53 +0200

    code: Creating Threads with the Concurrency API  ==> Scheduling Taks (Title). Using scheduleAtFixedRate. The scheduleAtFixedRate() method creates a new task and submits it to the executor every period, regardless of whether the previous task finished.

--- creatingThreadsWithConcurrencyApi/SchedulingTasks.java
+++ creatingThreadsWithConcurrencyApi/SchedulingTasks.java
@@ -9,6 +9,11 @@ public class SchedulingTasks{
      ScheduledFuture<?> r1 = service.schedule(task1, 10, TimeUnit.SECONDS );
     ScheduledFuture<?> r2 = service.schedule(task2, 8, TimeUnit.MINUTES );
    }
-
+  
+ static void repeatTaskWithScheduleAtFixedRate(){
+   ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+   Runnable task1 = () -> System.out.println("Hello Zoo");
+    service.scheduleAtFixedRate(task1, 5, 1, TimeUnit.MINUTES);
+  }
 
 }
