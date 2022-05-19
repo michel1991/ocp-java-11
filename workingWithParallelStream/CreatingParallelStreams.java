0d501dead5d7d96f5a2d204e1766d90173cbd5c3
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-19 19:54:15 +0200

    code: Working with Parallel Stream ==> Creating Parallel Streams => Calling parallel() on an Existing Stream (Title). Calling parallel method on existing stream.

---
+++ workingWithParallelStream/CreatingParallelStreams.java
@@ -0,0 +1,11 @@
+package workingWithParallelStream;
+import java.util.stream.*;
+import java.util.*;
+
+public class CreatingParallelStreams{
+   static void allingParallelOnAnExistingStream(){
+      Stream<Integer> s1 = List.of(1, 2).stream();
+       Stream<Integer> s2 = s1.parallel();
+   }
+ 
+}
