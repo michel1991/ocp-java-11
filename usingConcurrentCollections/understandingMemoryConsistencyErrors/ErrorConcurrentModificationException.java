3b5988a6675dc61ec95f7fd928e10de08614cc90
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-19 12:49:19 +0200

    code-error: Using Concurrent Collections ==> Understanding Memory Consistency Errors(Title). Example of ConcurrentModificationException due to loop through collection and at the same time delete data

---
+++ usingConcurrentCollections/understandingMemoryConsistencyErrors/ErrorConcurrentModificationException.java
@@ -0,0 +1,13 @@
+package  usingConcurrentCollections.understandingMemoryConsistencyErrors;
+import java.util.*;
+
+public class  ErrorConcurrentModificationException{
+  public static void main(String[] args){
+      var foodData = new HashMap<String, Integer>();
+      foodData.put("penguin", 1);
+      foodData.put("flamingo", 2);
+       for (String key: foodData.keySet())
+            foodData.remove(key);
+  }
+
+}
