99183f08768f920326c6cb5f770e162b8986d1e8
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-19 16:31:10 +0200

    code-error: Using Concurrent Collections ==> Obtaining Synchronized Collections (Title). Delete element while looping in  Synchronized Collections generate ConcurrentModificationException

---
+++ usingConcurrentCollections/ObtainingSynchronizedCollections.java
@@ -0,0 +1,14 @@
+package usingConcurrentCollections;
+import java.util.*;
+
+public class ObtainingSynchronizedCollections{
+  public static void main(String[] args){
+       var foodData = new HashMap<String, Object>();
+        foodData.put("penguin", 1);
+         foodData.put("flamingo", 2);
+
+         var synFoodData = Collections.synchronizedMap(foodData);
+         for(String key : synFoodData.keySet() )
+            synFoodData.remove(key);
+  }
+}
