3f76004e1af1a77109be6af628e633f5967ee5d7
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-19 17:48:25 +0200

    code-error: Identifying Threading Problems ==> Understanding Liveness ==> Deadlock (Title); Example of Deadlock

---
+++ identifyingThreadingProblems/understandingLiveness/deadlock/Fox.java
@@ -0,0 +1,73 @@
+package identifyingThreadingProblems.understandingLiveness.deadlock;
+import java.util.concurrent.*;
+
+class Food{
+
+}
+
+class Water{
+
+}
+
+public class Fox{
+   public void eatAndDrink(Food food, Water water){
+       synchronized(food){
+           System.out.println("Got Food!");
+           move();
+           synchronized(water){
+               System.out.println("Got Water!");
+           }
+       }
+   } // end method
+
+
+   public void drinkAndEat(Food food, Water water){
+        synchronized(water){
+           System.out.println("Got Water!");
+           move();
+           synchronized(food){
+               System.out.println("Got Food!");
+           }
+       }
+
+   }// end method
+
+
+ public void move(){
+       try{
+           Thread.sleep(100);
+       }catch(InterruptedException e){
+        //Handle exception
+
+      }
+  }
+
+
+  public static void main(String[] args){
+     // Create participants and resources
+     Fox foxy = new Fox();
+     Fox tails = new Fox();
+     Food food = new Food();
+     Water water = new Water();
+
+    //process data
+    ExecutorService service = null;
+    try{
+          service = Executors.newScheduledThreadPool(10);
+          service.submit( 
+                  () -> foxy.eatAndDrink(food, water)
+          );
+
+         service.submit(
+                  () -> tails.drinkAndEat(food, water)
+          );
+    }finally{
+        if(service !=null)
+           service.shutdown(); 
+     }
+ 
+  }
+
+
+
+}
