ffefb3a666a8c07b6058a1090d638330a94084b4
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-20 07:41:24 +0200

    code Working: with Parallel Stream ==> Processing Parallel Reductions ==> Combining Results with reduce (Title). Using reduce with parallel stream omit combiner parameter that produce incorrect result

--- workingWithParallelStream/ProcessingParallelReductionCombineResults.java
+++ workingWithParallelStream/ProcessingParallelReductionCombineResults.java
@@ -15,8 +15,22 @@ public class  ProcessingParallelReductionCombineResults{
      );
    }
 
+   static void substractWithReduceWithOmitCombinerParameter(){
+      System.out.println(
+          List.of(1, 2, 3, 4, 5, 6)
+              .parallelStream()
+              .reduce(
+                  0,
+                  (a, b) -> (a - b)
+              )
+     );
+   }
+
    public static void main(String[] args){
        System.out.println("Reduce list of char bellow");
        reduceListOfChar();
+       System.out.println();
+        System.out.println("Substract with Reduce omit combiner parameter using value [1, 2, 3, 4, 5, 6]");
+       substractWithReduceWithOmitCombinerParameter();
    }
 }
