5beca8ce96d4908e907f0395e3108afd62382ecc
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-20 06:16:56 +0200

    code: Working with Parallel Stream ==> Performing a Parallel Decomposition ==> Ordering forEach Results (Title). Example that using forEachOrdered in parallel stream. The forEachOrdered operation forces our stream into a single-threaded process

--- workingWithParallelStream/PerformingAParallelDecomposition.java
+++ workingWithParallelStream/PerformingAParallelDecomposition.java
@@ -39,14 +39,33 @@ class PerformingAParallelDecomposition{
      System.out.println("Time: " + timeTaken + " seconds");
    }
 
+  static void usingForEachOrderedOnParallelStream(){
+     long start = System.currentTimeMillis();
+     List.of(1, 2, 3, 4, 5)
+         .parallelStream()
+         .map( w -> doWork(w) )
+         .forEachOrdered( s -> System.out.print( s + " ") )
+
+      ;
+     System.out.println();
+     var timeTaken = ( System.currentTimeMillis() - start ) / 1000;
+     System.out.println("Time: " + timeTaken + " seconds");
+   }
+
 
    public static void main(String[] args)
    {
      System.out.println("using serial stream below ");
      usingSerialStream();
+     System.out.println();
 
      System.out.println("using parallel stream below ");
      usingParallelStream();
+     System.out.println();
+
+     System.out.println("using parallel stream with forEachOrdered who work like serial stream  below ");
+     usingForEachOrderedOnParallelStream();
+     System.out.println();
    }
 
 }
