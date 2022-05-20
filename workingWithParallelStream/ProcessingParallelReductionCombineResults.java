47bd1e8ef75598ac48019a070bc725bf77cfdeba
michel1991 <michelmvoumamvouma@gmail.com>
2022-05-20 07:50:28 +0200

    code Working: with Parallel Stream ==> Processing Parallel Reductions ==> Combining Results with reduce (Title). Using reduce with parallel stream, using an identity parameter that is not truly an identity value

--- workingWithParallelStream/ProcessingParallelReductionCombineResults.java
+++ workingWithParallelStream/ProcessingParallelReductionCombineResults.java
@@ -26,11 +26,29 @@ public class  ProcessingParallelReductionCombineResults{
      );
    }
 
+
+    static void reduceListOfStringWithInvalidIdentityParamter(){
+      System.out.println(
+          List.of("w", "o", "l", "f")
+              .parallelStream()
+              .reduce(
+                  "X",
+                  String::concat
+              )
+     );
+   }
+
+
    public static void main(String[] args){
        System.out.println("Reduce list of char bellow");
        reduceListOfChar();
        System.out.println();
-        System.out.println("Substract with Reduce omit combiner parameter using value [1, 2, 3, 4, 5, 6]");
+       System.out.println("Substract with Reduce omit combiner parameter using value [1, 2, 3, 4, 5, 6]");
        substractWithReduceWithOmitCombinerParameter();
-   }
+       System.out.println();
+       System.out.println("Reduce list of char using an identity parameter that is not truly an identity value bellow");
+      reduceListOfStringWithInvalidIdentityParamter();
+      System.out.println();;     
+
+    }
 }
