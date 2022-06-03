1c34c28f71361f3706f9738e44029bf9db5fcd9c
michel1991 <michelmvoumamvouma@gmail.com>
2022-06-03 14:46:41 +0200

    code: Low‐Level vs. High‐Level Streams (Title). Wrapping high level stream into high level stream example

--- lowLevelHighLevelStreams/LowLevelHighLevelStreams.java
+++ lowLevelHighLevelStreams/LowLevelHighLevelStreams.java
@@ -3,8 +3,20 @@ import java.io.*;
 
 class LowLevelHighLevelStreams{
    static void wrappingLowLevelStreamToHighLevelStream() throws IOException{
-         try( var br = new BufferedReader(new FileReader("zoo-data_text")) ){
+         try( var br = new BufferedReader(new FileReader("zoo-data.txt")) ){
            System.out.println(br.readLine());
         }
    }
+
+    static void wrappingHighLevelStreamToHighLevelStream() throws IOException,  ClassNotFoundException{
+         try( var ois = new ObjectInputStream( new BufferedInputStream (
+                                                   new FileInputStream("zoo-data.txt")
+                                                   )
+                                              ) 
+          ){
+             System.out.println(ois.readObject());
+        }
+   }
+
+
 }
