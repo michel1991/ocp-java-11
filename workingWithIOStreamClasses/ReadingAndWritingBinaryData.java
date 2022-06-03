e3627ccd2f428af42a073341e81948de00190e0e
michel1991 <michelmvoumamvouma@gmail.com>
2022-06-03 19:59:38 +0200

    code:Working with I/O Stream Classes ==> Reading and Writing Binary Data (Title). Example of reading and writing binary Data using buffer

--- workingWithIOStreamClasses/ReadingAndWritingBinaryData.java
+++ workingWithIOStreamClasses/ReadingAndWritingBinaryData.java
@@ -14,4 +14,25 @@ class ReadingAndWritingBinaryData{
        }
   }
 
+  void copyFileWithBuffer(File src, File dest) throws IOException{
+      try (
+             var in = new BufferedInputStream (
+                  new FileInputStream (src)
+             );
+
+             var out = new BufferedOutputStream(
+                     new FileOutputStream(dest)
+                 )
+         ) {
+            var buffer = new byte[1024];
+            int lengthRead;
+            
+            while( (lengthRead = in.read(buffer)) >0 ) {
+                out.write(buffer, 0, lengthRead);
+                out.flush();
+            }
+
+        }
+  }
+
 }
