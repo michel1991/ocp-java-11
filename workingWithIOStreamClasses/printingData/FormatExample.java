9338bc1d0d862f4d1dc709f7737dff0a728b261a
michel1991 <michelmvoumamvouma@gmail.com>
2022-06-04 14:58:15 +0200

    code: Printing Data ==> format() (Title). adding another Example of using format method of System.out

--- workingWithIOStreamClasses/printingData/FormatExample.java
+++ workingWithIOStreamClasses/printingData/FormatExample.java
@@ -1,5 +1,13 @@
 package workingWithIOStreamClasses.printingData;
 class FormatExample{
+
+   static void anotherExample(){
+       String name = "James";
+       double score = 90.25;
+      int total = 100;
+      System.out.format("%s:%n  Score: %f out of %d", name, score, total);      
+   }
+
   public static void main(String ...args){
       String name = "Lindsey";
      int orderId = 5;
@@ -7,5 +15,7 @@ class FormatExample{
      System.out.format("Hello "+name+", order "+orderId+" is ready");
      System.out.println();
      System.out.format("Hello %s, order %d is ready", name, orderId);
+     System.out.println();
+     anotherExample();
    }
 }
