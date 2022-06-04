1ac595b986c69f70a80d11df4af29fcad9268f7f
michel1991 <michelmvoumamvouma@gmail.com>
2022-06-04 04:03:44 +0200

    code:Working with I/O Stream Classes ==> Serializing Data ==> Storing Data with ObjectOutputStream and ObjectInputStream (Title). Example of deserialize class from file

--- workingWithIOStreamClasses/serializingData/StoringDataWithObjectOutputStreamAndObjectInputStream.java
+++ workingWithIOStreamClasses/serializingData/StoringDataWithObjectOutputStreamAndObjectInputStream.java
@@ -15,4 +15,32 @@ class StoringDataWithObjectOutputStreamAndObjectInputStream{
                 out.writeObject(gorilla);
        } // try-resource
   }
+
+   
+List <Gorilla> readFromFile(File dataFile) throws IOException, ClassNotFoundException{
+     var gorillas =  new ArrayList<Gorilla>();
+      try(
+               var in = new ObjectInputStream(
+                     new BufferedInputStream(
+                         new FileInputStream(dataFile)
+                      )
+                 )
+       ){
+            while (true){
+                var object = in.readObject();
+                if(object instanceof Gorilla)
+                  gorillas.add( (Gorilla) object );
+
+
+           }
+       }catch(EOFException e ){
+            //File end reached
+       }
+     
+      return gorillas;
+ }
+
+
+
+
 }
