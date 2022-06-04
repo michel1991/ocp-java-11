f4732b0a194a464d27580ff9ed55b2ebf2c0e731
michel1991 <michelmvoumamvouma@gmail.com>
2022-06-04 15:36:30 +0200

    code: Printing Data ==> Sample PrintWriter Program  (Title). Sample print writer program

---
+++ workingWithIOStreamClasses/printingData/SamplePrinterProgram.java
@@ -0,0 +1,24 @@
+package workingWithIOStreamClasses.printingData;
+import java.io.*;
+
+class SamplePrinterProgram{
+  void sample() throws IOException{
+        File source = new File("zoo-sample-printwriter-program.log");
+        try( 
+            var out = new PrintWriter(
+               new BufferedWriter (
+                   new FileWriter(source)
+               )
+            )
+        ){
+             out.print("Today's weather is: ");
+             out.print("Sunny");
+             out.print("Today's temperature at the zoo is:");
+             out.print(1 / 3.0);
+             out.println('C');
+             out.format("It has rained %5.2f inches this year %d", 10.2, 2021);
+             out.println();
+            out.printf("It may rain %s more inches this year", 1.2);
+         }
+   }
+}
