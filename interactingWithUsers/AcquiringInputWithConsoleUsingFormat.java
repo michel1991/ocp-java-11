cef61208754b408707bc58bb4942af533f7355bf
michel1991 <michelmvoumamvouma@gmail.com>
2022-06-04 17:12:00 +0200

    code:Interacting with Users ==>  Acquiring Input with Console ==> format()  (Title). Example of  Acquiring Input with Console using format method

---
+++ interactingWithUsers/AcquiringInputWithConsoleUsingFormat.java
@@ -0,0 +1,16 @@
+package interactingWithUsers;
+import java.io.*;
+
+class AcquiringInputWithConsoleUsingFormat{
+   public static void main(String[] args){
+      Console console = System.console();
+     if( console == null){
+         throw new RuntimeException("Console not available");
+      }else{
+           console.writer().println("Welcome to Our Zoo!");
+           console.format("It has %d animals and employs %d people", 391, 25);
+           console.writer().println();
+           console.printf("The zoo spans %5.1f acres", 128.91);
+       }
+   }
+}
