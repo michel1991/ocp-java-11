1300b420d76d2070ca56b534f3bafa97bd4647b4
michel1991 <michelmvoumamvouma@gmail.com>
2022-06-04 15:18:55 +0200

    code: Printing Data ==> format() (Title). adding another Example of using format method of System.out with flags

--- workingWithIOStreamClasses/printingData/FormatExample.java
+++ workingWithIOStreamClasses/printingData/FormatExample.java
@@ -8,6 +8,20 @@ class FormatExample{
       System.out.format("%s:%n  Score: %f out of %d", name, score, total);      
    }
 
+   static void exampleWithFlags(){
+      var pi = 3.14159265359;
+     System.out.format("[%f]", pi); // 3.141593
+      System.out.println();
+      System.out.format("[%12.8f]", pi); // 3.14159265 
+      System.out.println();
+      System.out.format("[%012f]", pi); // 00003.141593 
+      
+       System.out.println();
+       System.out.format("[%12.2f]", pi); // 3.14
+       System.out.println();
+      System.out.format("[%.3f]", pi); // 3.142 
+     }
+
   public static void main(String ...args){
       String name = "Lindsey";
      int orderId = 5;
@@ -17,5 +31,8 @@ class FormatExample{
      System.out.format("Hello %s, order %d is ready", name, orderId);
      System.out.println();
      anotherExample();
+     System.out.println();
+    System.out.println("Using format with flags");
+    exampleWithFlags();
    }
 }
