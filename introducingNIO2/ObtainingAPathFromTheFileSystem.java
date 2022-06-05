436a480752d23d7b213e6bcd63c05bbb36129d12
michel1991 <michelmvoumamvouma@gmail.com>
2022-06-05 15:58:07 +0200

    code: Connecting to Remote File Systems

--- introducingNIO2/ObtainingAPathFromTheFileSystem.java
+++ introducingNIO2/ObtainingAPathFromTheFileSystem.java
@@ -1,10 +1,19 @@
 package introducingNIO2;
 import java.nio.*;
 import java.nio.file.*;
+import java.net.*;
 
 class ObtainingAPathFromTheFileSystem{
   void obtaining(){
        Path path3 = FileSystems.getDefault().getPath("/home/users");
    }
+
+ void connectToRemoteFileSystems() throws URISyntaxException{
+     FileSystem fileSystem = FileSystems.getFileSystem(
+         new URI("http://www.selikoff.net")
+     );
+
+     Path path = fileSystem.getPath("duck.txt");
+  }
 
 }
