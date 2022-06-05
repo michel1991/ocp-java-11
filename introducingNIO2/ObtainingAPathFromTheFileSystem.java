1d239f1ef044cf93f9a100cb74a787df2192e39a
michel1991 <michelmvoumamvouma@gmail.com>
2022-06-05 15:49:37 +0200

    code: Obtaining a Path from the FileSystem

---
+++ introducingNIO2/ObtainingAPathFromTheFileSystem.java
@@ -0,0 +1,10 @@
+package introducingNIO2;
+import java.nio.*;
+import java.nio.file.*;
+
+class ObtainingAPathFromTheFileSystem{
+  void obtaining(){
+       Path path3 = FileSystems.getDefault().getPath("/home/users");
+   }
+
+}
