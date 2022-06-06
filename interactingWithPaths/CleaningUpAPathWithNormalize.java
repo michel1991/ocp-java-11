e479d578c3344c55599bd3240cb0611ce23f4f1b
michel1991 <michelmvoumamvouma@gmail.com>
2022-06-06 08:19:10 +0200

    code:  Interacting with Paths ==> Cleaning Up a Path with normalize() (Title) . Compare equivalent path

--- interactingWithPaths/CleaningUpAPathWithNormalize.java
+++ interactingWithPaths/CleaningUpAPathWithNormalize.java
@@ -2,6 +2,13 @@ package interactingWithPaths;
 import java.nio.file.*;
 
 class CleaningUpAPathWithNormalize{
+
+   static void compareEquivalentPath(){
+     var p1 =  Paths.get("/pony/../weather.txt");
+     var p2 =  Paths.get("/weather.txt");
+     System.out.println(p1.equals(p2));
+     System.out.println(p1.normalize().equals(p2.normalize()));
+   }
    public static void main(String... args){
      var p1 = Path.of("./armadillo/../shells.txt");
      System.out.println(p1.normalize());  // shells.txt
@@ -11,5 +18,8 @@ class CleaningUpAPathWithNormalize{
 
      var p3 = Path.of("../../fish.txt");
      System.out.println(p3.normalize());  // ../../fish.txt
+     System.out.println();
+     System.out.println("compare equivalent path ");
+     compareEquivalentPath();
   }
 }
