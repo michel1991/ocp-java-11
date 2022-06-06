990d476187e9b33854adce00ec8eda6011474f4b
michel1991 <michelmvoumamvouma@gmail.com>
2022-06-06 07:39:24 +0200

    code:  Interacting with Paths ==> Deriving a Path with relativize()  (Title). f both path values are relative, then the relativize() method computes the paths as if they are in the same current working directory. Alternatively, if both path values are absolute, then the method computes the relative path from one absolute location to another, regardless of the current working directory. The following example demonstrates this property when run on a Windows computer

--- interactingWithPaths/DerivingAPathWithRelativize.java
+++ interactingWithPaths/DerivingAPathWithRelativize.java
@@ -2,6 +2,13 @@ package interactingWithPaths;
 import java.nio.file.*;
 
 class DerivingAPathWithRelativize{
+  static void withAbsolutePath(){
+     Path path3 = Paths.get("E:\\habitat");
+     Path path4 = Paths.get("E:\\sanctuary\\raven\\poe.txt");
+     System.out.println(path3.relativize(path4)); // ..\sanctuary\raven\poe.txt
+    System.out.println(path4.relativize(path3)); // ..\..\..\habitat
+  }
+
   public static void main(String... args){
      Path path1 = Path.of("fish.txt");
      Path path2 = Path.of("friendly/birds.txt");
