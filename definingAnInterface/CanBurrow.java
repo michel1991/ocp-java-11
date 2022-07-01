db4de3caa6729447fa89ff709c91ed205a180d5d
michel1991 <michelmvoumamvouma@gmail.com>
2022-07-01 13:00:40 +0200

    code:Defining an Interface (Title). This example demonstrate that interface inherit from rule of abstract class. So we have implicit access modifier (public, package-private) in the declaration of the interface.We could also add an implicit specifier(abstract) follow by the keyword interface and follow by the name of interface. All access modifier of method are public but they can be implicit. We could also add implicit specifier abstract in the declaration of method. For the end about constant we have implicit access modifier and specifier who is public static final.

---
+++ definingAnInterface/CanBurrow.java
@@ -0,0 +1,6 @@
+package definingAnInterface;
+
+public abstract interface CanBurrow{
+   public abstract Float getSpeed(int age); // abstract method interface
+   public static final int MINIMUM_DEPTH = 2; // Interface variable
+}
