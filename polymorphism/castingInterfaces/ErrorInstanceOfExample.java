43052c171ccb93c699188ea7de44310b55f09049
michel1991 <michelmvoumamvouma@gmail.com>
2022-07-02 03:40:28 +0200

    code-error: Interfaces and the instanceof Operator (Title). Adding example with two abstract class at left and right when using instanceof operator do not compile

--- polymorphism/castingInterfaces/ErrorInstanceOfExample.java
+++ polymorphism/castingInterfaces/ErrorInstanceOfExample.java
@@ -1,13 +1,15 @@
 package polymorphism.castingInterfaces;
 import java.util.*;
-public class InstanceOfExample{
+public class ErrorInstanceOfExample{
    public static void main(String[] args){
      Number myNumber = 5;
      Double myDouble = Double.valueOf(6);
-     boolean result =  myNumber instanceof List;
-     boolean resultWithoutBaseClass =   myDouble instanceof List;
-     System.out.println(" with ref from base class " + result);
-     System.out.println(" with ref from base class " +  resultWithoutBaseClass);
+     boolean resultWithAbstractTypeAtLeft =  myNumber instanceof List;
+     boolean resultWithoutConcreteClassAtLeft =   myDouble instanceof List;
+      boolean resultWithTwoAbstractType = myNumber instanceof AbstractList;
+     System.out.println(" with abstract base class reference  at left and interface at right " + resultWithAbstractTypeAtLeft);
+     System.out.println(" with concrete class at left and interface at right " +  resultWithoutConcreteClassAtLeft);
+     System.out.println(" with abstract class reference at left and abstract at right " +  resultWithTwoAbstractType);
 
    }
 }
