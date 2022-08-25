73cba5090b54cdbb069e40a035389158e6daa6b8
michel1991 <michelmvoumamvouma@gmail.com>
2022-08-25 22:29:36 +0200

    code: Creating a static Nested Class (Title). Using static member field of static inner class

--- creatingAStaticNestedClass/Enclosing.java
+++ creatingAStaticNestedClass/Enclosing.java
@@ -1,14 +1,20 @@
-package declaringAnInnerClass;
+package creatingAStaticNestedClass;
 
 public class Enclosing{
 
   static class Nested{
       private int price = 6;
+      static String name = "jobs";
+  }
+
+  public static String getNameOfNestedClass(){
+     return Nested.name;
   }
 
   public static void main(String[] args){
      Nested nested = new Nested();
      System.out.println(nested.price);
+     System.out.println(" member field from static inner class " + getNameOfNestedClass() );
   }
 
 }
