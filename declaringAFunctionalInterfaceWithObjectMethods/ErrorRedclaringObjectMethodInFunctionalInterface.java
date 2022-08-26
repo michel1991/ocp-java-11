73d7533ba07298e1ba4416edd93182284cef768c
michel1991 <michelmvoumamvouma@gmail.com>
2022-08-26 15:58:21 +0200

    code-error:Declaring a Functional Interface with Object Methods (Title). If we override object method(equals, hashCode, toString) in Interface(Functional interface) with invalid declration the compiler generate error. This proove that this methods are not count as functional interface method

---
+++ declaringAFunctionalInterfaceWithObjectMethods/ErrorRedclaringObjectMethodInFunctionalInterface.java
@@ -0,0 +1,6 @@
+package declaringAFunctionalInterfaceWithObjectMethods;
+interface ErrorRedclaringObjectMethodInFunctionalInterface{
+  int toString();
+   String hashCode();
+   int equals(Object ob);
+}
