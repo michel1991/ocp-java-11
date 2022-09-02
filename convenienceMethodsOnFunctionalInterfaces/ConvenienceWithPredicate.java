44490f6233d965e4342544c7efb0b28ed1338923
michel1991 <michelmvoumamvouma@gmail.com>
2022-09-02 21:19:06 +0200

    refactor: Convenience Methods on Functional Interfaces. refactor and method of Predicate with advantage to use and method with separate common use case with new way to do thing

--- convenienceMethodsOnFunctionalInterfaces/ConvenienceWithPredicate.java
+++ convenienceMethodsOnFunctionalInterfaces/ConvenienceWithPredicate.java
@@ -2,22 +2,60 @@ package convenienceMethodsOnFunctionalInterfaces;
 import java.util.function.*;
 
 public class ConvenienceWithPredicate{
- 
-  static void predicate(){
-     Predicate<String> egg = s -> s.contains("egg");
+   static Predicate<String>  getEgg(){
+       Predicate<String> egg = s -> s.contains("egg");
+       return egg;
+   }
+
+   static Predicate<String>  getBrown(){
      Predicate<String> brown = s -> s.contains("brown"); 
-    
-     Predicate<String> brownEggs = s -> s.contains("egg") && s.contains("brown");
+     return brown;
+   }
 
-     Predicate<String> otherEggs = s -> s.contains("egg") && !s.contains("brown");
+   static Predicate<String> getBrownEgg(){
+       Predicate<String> egg = getEgg();
+        Predicate<String> brown = getBrown();
+	Predicate<String> convenienceBrownEggs = egg.and(brown);
 
-     Predicate<String> convenienceBrownEggs = egg.and(brown);
-     Predicate<String> convenienceOtherEggs = egg.and(brown.negate());   
+	return convenienceBrownEggs;
   }
 
+  static Predicate<String>  getOtherEggs(){
+       Predicate<String> egg = getEgg();
+       Predicate<String> brown = getBrown();
+       Predicate<String> convenienceOtherEggs = egg.and(brown.negate());
+
+       return convenienceOtherEggs;
+  }
+
+  static Predicate<String>  getBrownEggOldVersion(){
+    Predicate<String> brownEggs = s -> s.contains("egg") && s.contains("brown");
+
+    return   brownEggs ;
+  }
+
+  static Predicate<String> getOtherEggsOldVersion(){
+    Predicate<String> otherEggs = s -> s.contains("egg") && !s.contains("brown");
+
+    return otherEggs;
+  }
+
+
  public static void main(String[] args){
-   predicate();
-     
+   //predicate();
+   String myText = "I Like eat egg and I have brown head";
+   Predicate<String> eggBrownVersionWithoutAnd =  getBrownEggOldVersion();
+   System.out.println(" find egg brown: Result without and method of Predicate " + eggBrownVersionWithoutAnd.test(myText) );
+
+   Predicate<String> eggBrownVersionWithoutVersionWithAnd =  getBrownEgg();
+   System.out.println(" find egg brown:  Result with and method of Predicate " + eggBrownVersionWithoutVersionWithAnd.test(myText) );
+   
+
+   Predicate<String> otherEggsWithoutCombinaisonOfPredicate = getOtherEggsOldVersion();
+   System.out.println(" find egg not contains brown: Result without and method of Predicate " + otherEggsWithoutCombinaisonOfPredicate.test(myText) );
+   Predicate<String> otherEggsWithAndMethod = getOtherEggs();
+   System.out.println(" find egg not contains brown: Result without and method of Predicate " + otherEggsWithAndMethod.test(myText) );
+
  }
 
 }
