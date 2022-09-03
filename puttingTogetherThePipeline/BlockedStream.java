add477d99aa5e2fd8a6394b7966589fcedbcaf97
michel1991 <michelmvoumamvouma@gmail.com>
2022-09-03 19:31:03 +0200

    code-error: Putting Together the Pipeline (Title). Blocked infinite stream on filter intermediary operation. The  filter condition is not satisfy

--- puttingTogetherThePipeline/BlockedStream.java
+++ puttingTogetherThePipeline/BlockedStream.java
@@ -12,9 +12,22 @@ class BlockedStream{
          .sorted()
          .limit(2)
          .forEach(System.out::println)
-    ;
+         ;
     }
+
+    static void filterWaitSoLong(){
+      
+     Stream<String> list = Stream.generate( () -> "Olaf Lazisson" );
+     list
+         .filter( n -> n.length() == 4)
+         .sorted()
+         .limit(2)
+         .forEach(System.out::println)
+         ;
+    }
+    
   public static void main(String... args){
+     System.out.println("Do not start this program it will consume all your memory ");
 
   }
 }
