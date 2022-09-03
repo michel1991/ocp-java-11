40a7bf65966ef2278dedd0d69f0ff60d07cd1217
michel1991 <michelmvoumamvouma@gmail.com>
2022-09-03 19:00:57 +0200

    code: Putting Together the Pipeline (Title). Adding code context that drive us to use functional programing with stream pipeline by focus on what not how

--- puttingTogetherThePipeline/FirstTwoNamesOfOurFriends.java
+++ puttingTogetherThePipeline/FirstTwoNamesOfOurFriends.java
@@ -3,6 +3,21 @@ import java.util.*;
 import java.util.stream.*;
 
 class FirstTwoNamesOfOurFriends{
+
+   public void wayWeUseHow(){
+       var list = List.of("Toby", "Anna", "Leroy", "Alex");
+       List<String> filtered = new ArrayList<>();
+       for (String name : list)
+	        if(name.length() == 4)
+			filtered.add(name);
+       Collections.sort(filtered);
+       var iter = filtered.iterator();
+       if(iter.hasNext())
+	       System.out.println(iter.next());
+        if(iter.hasNext())
+		System.out.println(iter.next());
+   }	
+
   public static void main(String[] args){
      var list = List.of("Toby", "Anna", "Leroy", "Alex");
      list.stream()
