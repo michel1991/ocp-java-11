3492571a247a46d6defab561d1db4f883003c9e3
michel1991 <michelmvoumamvouma@gmail.com>
2022-09-15 09:49:01 +0200

    refactor: intermediate operation by sorted, using lambda expression to implement the compare method

--- usingCommonIntermediateOperations/SortedOperation.java
+++ usingCommonIntermediateOperations/SortedOperation.java
@@ -6,6 +6,7 @@ import java.util.function.*;
 class SortedOperation{
   
    static void basicExample(){
+      System.out.println("Sorted without comparator using natural order,  values are: brown-, bear-"); 	   
       Stream<String> s = Stream.of("brown-", "bear-");
 
     
@@ -13,20 +14,53 @@ class SortedOperation{
             .forEach(System.out::print);  
    }
 
-   static void exampleWithComparator(){
+   static void exampleWithReverseComparatorUsingLamnbda(){
+      System.out.println("Below with commparator define function to reverse, lambda, data are: brown bear-,  grizzly-");	   
       Stream<String> s = Stream.of("brown bear-", "grizzly-");  
       
-      
-      s.sorted(Comparator.reverseOrder())                
+      Comparator<String>  comparator = (a, b) -> b.compareTo(a);       
+      s.sorted(comparator)              // Comparator.reverseOrder()   
             .forEach(System.out::print);  
    }
 
+   static void exampleWithComparatorMethodReference(){                            
+      System.out.println("Below with commparator using reference method, data are: brown bear-,  grizzly-");
+      Stream<String> s = Stream.of("brown bear-", "grizzly-");  
+      
+      Comparator<String>  comparator = (a, b) -> b.compareTo(a);       
+      s.sorted(Comparator.reverseOrder())                 
+            .forEach(System.out::print);  
+   }
+  
 
-  public static void main(String[] args){
+   static void exampleWithCustomComparator(){
+     System.out.println("Below with commparator using lambda that print each value use by method to compare, data are: michjobs-,  mvouma-, stephane-, mvouma-"); 
+     Stream<String> s = Stream.of("michjobs-", "mvouma-", "stephane-", "mvouma-");  
+      
+      Comparator<String>  comparator = (a, b) -> {
+	System.out.println("a: " + a + " b: "+ b);
+         return  b.compareTo(a);
+      };       
+
+      s.sorted(comparator)            
+            .forEach(System.out::print);  
+   }
+
+  public static void main(String[] args){	  
       basicExample();
       System.out.println();
-      System.out.println("Below with commparator");
-     exampleWithComparator();
+      System.out.println();
+
+      System.out.println();
+      exampleWithReverseComparatorUsingLamnbda();
+      System.out.println();
+
+      System.out.println();
+      exampleWithComparatorMethodReference();
+      System.out.println(); 
+
+     System.out.println();
+     exampleWithCustomComparator();
   }
 
 }
