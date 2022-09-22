d1cf115248254b9d26c7f8800733122675ad5aa0
michel1991 <michelmvoumamvouma@gmail.com>
2022-09-22 14:09:45 +0200

    code: Creating Threads with the Concurrency API ==> Submitting Task Collections (Title). Part with invokeAny method. Create thread executor when using invokeAny methods

--- creatingThreadsWithConcurrencyApi/SubmittingTaskCollectionsWithInvokeAny.java
+++ creatingThreadsWithConcurrencyApi/SubmittingTaskCollectionsWithInvokeAny.java
@@ -6,14 +6,27 @@ public class SubmittingTaskCollectionsWithInvokeAny{
   static void example() throws InterruptedException, ExecutionException{
      ExecutorService service = null;
      System.out.println("begin");
-     Callable<String> task = () -> "result";
-     String data = service.invokeAny(
-          List.of(task, task, task)
-      );
+     try{
+	   service = Executors.newSingleThreadExecutor();  
+         Callable<String> task = () -> "result";
+         String data = service.invokeAny(
+              List.of(task, task, task)
+          );
       
       System.out.println(data);
+     }finally{
+       if( service != null){
+            service.shutdown();
+       }
+        System.out.println("end");
+     }
   
-      System.out.println("end");
+  }
+
+  public static void main(String... args) throws InterruptedException, ExecutionException {
+      System.out.println("The main Thread start "); 
+      example();
+      System.out.println("The main Thread end ");
   }
 
 }
