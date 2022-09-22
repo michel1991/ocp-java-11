43726ed6f97ace28d524f2dd3da81e0c36262a53
michel1991 <michelmvoumamvouma@gmail.com>
2022-09-22 13:51:54 +0200

    code: Creating Threads with the Concurrency API ==> Submitting Task Collections (Title). Par with invokeAll method. Create thread executor when using invokeAll methods

--- creatingThreadsWithConcurrencyApi/SubmittingTaskCollectionsWithInvokeAll.java
+++ creatingThreadsWithConcurrencyApi/SubmittingTaskCollectionsWithInvokeAll.java
@@ -6,14 +6,27 @@ public class SubmittingTaskCollectionsWithInvokeAll{
   static void example() throws InterruptedException, ExecutionException{
      ExecutorService service = null;
      System.out.println("begin");
-     Callable<String> task = () -> "result";
-     List<Future<String>> list = service.invokeAll(
-          List.of(task, task, task)
-      );
-      for(Future<String> future : list){
-          System.out.println(future.get());
-       }
+     try{
+	   service = Executors.newSingleThreadExecutor();    
+          Callable<String> task = () -> "result";
+          List<Future<String>> list = service.invokeAll(
+               List.of(task, task, task)
+            );
+          for(Future<String> future : list){
+               System.out.println(future.get());
+          }
+     } finally{
+	     if( service != null){
+                service.shutdown();
+	     }
       System.out.println("end");
+     }
+  }
+
+  public static void main(String... args) throws InterruptedException, ExecutionException {
+          System.out.println("main thread start ");
+	  example() ;
+	   System.out.println("main thread end ");
   }
 
 }
