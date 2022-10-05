ddedc747f4c204778f89a3260635cde2a8679256
michel1991 <michelmvoumamvouma@gmail.com>
2022-10-05 18:41:01 +0200

    code: file mark and reset method example:  adding file input stream for test

--- commonIOStreamOperation/manipulatingInputStreams/MarkAndReset.java
+++ commonIOStreamOperation/manipulatingInputStreams/MarkAndReset.java
@@ -3,15 +3,35 @@ import java.io.*;
 
 class MarkAndReset{
       public void readData(InputStream is) throws IOException{
-         System.out.print( (char) is.read() ); // L
+         System.out.println( (char) is.read() ); // L
          if(is.markSupported()) {
                is.mark(100); // marks up to 100 bytes
-              System.out.print( (char) is.read() ); //I
-              System.out.print( (char) is.read() ); //O
+              System.out.println( (char) is.read() ); //I
+              System.out.println( (char) is.read() ); //O
               is.reset(); // Resets stream to position before I
           }
-          System.out.print( (char) is.read() ); // I
-          System.out.print( (char) is.read() ); // O
-          System.out.print( (char) is.read() ); // N
-     }    
+          System.out.println( (char) is.read() ); // I
+          System.out.println( (char) is.read() ); // O
+          System.out.println( (char) is.read() ); // N
+     } 
+
+     public static void main(String... args) throws IOException{
+        BufferedInputStream bis = null;
+        try{ 
+	    String fullPath = System.getProperty("user.dir") + "/commonIOStreamOperation/manipulatingInputStreams"+ "/markAndResetData.txt";	
+            File file = new File(fullPath);
+	    
+             bis = new BufferedInputStream(new FileInputStream( file ));
+             MarkAndReset sample = new MarkAndReset();
+             sample.readData(bis);
+        } catch(IOException ex){
+         ex.printStackTrace();
+        } finally{
+                if( bis != null ){
+                   bis.close();
+                }
+
+        }
+     }
+
 }
