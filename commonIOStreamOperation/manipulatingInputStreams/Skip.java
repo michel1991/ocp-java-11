cae5e609852477fd1b0321a24bbea6007c60b163
michel1991 <michelmvoumamvouma@gmail.com>
2022-10-05 18:38:58 +0200

    code: file skip method example:  adding file input stream for test

--- commonIOStreamOperation/manipulatingInputStreams/Skip.java
+++ commonIOStreamOperation/manipulatingInputStreams/Skip.java
@@ -1,7 +1,7 @@
 package commonIOStreamOperation.manipulatingInputStreams;
 import java.io.*;
 
-class MarkAndReset{
+class Skip{
       // Example of input TIGERS
       public void readData(InputStream is) throws IOException{
          System.out.print( (char) is.read() ); // T
@@ -10,5 +10,26 @@ class MarkAndReset{
           System.out.print( (char) is.read() ); // R
           System.out.print( (char) is.read() ); // S
 
-     }    
+     }
+
+     public static void main(String... args) throws IOException{
+
+
+         BufferedInputStream bis = null;
+        try{ 
+            String fullPath = System.getProperty("user.dir") + "/commonIOStreamOperation/manipulatingInputStreams"+ "/skipData.txt";    
+            File file = new File(fullPath);
+            
+             bis = new BufferedInputStream(new FileInputStream( file ));
+             MarkAndReset sample = new MarkAndReset();
+             sample.readData(bis);
+        } catch(IOException ex){
+         ex.printStackTrace();
+        } finally{
+                if( bis != null ){
+                   bis.close();
+                }
+
+        }
+     }
 }
