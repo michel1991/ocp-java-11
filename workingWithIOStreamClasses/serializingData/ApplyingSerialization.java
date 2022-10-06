53676089a0016cf6cfbe200176a6bba6b487cd08
michel1991 <michelmvoumamvouma@gmail.com>
2022-10-06 12:18:24 +0200

    code: sample read gorilla from file (Deserialization)

--- workingWithIOStreamClasses/serializingData/ApplyingSerialization.java
+++ workingWithIOStreamClasses/serializingData/ApplyingSerialization.java
@@ -3,13 +3,16 @@ import java.util.*;
 import java.io.*;
 
 class ApplyingSerialization{
-   public static void main(String... args) throws IOException{
+   public static void main(String... args) throws IOException, ClassNotFoundException{
       StoringDataWithObjectOutputStreamAndObjectInputStream handle = new StoringDataWithObjectOutputStreamAndObjectInputStream();
       var gorillas = new ArrayList<Gorilla>();
       gorillas.add(new Gorilla( "Grodd", 5, false) );
       gorillas.add(new Gorilla( "Ishmael", 8, true) );
      String fullPath = System.getProperty("user.dir") + "/workingWithIOStreamClasses/serializingData"+ "/gorilla.txt";    
      File file = new File(fullPath);
-      handle.saveToFile(gorillas, file); 
+      handle.saveToFile(gorillas, file);
+
+      var gorillasFromDisk = handle.readFromFile(file);
+      System.out.println(gorillasFromDisk);
    }
 } 
