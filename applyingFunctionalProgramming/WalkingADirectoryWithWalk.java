088304f17ea51fc60754bf655552a681397d913e
michel1991 <michelmvoumamvouma@gmail.com>
2022-10-09 16:14:27 +0200

    code: applying Functional Programming ==> Walking A Directory With Walk(Title). adding main method

--- applyingFunctionalProgramming/WalkingADirectoryWithWalk.java
+++ applyingFunctionalProgramming/WalkingADirectoryWithWalk.java
@@ -7,9 +7,11 @@ import java.util.function.*;
 class WalkingADirectoryWithWalk{
   private long getSize(Path p) {
      try{
+      System.out.println(" path to get size " + p);	     
      return Files.size(p);
      } catch(IOException e){
           // Hanlde exception
+	  e.printStackTrace();
        }
 
      return 0L;
@@ -29,16 +31,25 @@ class WalkingADirectoryWithWalk{
         try (var s = Files.walk(source, depth) ){
              return s.parallel()
                      .filter( p -> !Files.isDirectory(p) )
-                     .mapToLong(this::getSize)
+                     .mapToLong(this::getSize) // use this method to handle exception with Lambda
                      .sum()
                   ;
         }
     }
 
-   void sampleOfCall() throws IOException{
+   static void sampleOfCall() throws IOException{
        var myObject = new WalkingADirectoryWithWalk();
-       var size = myObject.getPathSize(Path.of("/fox/data"));
-       System.out.format("Total Size: %.2f megabytes", (size/1000000.0) );
+
+       String mainDirectory = "folderWalkingADirectoryWithWalk";      
+       String fullPathSource = System.getProperty("user.dir") + "/applyingFunctionalProgramming" + "/" + mainDirectory;
+       var path = Paths.get(fullPathSource, "home");
+
+       var size = myObject.getPathSize(path);
+       System.out.format("Total Size: %.2f megabytes,  file source %s", (size/1000000.0), path.toString() );
+   }
+
+   public static void main(String... args) throws IOException{
+     sampleOfCall();
    }
 
 }
