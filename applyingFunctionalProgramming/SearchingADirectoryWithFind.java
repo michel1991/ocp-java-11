60729ff307181bb4d2c764c60a8278fdd2a21a39
michel1991 <michelmvoumamvouma@gmail.com>
2022-10-09 16:59:38 +0200

    code: applying Functional Programming ==> Searching a directory with find. adding main method

--- applyingFunctionalProgramming/SearchingADirectoryWithFind.java
+++ applyingFunctionalProgramming/SearchingADirectoryWithFind.java
@@ -4,19 +4,29 @@ import java.io.*;
 
 class SearchingADirectoryWithFind{
    static void make() throws IOException{
-     Path path = Paths.get("/bigcats");
+     //Path path = Paths.get("/bigcats");
+
+      String fullPathSource = System.getProperty("user.dir") + "/applyingFunctionalProgramming";
+       var path = Paths.get(fullPathSource);
      long minSize = 1_000;
      try(
            var s = Files.find(
                         path,
                         10,
-                       (p, a) -> a.isRegularFile() && p.toString().endsWith(".java") && a.size() > minSize
+                       (p, a) -> { 
+			     System.out.println("file visited " + p);  
+			     return  a.isRegularFile() && p.toString().endsWith(".java") && a.size() > minSize;
+			}
 
                )
        ){
            s.forEach(System.out::println);
        }
 
+   }
+
+   public static void main(String... args) throws IOException{
+      make();
    }
   
 }
