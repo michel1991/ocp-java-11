8dabda139b8643cd7b7e732aeb41af39979b7a18
michel1991 <michelmvoumamvouma@gmail.com>
2022-10-09 17:27:53 +0200

    code: applying Functional Programming ==> Reading a file with lines. adding main method

--- applyingFunctionalProgramming/ReadingAFileWithLines.java
+++ applyingFunctionalProgramming/ReadingAFileWithLines.java
@@ -3,15 +3,25 @@ import java.nio.file.*;
 import java.io.*;
 
 class ReadingAFileWithLines{
+
+   public static Path getSourcePath(){
+       String mainDirectory = "folderReadingAFileWithLines";    
+       String fullPathSource = System.getProperty("user.dir") + "/applyingFunctionalProgramming" + "/" + mainDirectory;    
+       
+       Path path = Paths.get( fullPathSource, "fish", "sharks.txt");
+
+       return path;
+   }
+
    static void read() throws IOException{
-       Path path = Paths.get("/fish/sharks.log");
+       Path path = getSourcePath();
        try( var s = Files.lines(path) ){
              s.forEach(System.out::println);
         }
    }
 
    static void readWithFilter() throws IOException{
-       Path path = Paths.get("/fish/sharks.log");
+       Path path = getSourcePath();
        try( var s = Files.lines(path) ){
              s.filter( f -> f.startsWith("WARN:") )
               .map( f -> f.substring(5) )
@@ -20,5 +30,15 @@ class ReadingAFileWithLines{
             ;
         }
    }
+   
 
+   public static void main(String... args) throws IOException{
+     System.out.println( " source path is " + getSourcePath());	   
+     System.out.println("read all lines in lazy way");
+     read();
+     System.out.println();
+
+     System.out.println(" find line that start with WARN" );
+     readWithFilter();
+   }
 }
