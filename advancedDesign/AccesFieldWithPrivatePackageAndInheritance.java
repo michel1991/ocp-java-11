b1dbeaeefcf56a5b9affa6e20655a699cf38cee9
michel1991 <michelmvoumamvouma@gmail.com>
2022-10-14 19:08:59 +0200

    doc: adding explanation about this response

--- advancedDesign/AccesFieldWithPrivatePackageAndInheritance.java
+++ advancedDesign/AccesFieldWithPrivatePackageAndInheritance.java
@@ -21,6 +21,19 @@ class Macaw extends Parrot {
     }
 }
 
+/*
+  * The compiles and runs without issue, making options A through D incorrect.
+  * The fly() method is correctly overridden in each subclass since the signature is the same,
+  *  the access modifier is less restrictive, and the return types are covariant.
+  * For covariance, Macaw is a subtype of Parrot, which is a subtype of Bird,
+  * so overridden return types are valid. Likewise, the constructors are all implemented properly,
+  * with explicit calls to the parent constructors as needed.
+  * Line 19 calls the overridden version of fly() defined in the Macaw class, as overriding replaces the method
+  * regardless of the reference type. This results in feathers being assigned a value of 3.
+  * The Macaw object is then cast to Parrot, which is allowed because Macaw inherits Parrot.
+  * The feathers variable is visible since it is defined in the Bird class, and line 19 prints 3,
+  * making option E the correct answer.
+*/
 public class AccesFieldWithPrivatePackageAndInheritance {
     public static void main(String... sing) {
         Bird p = new Macaw(4);
