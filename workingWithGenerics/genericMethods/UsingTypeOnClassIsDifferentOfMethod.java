c62f66e967cf90edcfc6f5107cd698e50fd3c0f6
michel1991 <michelmvoumamvouma@gmail.com>
2022-10-14 13:10:57 +0200

    code: Generic Methods(Title). Adding another example with int who show that formal type declare on method is different of formal type declared at class level

--- workingWithGenerics/genericMethods/UsingTypeOnClassIsDifferentOfMethod.java
+++ workingWithGenerics/genericMethods/UsingTypeOnClassIsDifferentOfMethod.java
@@ -4,10 +4,18 @@ class UsingTypeOnClassIsDifferentOfMethod{
        CrateParmeterTypeOnClassIsDifferentOfMethod<String> crate = new CrateParmeterTypeOnClassIsDifferentOfMethod<String>();
        return crate.tricky("michjobs");
     }
+        
+    static int create(int size){
+       CrateParmeterTypeOnClassIsDifferentOfMethod<String> crate = new CrateParmeterTypeOnClassIsDifferentOfMethod<String>();
+       return crate.tricky(size);
+    }
 
     public static void main(String... args){
        String name = createName();
        System.out.println("the name create is " + name);
+
+         System.out.println("the name create size is " + create(1));
+
     }
 
 }
