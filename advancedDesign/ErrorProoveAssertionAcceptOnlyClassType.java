2331575112108377bf8afee009ec0a219e541f64
michel1991 <michelmvoumamvouma@gmail.com>
2022-10-15 21:21:00 +0200

    code-error: show that Class type only use in annotation declaration no custom class

---
+++ advancedDesign/ErrorProoveAssertionAcceptOnlyClassType.java
@@ -0,0 +1,17 @@
+import java.lang.annotation.*;
+
+/*
+* Line 4 does not compile because the default value of an element must be a non-null constant expression.
+*  Line 5 also does not compile because an element type must be one of the predefined immutable types: a primitive, String,
+* Class, enum, another annotation, or an array of these types. The rest of the lines do not contain any compilation errors.
+*/
+public class ErrorProoveAssertionAcceptOnlyClassType {
+     static class Food {}
+    @Inherited public @interface Unexpected {
+         public String rsvp() default null;
+          Food food();
+          public String[] dessert();
+          final int numberOfGuests = 5;
+          long startTime() default 0L;
+     }
+}
