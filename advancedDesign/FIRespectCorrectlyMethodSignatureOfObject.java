ce6fc9480e5fcd38c42577183d8693a25dbec8b0
michel1991 <michelmvoumamvouma@gmail.com>
2022-10-15 14:29:31 +0200

    code: FI respect correctly the declaration of the methods toString, hashCode, equals(who take object) of the Object class

---
+++ advancedDesign/FIRespectCorrectlyMethodSignatureOfObject.java
@@ -0,0 +1,27 @@
+/*
+* The requirement for a functional interface is that it contains a single abstract method, excluding any that match the signature
+* of public methods in java.lang.Object. Meow is a valid functional interface, as is Whistle that extends it,
+* because the only abstract method that counts is meow(). The hashCode() and toString(), while abstract, match the
+* signature of methods in Object, while purr() is a default method. For these reasons, options A and C are correct.
+* Options B and D are incorrect because they do not contain any abstract methods.
+* Option E is incorrect because it contains two abstract methods. If the equals() method took Object instead of Woof,
+* then it would match the signature of the equals() method, and Object and Woof would be a functional interface.
+*/
+public class FIRespectCorrectlyMethodSignatureOfObject {
+    public interface Whistle extends Meow {
+        public abstract int hashCode();
+    }
+    public interface Bark {
+        public static int bark() { return 10; }
+    }
+    public interface Meow {
+        abstract String toString();
+        public int meow();
+        public default void purr() {}
+    }
+    public interface Howl {}
+    public interface Woof extends Bark {
+        abstract Long wolf();
+        public abstract boolean equals(Woof w);
+    }
+}
