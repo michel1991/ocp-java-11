2da347f32aee531bec69e2c3b5a6f77d0635d498
michel1991 <michelmvoumamvouma@gmail.com>
2022-10-16 21:40:23 +0200

    code: format local date time/zoned date time using all format style

---
+++ time/FormatDateWithDifferentFormatStyle.java
@@ -0,0 +1,38 @@
+package time;
+import java.time.format.*;
+import java.time.*;
+
+public class FormatDateWithDifferentFormatStyle {
+ static void formatWithShortStyle(){
+     LocalDateTime ldt = LocalDateTime.of(2020, 5, 10, 11, 22, 33);
+     var f = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT);
+     System.out.println("Short Format " + ldt.format(f));
+  }
+
+    static void formatWithMediumStyle(){
+     LocalDateTime ldt = LocalDateTime.of(2020, 5, 10, 11, 22, 33);
+      var f = DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM);
+      System.out.println("MEDIUM Format " + ldt.format(f));
+  }
+
+    static void formatWithLongStyleOnlyWithZonedId(){
+      var f = DateTimeFormatter.ofLocalizedTime(FormatStyle.LONG);
+      ZonedDateTime zoned = ZonedDateTime.now();
+
+      System.out.println("Long Format " + zoned.format(f));
+   }
+
+    static void formatWithFullStyleOnlyWithZonedId(){
+     ZonedDateTime zoned = ZonedDateTime.now();
+     var f = DateTimeFormatter.ofLocalizedTime(FormatStyle.FULL);
+     System.out.println("Long Format " + zoned.format(f));
+   }
+
+    public static void main(String... args){
+     formatWithShortStyle();
+     formatWithMediumStyle();
+     formatWithLongStyleOnlyWithZonedId();
+     formatWithFullStyleOnlyWithZonedId();
+   }
+
+}
