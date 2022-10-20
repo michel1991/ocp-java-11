966ec28b75e4edfe2fcce1242ccfee22b5ab7da1
michel1991 <michelmvoumamvouma@gmail.com>
2022-10-20 19:27:59 +0200

    code: list of file of directory

---
+++ nio/IterateListOfDirectory.java
@@ -0,0 +1,28 @@
+package nio;
+import java.util.stream.*;
+import java.nio.file.*;
+import java.io.*;
+
+/**
+* The code compiles, so option F is incorrect. The first path simplifies bear/polar with the path symbols removed,
+* which means Files.list() is executed on /mammals/bear/polar. The stream operation iterates over the files in the directory,
+* rather than the directory tree. For this reason, option A is correct. Note that calling toFile() on a Path instance returns
+* a java.io.File object, while toPath() reverses that operation. None of these map() operations, along with the call to normalize(),
+* impacts the result.
+ */
+public class IterateListOfDirectory {
+  public static void iterate() throws IOException{
+      var s = Path.of("bear/polar/./environment") .normalize().getParent();
+      System.out.println(Files.list(s)
+        .filter(p -> !Files.isDirectory(p))
+        .map(x -> x.toFile())
+        .map(w -> w.toPath())
+        .map(k -> k.normalize())
+        .collect(Collectors.toSet())
+        .size());
+  }
+
+    public static void main(String... args) throws IOException{
+         iterate();
+    }
+}
