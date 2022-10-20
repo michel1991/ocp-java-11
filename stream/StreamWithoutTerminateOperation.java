e0b96bfb08b1d07fd2ab920a7c8f85c66072205c
michel1991 <michelmvoumamvouma@gmail.com>
2022-10-20 20:28:08 +0200

    code: stream that contains list without terminate operation

---
+++ stream/StreamWithoutTerminateOperation.java
@@ -0,0 +1,30 @@
+package stream;
+import java.util.stream.*;
+import java.util.*;
+/**
+* What’s the terminal operation? There isn’t one. That means that the intermediate operations,
+* including both peek() statements,
+* never run. Options A and D are correct because no output is produced.
+ */
+public class StreamWithoutTerminateOperation {
+  public  static void listOfList(){
+      List<Integer> x1 = Arrays.asList();
+      List<Integer> x2 = Arrays.asList(1, 2, 3);
+      List<Integer> x3 = Arrays.asList(4, 5, 6);
+
+        Stream.of(x1, x2, x3)
+             .limit(2)
+             .peek(System.out::println) // peek 1
+             .flatMap(x -> x.stream())
+             .peek(System.out::println) // peek 2
+             .map(x -> x + 1)
+            // .collect(Collectors.toList())
+            // .forEach(System.out::println)
+      ;
+
+  }
+
+    public static void main(String... args){
+      listOfList();
+   }
+}
