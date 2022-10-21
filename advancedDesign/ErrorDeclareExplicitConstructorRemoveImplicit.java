8e05ba64dd34a3f4db3f84bada2f52bdccb6f897
michel1991 <michelmvoumamvouma@gmail.com>
2022-10-21 13:48:53 +0200

    code-error: declare explicit constructor remove implicit constructor, adding doc

--- advancedDesign/ErrorDeclareExplicitConstructorRemoveImplicit.java
+++ advancedDesign/ErrorDeclareExplicitConstructorRemoveImplicit.java
@@ -1,4 +1,13 @@
-package advancedDesign; 
+package advancedDesign;
+/**
+* The code doesn’t compile, so option A is incorrect.
+* The first compilation error is on line 2, as var cannot be used as a constructor argument type.
+* The second compilation error is on line 8. Since Rodent declares at least one constructor and it
+* is not a no-argument constructor, Beaver must declare a constructor with an explicit call to a super() constructor.
+* Line 9 contains two compilation errors. First, the return types are not covariant since Number is a supertype,
+*  not a subtype, of Integer. Second, the inherited method is static, but the overridden method is not,
+* making this an invalid override. The code contains four compilation errors, although they are limited to three lines, making option D the correct answer.
+ */
 class Rodent {
     public Rodent(var x) {}
     protected static Integer chew() throws Exception {
