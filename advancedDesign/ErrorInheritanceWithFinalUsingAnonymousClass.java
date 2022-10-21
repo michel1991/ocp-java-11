34b09023b2616c784ea61a114003a2e1fb3914bd
michel1991 <michelmvoumamvouma@gmail.com>
2022-10-21 19:19:31 +0200

    code-error: make inheritance with final class using anonymous class

---
+++ advancedDesign/ErrorInheritanceWithFinalUsingAnonymousClass.java
@@ -0,0 +1,17 @@
+final class Cook {
+    static class Vegetable {
+        float vitamins() { return 8f; }
+    }
+    final class Carrot extends Vegetable {
+        final float vitamins() { return 4; }
+    }
+
+}
+
+
+public class ErrorInheritanceWithFinalUsingAnonymousClass {
+    public final static void main(String[] unused) {
+        final Cook.Carrot c = new Cook().new Carrot() {};
+        System.out.println(c.vitamins());
+    }
+}
