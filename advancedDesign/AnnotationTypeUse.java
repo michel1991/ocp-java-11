b725f73179de8875573e35ce529db19d6f2dcaac
michel1991 <michelmvoumamvouma@gmail.com>
2022-10-28 11:00:12 +0200

    code: example that demonstrate how to use Annotation Target(ElementType.TYPE_USE)

---
+++ advancedDesign/AnnotationTypeUse.java
@@ -0,0 +1,28 @@
+package advancedDesign;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE_USE)
+@interface Valid {
+
+}
+
+class Tester {
+    void test() {
+        int age = (@Valid int) 1;
+    }
+}
+
+
+/**
+* Trick question! Annotations are applied to declarations, not values.
+* They can be applied to almost any type declaration in Java including classes,
+* interfaces, enums, methods, constructors, variables (member, static, and local),
+* parameters, cast operations, etc. They cannot be applied to a value or expression,
+* except as a cast operation. When used in a cast operation, a type is required such as int b = (@Valid int) 10.
+ */
+public class AnnotationTypeUse {
+   public static void main(String... args){
+       new Tester().test();
+   }
+}
