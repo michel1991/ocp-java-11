11bcd48207627af9f2d7abe5eb4273e0fdcfb659
michel1991 <michelmvoumamvouma@gmail.com>
2022-11-09 21:55:39 +0100

    code: adding correct explanation

--- list/ErrorFollowTypeReturnType.java
+++ list/ErrorFollowTypeReturnType.java
@@ -1,12 +1,16 @@
 package list;
 import java.util.*;
 
-/*
-* The primary reason to make any public method private is to improve encapsulation, making option B the correct answer.
-* Option A is a close second choice, as it is one of the reasons to use private interface methods, but that applies only
-* when adding a new method. If the method already exists, making it private does not reduce code duplication.
-* The rest of the options do not apply to private interface methods.
-*/
+
+
+/**
+ * The key to this question is keeping track of the types. Line 48 is a Map<Integer, Integer>.
+ * Line 49 builds a List out of a Set of Entry objects, giving us List<Entry<Integer, Integer>>.
+ * This causes a compile error on line 56 since we can’t multiply an Entry object by two.
+ * Lines 51 through 54 are all of type List<Integer>. The first three are immutable, and the one on line 54 is mutable.
+ * This means line 57 throws an UnsupportedOperationException since we attempt to modify the list.
+ * Line 58 would work if we could get to it. Since there is one compiler error and one runtime error, options A and E are correct.
+ */
 public class ErrorFollowTypeReturnType {
   public static void main(String... args){
       var map = Map.of(1,2, 3, 6);
