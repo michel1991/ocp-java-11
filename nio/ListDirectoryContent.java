9d5d67ea775e73ad3254f9775584fd3e2c5b3b14
michel1991 <michelmvoumamvouma@gmail.com>
2022-11-11 20:08:36 +0100

    code: recursive list directory

---
+++ nio/ListDirectoryContent.java
@@ -0,0 +1,34 @@
+package nio;
+import java.nio.file.*;
+import java.io.*;
+import java.util.stream.*;
+public class ListDirectoryContent {
+
+  /**
+  * The code compiles without issue, so options E and F are incorrect.
+  * The toRealPath() method will simplify the path to /animals and throw an exception if it does not exist, making option D correct.
+  * If the path does exist, calling getParent() on it returns the root directory.
+  * Walking the root directory with the filter expression will print all .java files in the root directory (along with all .java files in the directory tree),
+  * making option A correct. Option B is incorrect because it will skip files and directories that do not end in the .java extension.
+  * Option C is also incorrect as Files.walk() does not follow symbolic links by default.
+  * Only if the FOLLOW_LINKS option is provided and a cycle is encountered will the exception be thrown.
+   */
+  public static void printContent() throws IOException{
+      var rootPath = System.getProperty("user.dir");
+      //var x = Path.of( rootPath, "nio",  "/animals/fluffy/..");
+      var x = Path.of( rootPath, "nio",  "animals/fluffy/..");
+      //var x = Path.of( rootPath, "nio",  "animals", "fluffy", "..");
+      //System.out.println(" here "+ rootPath+"/nio/animals/fluffy/..");
+      //var x = Path.of(rootPath+"/nio/animals/fluffy/..");
+      //System.out.println(" parent " + x.toRealPath().getParent() + " " + x);
+      Files.walk(x.toRealPath().getParent()) // u1
+            .map(p -> p.toAbsolutePath().toString()) // u2
+            .filter(s -> s.endsWith(".txt")) // u3 or java
+            .collect(Collectors.toList())
+            .forEach(System.out::println);
+  }
+
+    public static void main(String... args) throws IOException{
+      printContent();
+   }
+}
