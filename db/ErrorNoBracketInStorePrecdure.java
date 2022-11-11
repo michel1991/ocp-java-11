1596368405dd6e68cffbf2333beedb1a4922501f
michel1991 <michelmvoumamvouma@gmail.com>
2022-11-11 16:07:02 +0100

    code-error: when call procedure, we must use bracket

---
+++ db/ErrorNoBracketInStorePrecdure.java
@@ -0,0 +1,29 @@
+package db;
+import java.sql.*;
+
+public class ErrorNoBracketInStorePrecdure {
+    /**
+    * Option A is one of the answers because you are supposed to use braces ({}) for all SQL in a CallableStatement.
+    * Option B is the other answer because each parameter should be passed with a question mark (?).
+    * The rest of the code is correct. Note that your database might not behave the way that’s described here,
+    * but you still need to know this syntax for the exam.
+     */
+   public static void badQuery() throws SQLException{
+       var sql = "call learn()";
+       try ( var conn = DriverManager.getConnection(
+               "jdbc:derby:zoo");
+                var cs = conn.prepareCall(sql)
+         ) {
+              cs.setString(1, "java");
+               try (var rs = cs.executeQuery()) {
+                    while (rs.next()) {
+                           System.out.println(rs.getString(3));
+                         }
+             }
+      }
+   }
+
+    public static void main(String... args){
+
+    }
+}
