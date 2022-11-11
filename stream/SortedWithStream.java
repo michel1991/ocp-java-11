28f7d8edd6a4d5b86ae35e348d3c069ed8731a29
michel1991 <michelmvoumamvouma@gmail.com>
2022-11-11 19:27:04 +0100

    code: sort list by desc adding explanation

--- stream/SortedWithStream.java
+++ stream/SortedWithStream.java
@@ -2,6 +2,12 @@ package stream;
 import java.util.stream.*;
 import java.util.*;
 public class SortedWithStream {
+    /**
+    * There is no Stream<T> method called compare() or compareTo(), so options A through D can be eliminated.
+    * The sorted() method is correct to use in a stream pipeline to return a sorted Stream.
+    * The collect() method can be used to turn the stream into a List.
+    * The collect() method requires a collector be selected, making option E incorrect and option F correct.
+     */
    public static List<String> sortByDesc(List<String> list){
        return list.stream()
        .sorted((a, b) -> b.compareTo(a))
