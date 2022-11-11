4602abfc3e6f9e756175414620e54e167d777607
michel1991 <michelmvoumamvouma@gmail.com>
2022-11-11 19:33:29 +0100

    code: remember return type of method for example delete method of path return void

---
+++ nio/ErrorRememberMethodReturnType.java
@@ -0,0 +1,20 @@
+package java.nio;
+import java.nio.file.*;
+import java.io.*;
+public class ErrorRememberMethodReturnType {
+    /**
+    * The return type of Files.delete() is void, which means this method will not compile regardless of what is placed in the blanks,
+    * making option G correct. The Files.deleteIfExists() method does return a boolean value, but that is not the method used on line 7.
+    * Independently, line 6 will compile if Stream<String> is used, while line 8 will compile if List<String> is used.
+     */
+    static void swim() throws IOException {
+        Path p = Path.of("shark.txt").toAbsolutePath();
+           var t1 = Files.lines(p); // --------------
+           var t2 = Files.delete(p); // -------------
+          var  t3 = Files.readAllLines(p); // ____________
+     }
+
+    public static void main(String... args){
+
+   }
+}
