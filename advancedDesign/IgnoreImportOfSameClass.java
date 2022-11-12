be9a9fc3bd8c8512d36c96594bcad070697edabe
michel1991 <michelmvoumamvouma@gmail.com>
2022-11-12 18:46:12 +0100

    code: java ignore import of the same class

---
+++ advancedDesign/IgnoreImportOfSameClass.java
@@ -0,0 +1,8 @@
+package advancedDesign;
+import advancedDesign.IgnoreImportOfSameClass;
+import static advancedDesign.IgnoreImportOfSameClass.*;
+public class IgnoreImportOfSameClass {
+  public static void main(String... args){
+    System.out.println("my name is michel");
+  }
+}
