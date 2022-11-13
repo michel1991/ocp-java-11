ee47c6458492266c67e8154090530cf7cdf5811f
michel1991 <michelmvoumamvouma@gmail.com>
2022-11-14 00:24:26 +0100

    code: adding priest name

--- modulars/preparation/choice/Applying.java
+++ modulars/preparation/choice/Applying.java
@@ -1,6 +1,10 @@
 package choice;
+import program.ChurchInTheMorning;
+import java.util.*;
 public class Applying {
    public static void main(String... args){
      System.out.println("entry points necessary to go to church");
+     var church = new ChurchInTheMorning();
+     System.out.println(Arrays.asList(church.priestName()));
    }
 }
