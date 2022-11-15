47d4d875b87a8112ad2efe82039891316ebec730
michel1991 <michelmvoumamvouma@gmail.com>
2022-11-15 01:28:13 +0100

    code-error: bad definition of procedure

---
+++ db/ErrorBadDefinitionOfPrecedure.java
@@ -0,0 +1,27 @@
+package db;
+import java.sql.*;
+
+public class ErrorBadDefinitionOfPrecedure {
+  /**
+  * Lines 17 and 19 do not compile because they are not allowed in a try-with-resources declaration.
+  * Only declarations that implement AutoCloseable or Closeable are permitted.
+  * Line 20 does not compile because execute() returns a boolean rather than a ResultSet.
+  * Finally, line 22 does not compile because the ResultSet method should be next(), not hasNext().
+  * Since there are four errors, option E is correct.
+   */
+  public static void execute(){
+      try (var sql = "{call learn (?)}"; // 17
+        var cs = conn.prepareCall(sql); // 18:
+         cs.setInt(1, 8); // 19:
+       var rs = cs.execute()) { //  20:
+
+          while (rs.hasNext()) { //  22:
+                    System.out.println( rs.getString(3)); // 23:
+                 } // 24:
+          }
+  }
+
+    public static void main(String... args){
+
+   }
+}
