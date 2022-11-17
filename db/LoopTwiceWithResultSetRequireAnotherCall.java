b79369ad7808e424d9ed55cb0575ac27149d490f
michel1991 <michelmvoumamvouma@gmail.com>
2022-11-17 18:41:04 +0100

    code-error: loop twice with the same result set require another execution of the query

---
+++ db/LoopTwiceWithResultSetRequireAnotherCall.java
@@ -0,0 +1,30 @@
+package db;
+import java.sql.*;
+
+/**
+*  Question: Suppose that the table counts has five rows with the numbers 1 to 5. How many lines does this code print?
+*
+* This code is tricky. While it does have two loops for the ResultSet, there is no call to the database in between.
+* Therefore, the second call to setInt() is unused, and the second while loop does not iterate through any data.
+* Only two lines are printed, making option C correct.
+ */
+public class LoopTwiceWithResultSetRequireAnotherCall {
+  public static void loop() throws SQLException{
+      var sql = "SELECT num FROM counts WHERE num > ?";
+      try (var conn = DriverManager.getConnection("jdbc:derby:zoo") ;
+            var ps = conn.prepareStatement(sql)
+      ) {
+          ps.setInt(1, 3);
+
+          try (var rs = ps.executeQuery()) {
+              while (rs.next())
+                  System.out.println(rs.getObject(1));
+
+              ps.setInt(1, 3);
+
+              while (rs.next())
+                  System.out.println(rs.getObject(1));
+          }
+      }
+  }
+}
