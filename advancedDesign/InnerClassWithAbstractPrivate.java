8d8f866436716c1ac4e73f4711da1e695940d711
michel1991 <michelmvoumamvouma@gmail.com>
2022-11-18 00:29:51 +0100

    code: inner class with incompatible modifier

---
+++ advancedDesign/InnerClassWithAbstractPrivate.java
@@ -0,0 +1,22 @@
+package advancedDesign;
+abstract class Elephant {
+    abstract static private class SleepsAlot {
+        abstract int sleep();
+    }
+
+    /* abstract static private class RunAlot extends SleepsAlot{
+        abstract int sleep();
+     }*/
+
+    private class EatAlot  extends SleepsAlot{
+         int sleep() {
+            return 0;
+        };
+    }
+}
+
+public class InnerClassWithAbstractPrivate {
+  public static void main(String... args){
+
+  }
+}
