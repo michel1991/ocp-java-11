5a7cc6d590629c0cd5c153c342573760a7e16f54
michel1991 <michelmvoumamvouma@gmail.com>
2022-11-19 01:51:49 +0100

    code: findFirst with serial stream and parallel stream return the same result

--- stream/Remember.java
+++ stream/Remember.java
@@ -1,5 +1,7 @@
 package stream;
 import java.util.stream.*;
+import java.util.*;
+import java.util.function.*;
 
 public class Remember {
     /**
@@ -18,10 +20,49 @@ public class Remember {
        System.out.println(resultEmptyStreamFindFirst);
    }
 
+    static void customPrintln(Object value){
+        System.out.println("\t "+ value + "\t");
+    }
+
+    /**
+    * The findFirst() method guarantees the first element in the stream will be returned, whether it is serial or parallel, making options A and D correct.
+    * While option B may consistently print 1 at runtime, the behavior of findAny() on a serial stream is not guaranteed, so option B is incorrect.
+    * Option C is likewise incorrect, with the output being random at runtime. Option E is incorrect because any of the previous options will allow the code to compile.
+     */
+    public static void findFirstWithParallelStream(){
+        System.out.println("begin find First with parallel stream and flat map ");
+        // Consumer<? super Integer> printResult =  (x) -> System.out.println("\t "+ x + "\t"); ;
+        Consumer<? super Number> printResult =  Remember::customPrintln ;
+        var data = List.of(List.of(1,2),
+                           List.of(3,4),
+                           List.of(5,6));
+                data.parallelStream() // p1
+                    .flatMap(s -> s.stream())
+                     .findFirst() // p2
+                .ifPresent(printResult); // System.out::print)
+                System.out.println("end find First with parallel stream and flat map");
+    }
+
+    public static void findFirstWithBasicStreamAndFlatMap(){
+        System.out.println("begin find First with basic stream and flat map ");
+        Consumer<? super Number> printResult =  Remember::customPrintln ;
+        var data = List.of(List.of(1,2),
+                           List.of(3,4),
+                           List.of(5,6));
+        data.stream() // p1
+        .flatMap(s -> s.stream())
+        .findFirst() // p2
+        .ifPresent(printResult); // System.out::println
+        System.out.println("end find First with basic stream and flat map");
+    }
+
 
 
     public static void main(String... args){
         findAnyAndFirst(Stream.empty(), Stream.empty());
         findAnyAndFirst(Stream.of(1), Stream.of(1));
+        System.out.println();
+        findFirstWithParallelStream();
+        findFirstWithBasicStreamAndFlatMap();
    }
 }
