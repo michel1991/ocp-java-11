add1572a9660724ec16a8979cedad834e8a7399b
michel1991 <michelmvoumamvouma@gmail.com>
2022-11-24 18:39:23 +0100

    refactor: move code about inheritance into correct directory

---
+++ inheritance/errors/ErrorAnnonymousClassWithInheritance.java
@@ -0,0 +1,24 @@
+package inheritance.error;
+
+/**
+* Trick question—the code does not compile! The Spirit class is marked final,
+* so it cannot be extended. The main() method uses an anonymous inner class that inherits from Spirit,
+* which is not allowed. If Spirit was not marked final, then options C and F would be correct.
+* Option A would print Booo!!!, while options B, D, and E would not compile for various reasons.
+ */
+public class ErrorAnnonymousClassWithInheritance {
+    static class Ghost {
+        public static void boo() {
+            System.out.println("Not scared");
+        }
+    protected final class Spirit {
+            public void boo() {
+                System.out.println("Booo!!!");
+            }
+        }
+    }
+
+    public static void main(String... haunt) {
+        var g = new Ghost().new Spirit() {};
+    }
+}
