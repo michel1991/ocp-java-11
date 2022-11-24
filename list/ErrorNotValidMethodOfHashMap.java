1e157bc58a8f7432d48ad3b5b48b9d043d6fb5f0
michel1991 <michelmvoumamvouma@gmail.com>
2022-11-24 18:20:19 +0100

    code-error: not available method contains on map

---
+++ list/ErrorNotValidMethodOfHashMap.java
@@ -0,0 +1,24 @@
+package list;
+import java.util.*;
+
+public interface ErrorNotValidMethodOfHashMap {
+    /**
+    * What is the result of the following code?
+    *
+      A. false
+      B. true
+      C. Compiler error on line 4
+      D. Compiler error on line 5
+      E. Compiler error on line 7
+
+      This question looks like it is about generics, but it’s not.
+      It is trying to see whether you noticed that Map does not have a contains() method.
+     It has containsKey() and containsValue() instead. If containsKey() was called, the answer would be false because 123 is an Integer key in the Map, rather than a String.
+     */
+   public static void invalidContainsMethod(){
+       Map m = new HashMap();
+       m.put(123, "456");
+       m.put("abc", "def");
+       System.out.println(m.contains("123"));
+   }
+}
