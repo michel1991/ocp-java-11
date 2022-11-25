cd9e6fc7b34b8e5da8afb37900d3d70fbecd882b
michel1991 <michelmvoumamvouma@gmail.com>
2022-11-25 19:36:13 +0100

    docs: add question/response be carefull to case during comparison

--- list/ListComparison.java
+++ list/ListComparison.java
@@ -4,11 +4,19 @@ import java.util.*;
 
 class MyComparator implements Comparator<String> {
     /**
+    *  What is the result of the following program?
+        A. Abb aab 123
+        B. aab Abb 123
+        C. 123 Abb aab
+        D. 123 aab Abb
+        E. The code does not compile.
+        F. A runtime exception is thrown.
+
     * The array is sorted using MyComparator, which sorts the elements in reverse alphabetical order in a case-insensitive fashion.
     * Normally, numbers sort before letters. This code reverses that by calling the compareTo() method on b instead of a.
      */
     public int compare(String a, String b) {
-        return b.toLowerCase().compareTo(a.toLowerCase());
+        return a.toLowerCase().compareTo(a.toLowerCase());
     }
 
     static String[] reverseAlphabetical(){
