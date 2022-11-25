17fcfa1b8532dc303a9210c2e1cb683a31b24589
michel1991 <michelmvoumamvouma@gmail.com>
2022-11-25 18:19:56 +0100

    fix: change type from interface to class

--- nio/ErrorRelativizeMethodRequireBothAbsolutePath.java
+++ nio/ErrorRelativizeMethodRequireBothAbsolutePath.java
@@ -17,7 +17,7 @@ import java.io.*;
    For this reason, option F is correct.
    Note that if the last line were changed to use the absolute path /zoo, the answer would be option A since the path from /zoo/elephant to /zoo is the .. path symbol.
  */
-public interface ErrorRelativizeMethodRequireBothAbsolutePath {
+public class ErrorRelativizeMethodRequireBothAbsolutePath {
    public static void relativizeMixPath() throws IOException{
        var rootPath = System.getProperty("user.dir");
        Path elephanPath = Path.of(rootPath, "nio", "resources", "zoo", "elephant.txt");
