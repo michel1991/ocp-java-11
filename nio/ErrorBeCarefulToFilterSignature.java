344c605db5e357f30f685c95e4fcfc819f815afc
michel1991 <michelmvoumamvouma@gmail.com>
2022-11-26 14:36:00 +0100

    docs: add  explanation of response

--- nio/ErrorBeCarefulToFilterSignature.java
+++ nio/ErrorBeCarefulToFilterSignature.java
@@ -5,6 +5,7 @@ import java.io.*;
 public class ErrorBeCarefulToFilterSignature {
 
     /**
+    * Assume that the directory /animals exists and is empty. What is the result of executing the following code?
     * The filter() operation applied to a Stream<Path> takes only one parameter, not two, so the code does not compile, and option C is correct.
     * If the code was rewritten to use the Files.
     * find() method with the BiPredicate as input (along with a maxDepth value), then the output would be option B, Has Sub, since the directory is given to be empty.
