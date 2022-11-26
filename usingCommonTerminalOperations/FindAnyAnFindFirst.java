1124eb9e085308ab87f04e9e5cc33878c89b1f5c
michel1991 <michelmvoumamvouma@gmail.com>
2022-11-26 07:26:14 +0100

    code: Using Common Terminal Operations (Title) add infinite operation with stream number

--- usingCommonTerminalOperations/FindAnyAnFindFirst.java
+++ usingCommonTerminalOperations/FindAnyAnFindFirst.java
@@ -3,12 +3,36 @@ import java.util.stream.*;
 import java.util.*;
 
 public class FindAnyAnFindFirst{
+    static void findAnyInfiniteStreamNumber(){
+        System.out.println("Beging findAnyInfiniteStreamNumber");
+        Stream
+              .iterate(1.0, (previousValue) -> previousValue + .5)
+              .findAny()
+              .ifPresent(System.out::println)
+        ;
+        System.out.println("end findAnyInfiniteStreamNumber");
+    }
+
+    static void findFirstInfiniteStreamNumber(){
+        System.out.println("Beging findFirstInfiniteStreamNumber");
+        Stream
+        .iterate(1.0, (previousValue) -> previousValue + .5)
+        .findFirst()
+        .ifPresent(System.out::println)
+        ;
+        System.out.println("end findFirstInfiniteStreamNumber");
+    }
+
   public static void main(String[] args){
      Stream<String> s = Stream.of("monkey", "gorilla", "bonobo");
      Stream<String> infinite = Stream.generate( () -> "chimp" );
 
       s.findAny().ifPresent(System.out::println); // monkey (usually)
+      System.out.println();
       infinite.findAny().ifPresent(System.out::println); // chimp
-   
+      System.out.println();
+      findAnyInfiniteStreamNumber();
+      System.out.println();
+      findFirstInfiniteStreamNumber();
   }
 }
