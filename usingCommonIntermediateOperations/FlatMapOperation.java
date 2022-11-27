9734b1e05c10f1d77cb794909b0e1396cb7e58b0
michel1991 <michelmvoumamvouma@gmail.com>
2022-11-27 09:54:48 +0100

    code: using Common Intermediate Operations Flat Map Operation(Title) using type with Function

--- usingCommonIntermediateOperations/FlatMapOperation.java
+++ usingCommonIntermediateOperations/FlatMapOperation.java
@@ -1,6 +1,7 @@
 package usingCommonIntermediateOperations;
 import java.util.stream.*;
 import java.util.*;
+import java.util.function.*;
 
 class FlatMapOperation{
   
@@ -10,13 +11,29 @@ class FlatMapOperation{
       var two = List.of("Mama Gorilla", "Baby Gorilla");
       Stream<List<String>> animals = Stream.of(zero, one, two);
 
-    
+
       animals.flatMap(m -> m.stream())
-            .forEach(System.out::println);  
+            .forEach(System.out::println);
+   }
+
+    static void basicExampleWithType(Function<List<String>, Stream<String>> forFlatMap){
+       List <String> zero = List.of();
+       var one = List.of("Bonobo");
+       var two = List.of("Mama Gorilla", "Baby Gorilla");
+       Stream<List<String>> animals = Stream.of(zero, one, two);
+
+       Function<List<String>, Stream<String>> reduceToOneElementPairStream =  forFlatMap;
+       animals
+       .flatMap(reduceToOneElementPairStream)
+       .forEach(System.out::println);
    }
 
   public static void main(String[] args){
       basicExample();
+      System.out.println();
+      basicExampleWithType(m -> m.stream());
+      System.out.println();
+      basicExampleWithType(List::stream);
   }
 
 }
