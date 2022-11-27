535d4d5eeb7638355ba0c20c9c6fe34fc9901771
michel1991 <michelmvoumamvouma@gmail.com>
2022-11-27 08:44:15 +0100

    refactor: move class into correct folder

--- advancedDesign/errors/FileInTheTrashException.java
+++ advancedDesign/errors/FileInTheTrashException.java
@@ -1,4 +1,4 @@
-package  advancedDesign;
+package  advancedDesign.errors;
 import java.io.*;
 /**
 * How many lines of the following program contain compiler errors?
@@ -16,12 +16,12 @@ import java.io.*;
    This is not compatible with the constructor calls on lines 4 and 8, so they do not compile.
    Since the code contains three lines that do not compile, option C is the correct answer.
  */
-public class ErrorFileInTheTrashException extends IOException { // 2
-    public void close() throws ErrorFileInTheTrashException { //  3:
-        throw new ErrorFileInTheTrashException("Unable to close"); //  4:
+public class FileInTheTrashException extends IOException { // 2
+    public void close() throws FileInTheTrashException { //  3:
+        throw new FileInTheTrashException("Unable to close"); //  4:
     } //  5:
     public static void main(String[] args) throws Exception { //   6:
-        try (var f = new ErrorFileInTheTrashException()) { // 7:
-            throw new ErrorFileInTheTrashException("Thrown away"); //  8:
+        try (var f = new FileInTheTrashException()) { // 7:
+            throw new FileInTheTrashException("Thrown away"); //  8:
         } } //  9:
 }
