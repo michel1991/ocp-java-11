e884289b19636f08f7cbf114ac181faf179dd1e8
michel1991 <michelmvoumamvouma@gmail.com>
2022-11-30 18:27:34 +0100

    docs: add explanation

--- internationalization/LoadCorrectDolphins.java
+++ internationalization/LoadCorrectDolphins.java
@@ -2,18 +2,38 @@ package internationalization;
 import java.util.*;
 
 /**
-* Java will use Dolphins_fr.properties as the matching resource bundle on line 7 because it is an exact match on the language of the requested locale.
-* Line 8 finds a matching key in this file. Line 9 does not find a match in that file; therefore, it has to look higher up in the hierarchy.
-* Once a bundle is chosen, only resources in that hierarchy are allowed.
-* It cannot use the default locale anymore, but it can use the default resource bundle specified by Dolphins.properties.
+*
+    Suppose that we have the following property files and code. Which bundle is used on lines 8 and 9, respectively?
+    Dolphins.properties
+    name=The Dolphin
+    age=0
+
+    Dolphins_de.properties
+    name=Dolly
+    age=4
+
+    Dolphins_en.properties
+    name=Dolly
+
+    A. Dolphins.properties and Dolphins.properties are used.
+    B. Dolphins.properties and Dolphins_en.properties are used.
+    C. Dolphins_en.properties and Dolphins.properties are used.
+    D. Dolphins_en.properties and Dolphins_en.properties are used.
+    E. Dolphins_de.properties and Dolphins_en.properties are used.
+    F. The code does not compile.
+
+    Java will use Dolphins_fr.properties as the matching resource bundle on line 7 because it is an exact match on the language of the requested locale.
+    Line 8 finds a matching key in this file. Line 9 does not find a match in that file; therefore, it has to look higher up in the hierarchy.
+    Once a bundle is chosen, only resources in that hierarchy are allowed.
+    It cannot use the default locale anymore, but it can use the default resource bundle specified by Dolphins.properties.
  */
 public class LoadCorrectDolphins {
   public static void main(String... args){
-      var fr = new Locale("fr");
-      Locale.setDefault(new Locale("en", "US"));
-      var b = ResourceBundle.getBundle("internationalization.Dolphins", fr);
-      b.getString("name");
-      b.getString("age");
+      var fr = new Locale("fr"); // 5
+      Locale.setDefault(new Locale("en", "US")); // 6
+      var b = ResourceBundle.getBundle("internationalization.Dolphins", fr);  // 7
+      b.getString("name");  // 8
+      b.getString("age");   // 9
       System.out.println(" name : " +   b.getString("name") + " age : " +  b.getString("age"));
   }
 }
