f4bd5d6e83d5a612be022621d85cb6bf09c9bfd1
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-01 13:25:55 +0100

    code: using functional interface with type

--- workingWithPrimitiveStreams/BasicExamplePrimitiveStream.java
+++ workingWithPrimitiveStreams/BasicExamplePrimitiveStream.java
@@ -1,6 +1,7 @@
 package workingWithPrimitiveStreams;
 import java.util.stream.*;
 import java.util.*;
+import java.util.function.*;
 
 class BasicExamplePrimitiveStream{
     static void sumWithMapToInt(){
@@ -11,7 +12,23 @@ class BasicExamplePrimitiveStream{
 
        System.out.println(" sum with mapToInt " + result);
     }
-    
+
+    /**
+     * Convert stream object into IntStream primitive
+     */
+    static void sumWithMapToIntWithType(){
+        System.out.println("Begin sum with mapToInt  begin ");
+        Stream<Integer> stream = Stream.of(1, 2, 3);
+        ToIntFunction<Integer> convertToInt = Integer::intValue;
+        var result = stream
+                           .mapToInt(convertToInt)
+                            .sum()
+        ;
+
+        System.out.println(" sum with mapToInt " + result);
+        System.out.println("End sum with mapToInt  begin ");
+    }
+
 
    static void average(){
      IntStream intStream = IntStream.of(1, 2, 3);
@@ -22,6 +39,9 @@ class BasicExamplePrimitiveStream{
    public static void main(String[] args){
        System.out.println("InStream apply sum with reduce from mapToInt stream");
        sumWithMapToInt();
+       System.out.println();
+       sumWithMapToIntWithType();
+       System.out.println();
         average(); 
    }
 }
