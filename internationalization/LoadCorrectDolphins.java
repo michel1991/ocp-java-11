26772ff1fb4acbf83d1896a0da3cbb3b898ca34d
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-01 21:18:03 +0100

    code: another example of which bundle property to load

--- internationalization/LoadCorrectDolphins.java
+++ internationalization/LoadCorrectDolphins.java
@@ -79,6 +79,27 @@ public class LoadCorrectDolphins {
         System.out.println("end Second example");
     }
 
+    /**
+     * Assume that all of the files mentioned in the answer choices exist and define the same keys. Which one will be used to find the key in line 8?
+        A. Dolphins.properties
+        B. Dolphins_US.properties
+        C. Dolphins_en.properties
+        D. Whales.properties
+        E. Whales_en_US.properties
+        F. The code does not compile.
+
+      Java will first look for the most specific matches it can find, starting with Dolphins_en_ US.properties.
+      Since that is not an answer choice, it drops the country and looks for Dolphins_en.properties, making option C correct.
+      Option B is incorrect because a country without a language is not a valid locale.
+
+     */
+    static void thirdExample(){
+        Locale.setDefault(new Locale("en", "US")); // 6:
+        var b = ResourceBundle.getBundle("Dolphins"); //   7:
+        System.out.println(b.getString("name")); //  8:
+
+    }
+
   public static void main(String... args){
         firstExample();
         System.out.println();
