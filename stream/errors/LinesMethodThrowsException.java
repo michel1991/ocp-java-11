79938f1c3880eaa604880182d3baa1e577749b4b
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-02 08:27:42 +0100

    docs: adding explanation

--- stream/errors/LinesMethodThrowsException.java
+++ stream/errors/LinesMethodThrowsException.java
@@ -6,10 +6,18 @@ public class LinesMethodThrowsException {
     /**
     * Assuming the file /giraffe/sleep.csv exists with the specified contents, what is the expected output of calling wakeUp() on it?
     *
-    * The code does not compile because Files.lines() declares IOException, which is not handled or declared by the wakeUp() method.
-    * For this reason, option F is correct. If the method was corrected to declare IOException, then the method would print 111 at runtime.
-    * The method reads the lines of the file, splits the values by commas, and then converts the stream of resulting string values to a stream of numbers using the length() method.
-    *  Finally, it combines the results into a List and prints each one.
+         A. It prints 2,5,9.
+        B. It prints 259.
+        C. It prints 111.
+        D. It does not print anything.
+        E. An exception is thrown at runtime.
+        F. None of the above, as the code does not compile.
+
+
+      The code does not compile because Files.lines() declares IOException, which is not handled or declared by the wakeUp() method.
+      For this reason, option F is correct. If the method was corrected to declare IOException, then the method would print 111 at runtime.
+      The method reads the lines of the file, splits the values by commas, and then converts the stream of resulting string values to a stream of numbers using the length() method.
+      Finally, it combines the results into a List and prints each one.
      */
     static void wakeUp(Path path) {
         Files.lines(path)
