cc41206285cd151f932be01e65f4a37a2a23cced
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-03 11:09:47 +0100

    code: add entry point

--- workingWithAdvancedStreamPipelineConcepts/CheckedExceptionsAndFunctionalInterfaces/UsingWrapperCatchException.java
+++ workingWithAdvancedStreamPipelineConcepts/CheckedExceptionsAndFunctionalInterfaces/UsingWrapperCatchException.java
@@ -3,8 +3,13 @@ import java.util.*;
 import java.util.function.*;
 
 class UsingWrapperCatchException{
-   public void wrapped(){
-      Supplier<List<String>> s2 = ExceptionCaseStudy::createSafe;
+   public static void wrapped(){
+       Supplier<List<String>> s2 = CreateSafeMethodCatchException::createSafe;
+   }
+
+    public static void main(String... args){
+       System.out.println("in main using wrapper catch exception ");
+       wrapped();
    }
 
 }
