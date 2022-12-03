b9fcec0ad78c5d3d90e14e8b31e7439b3eb7d8fe
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-03 11:59:06 +0100

    refactor: move the method allow to convert to Set using collectors

--- collectingUsingBasicCollectors/BasicExample.java
+++ collectingUsingBasicCollectors/BasicExample.java
@@ -6,15 +6,6 @@ import java.util.function.*;
 class BasicExample{
    
 
-  static void fromStreamToCollectionFilterAllStringStartsByT() {
-    var ohMy = Stream.of("lions", "tiggers", "bears");
-    TreeSet<String> result = ohMy.filter( s -> s.startsWith("t") )
-                                 .collect(Collectors.toCollection(TreeSet::new))
-     ;
-    System.out.println("from stream to Collection filter all string start by t " + result);
-  }
-
-
   static void basicCollectingIntoMap(){
     var ohMy = Stream.of("lions", "tiggers", "bears");
      Map<String,  Integer> map = ohMy.collect( Collectors.toMap(s-> s, String::length) );
@@ -30,8 +21,6 @@ class BasicExample{
   }  
  
   public static void main(String[] args){
-     fromStreamToCollectionFilterAllStringStartsByT();
-      System.out.println();
      basicCollectingIntoMap();
     
      System.out.println(); 
---
+++ collectingUsingBasicCollectors/ConvertToSetCollectors.java
@@ -0,0 +1,37 @@
+package collectingUsingBasicCollectors;
+import java.util.stream.*;
+import java.util.*;
+import java.util.function.*;
+
+public class ConvertToSetCollectors {
+
+    static void fromStreamToCollectionFilterAllStringStartsByT() {
+        System.out.println("Begin from stream to Collection filter all string start by t  without type" );
+        var ohMy = Stream.of("lions", "tiggers", "bears");
+        TreeSet<String> result = ohMy
+                                    .filter( s -> s.startsWith("t") )
+                                    .collect(Collectors.toCollection(TreeSet::new))
+        ;
+        System.out.println("\t from stream to Collection filter all string start by t " + result);
+        System.out.println("End from stream to Collection filter all string start by t  without type" );
+    }
+
+    static void fromStreamToCollectionFilterAllStringStartsByTWithType() {
+        System.out.println("Begin from stream to Collection filter all string start by t  with type" );
+        var ohMy = Stream.of("lions", "tiggers", "bears");
+        Predicate<String> filterPredicate =   s -> s.startsWith("t") ;
+        Supplier<TreeSet<String>> supplier = TreeSet::new;
+        TreeSet<String> result = ohMy
+                                    .filter(filterPredicate )
+                                    .collect(Collectors.toCollection(supplier))
+        ;
+        System.out.println("\t from stream to Collection filter all string start by t " + result);
+        System.out.println("End from stream to Collection filter all string start by t  with type" );
+    }
+
+    public static void main(String[] args){
+        fromStreamToCollectionFilterAllStringStartsByT();
+          System.out.println();
+        fromStreamToCollectionFilterAllStringStartsByTWithType();
+    }
+}
