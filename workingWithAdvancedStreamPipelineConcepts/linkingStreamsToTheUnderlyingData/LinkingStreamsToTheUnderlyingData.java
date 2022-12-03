03ae2a5c4871b250c0ddfdf17fdb3d8396578700
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-03 09:22:35 +0100

    docs: add explanation

--- workingWithAdvancedStreamPipelineConcepts/linkingStreamsToTheUnderlyingData/LinkingStreamsToTheUnderlyingData.java
+++ workingWithAdvancedStreamPipelineConcepts/linkingStreamsToTheUnderlyingData/LinkingStreamsToTheUnderlyingData.java
@@ -4,11 +4,15 @@ import java.util.*;
 
 class LinkingStreamsToTheUnderlyingData{
 
+    /**
+    * N'oubliez pas que les flux sont évalués paresseusement.
+    * Cela signifie que le flux n'est pas réellement créé à la ligne 28. Un objet est créé qui sait où rechercher les données lorsque cela est nécessaire.
+     */
   static void basicExample(){
      var cats = new ArrayList<String>();
      cats.add("Annie");
      cats.add("Ripley");
-     var stream = cats.stream();
+     var stream = cats.stream(); // ligne 28
      cats.add("KC");
      System.out.println("count " + stream.count());
   }
