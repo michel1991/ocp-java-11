c26975125380e2eaa68dff5359c91df4211ccea1
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-03 10:41:09 +0100

    docs : add explanation

--- workingWithAdvancedStreamPipelineConcepts/CheckedExceptionsAndFunctionalInterfaces/BadDeclarationCheckedException.java
+++ workingWithAdvancedStreamPipelineConcepts/CheckedExceptionsAndFunctionalInterfaces/BadDeclarationCheckedException.java
@@ -7,6 +7,7 @@ class BadDeclarationCheckedException{
   
     /**
      * Supplier not allow checked exception more generally functional interface
+     * When using method reference
      */
   public void bad() throws IOException{
       Supplier<List<String>> s =  CreateMethodExceptionCaseStudy::create; // does not compile
