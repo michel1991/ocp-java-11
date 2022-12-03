9e434b531e820eb87f648c3b8f3a32deb6d7f742
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-03 20:11:12 +0100

    docs: add documentation

--- db/ErrorBadDefinitionOfPrecedure.java
+++ db/ErrorBadDefinitionOfPrecedure.java
@@ -3,11 +3,20 @@ import java.sql.*;
 
 public class ErrorBadDefinitionOfPrecedure {
   /**
-  * Lines 17 and 19 do not compile because they are not allowed in a try-with-resources declaration.
-  * Only declarations that implement AutoCloseable or Closeable are permitted.
-  * Line 20 does not compile because execute() returns a boolean rather than a ResultSet.
-  * Finally, line 22 does not compile because the ResultSet method should be next(), not hasNext().
-  * Since there are four errors, option E is correct.
+  *  Suppose learn() is a stored procedure that takes one IN parameter. What is wrong with the following code? (Choose all that apply.)
+      A. Line 18 is missing braces.
+      B. Line 18 is missing a ?.
+      C. Line 19 is not allowed to use var.
+      D. Line 20 does not compile.
+      E. Line 22 does not compile.
+      F. Something else is wrong with the code.
+      G. None of the above. This code is correct.
+      
+      Lines 17 and 19 do not compile because they are not allowed in a try-with-resources declaration.
+      Only declarations that implement AutoCloseable or Closeable are permitted.
+      Line 20 does not compile because execute() returns a boolean rather than a ResultSet.
+      Finally, line 22 does not compile because the ResultSet method should be next(), not hasNext().
+      Since there are four errors, option E is correct.
    */
   public static void execute(){
       try (var sql = "{call learn (?)}"; // 17
