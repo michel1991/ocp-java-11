5e17df63f8dee85044c2bf99e4f3aa5c672c193b
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-04 07:25:36 +0100

    code: groupingBy change type of list that hold value and type of map we want

---
+++ collectingUsingBasicCollectors/GroupingByChangeTypeOfListThatHoldValue.java
@@ -0,0 +1,60 @@
+package collectingUsingBasicCollectors;
+import java.util.stream.*;
+import java.util.*;
+import java.util.function.*;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.mapping;
+import static java.util.stream.Collectors.minBy;
+
+public class GroupingByChangeTypeOfListThatHoldValue {
+    /**
+    * Le collecteur groupingBy() (The groupingBy() collector) indique à collect() qu'il doit regrouper tous les éléments du flux dans une Map.
+    * La fonction détermine les clés de la Map. Chaque valeur de la Map est une liste de toutes les entrées qui correspondent à cette clé.
+    * By default It is List that hold value
+    */
+    static void makeGroupingByChangeTypeOfListThatHoldValue(){
+        var ohMy = Stream.of("lions", "tigers", "bears");
+        Function<String, Integer> howToDefineKey = String::length;
+        // Collector<?, ?,  Map> theCollectors = Collectors.toSet();
+        Map<Integer, Set<String>> map =  ohMy
+                    .collect(
+                                groupingBy(
+                                        howToDefineKey,
+                                        Collectors.toSet() // // type of list that hold value
+                                )
+
+        );
+        System.out.println("Begin from stream to Map by specify the type of list that hold value" );
+        System.out.println("\t create string  without specify which map " + map);
+        System.out.println("\t " + map.getClass());
+        System.out.println("End from stream to Map the type of list that hold value" );
+    }
+
+    static void makeGroupingByChangeTypeOfListThatHoldValueAndTypeOfMapReturning(){
+        var ohMy = Stream.of("lions", "tigers", "bears");
+        Function<String, Integer> howToDefineKey = String::length;
+        Supplier<TreeMap<Integer, Set<String>>> supplierTypeOfMapToReturn =  TreeMap::new;
+
+        TreeMap<Integer, Set<String>> map =  ohMy
+                            .collect(
+                                    groupingBy(howToDefineKey,
+                                               supplierTypeOfMapToReturn,
+                                               Collectors.toSet() // type of list that hold value
+                                    )
+
+                            );
+        System.out.println("Begin from stream to Map by specify the type of list that hold value and type of map to return " );
+        System.out.println("\t create string  without specify which map " + map);
+        System.out.println("\t " + map.getClass());
+        System.out.println("End from stream to Map the type of list that hold value and type of map to return " );
+    }
+
+
+
+    public static void main(String... args){
+        makeGroupingByChangeTypeOfListThatHoldValue();
+        System.out.println();
+        makeGroupingByChangeTypeOfListThatHoldValueAndTypeOfMapReturning();
+    }
+
+}
