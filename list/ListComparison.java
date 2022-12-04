e7f803f8b0e5a8fe3b0f5597b5b7f84de6de6623
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-05 00:56:21 +0100

    docs: add explanation

--- list/ListComparison.java
+++ list/ListComparison.java
@@ -28,8 +28,15 @@ class MyComparator implements Comparator<String> {
 
 class ListComparison{
     /**
-    * This code has a double negative. The code c2.compareTo(c1) sorts in descending order. However,
-    * the negative sign before it switches to ascending order. Since uppercase sorts before lowercase, option B is correct.
+    * What is the output of the following code?
+        A. [leo, Olivia]
+        B. [Olivia, leo]
+        C. The code does not compile because of line X.
+        D. The code does not compile for another reason.
+        E. A runtime exception is thrown.
+        
+        This code has a double negative. The code c2.compareTo(c1) sorts in descending order. However,
+        the negative sign before it switches to ascending order. Since uppercase sorts before lowercase, option B is correct.
      */
    public static void ascendingUpperCaseFirst(){
       List<String> cats = new ArrayList<>();
