3597fee18f5e9bf5a6cb48c46b202b0530a78c29
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-04 08:32:53 +0100

    refactor: remove unused method

--- collectingUsingBasicCollectors/GroupingPartitioningMapping.java
+++ collectingUsingBasicCollectors/GroupingPartitioningMapping.java
@@ -3,53 +3,6 @@ import java.util.stream.*;
 import java.util.*;
 
 class GroupingPartitioningMapping{
-  static void groupByLength(){
-     var ohMy = Stream.of("lions", "tigers", "bears");
-    Map<Integer, List<String>> map = ohMy.collect(
-           Collectors.groupingBy(String::length)
-    );
-   System.out.println("group by length using List to hold data:  " + map);
-  }
-
-   static void groupByLengthUsingSetThatHoldValue(){
-     var ohMy = Stream.of("lions", "tigers", "bears");
-    Map<Integer, Set<String>> map = ohMy.collect(
-           Collectors.groupingBy(
-               String::length,
-               Collectors.toSet()
-
-          )
-    );
-   System.out.println("group by length  using Set to hold data: " + map + " class is " + map.getClass());
-  }
-
-
-   static void groupByLengthUsingTreeMapThatHoldValue(){
-     var ohMy = Stream.of("lions", "tigers", "bears");
-    TreeMap<Integer, Set<String>> map = ohMy.collect(
-           Collectors.groupingBy(
-               String::length,
-               TreeMap::new,
-               Collectors.toSet()
-
-          )
-    );
-   System.out.println("group by length  using TreeMap to hold data: " + map + " class is " + map.getClass());
-  }
-
-  static void groupByLengthPreserveTypeThatHoldValue(){
-     var ohMy = Stream.of("lions", "tigers", "bears");
-    TreeMap<Integer, List<String>> map = ohMy.collect(
-           Collectors.groupingBy(
-               String::length,
-               TreeMap::new,
-               Collectors.toList()
-
-          )
-    );
-   System.out.println("group by length preserve type that  hold data: " + map + " class is " + map.getClass());
-  }
-
   static void usingAnyCollectors(){
      var ohMy = Stream.of("lions", "tigers", "bears");
     Map<Integer, Long> map = ohMy.collect(
@@ -64,10 +17,7 @@ class GroupingPartitioningMapping{
 
 
   public static void main(String[] args){
-     groupByLength();
-     groupByLengthUsingSetThatHoldValue();
-     groupByLengthUsingTreeMapThatHoldValue();
-     groupByLengthPreserveTypeThatHoldValue();
+
      usingAnyCollectors();
   }
   
