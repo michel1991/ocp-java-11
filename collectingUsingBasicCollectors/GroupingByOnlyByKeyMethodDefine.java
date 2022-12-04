1024dae8c4f2cd1f4dc302c932983ed28a1e4526
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-04 06:39:56 +0100

    docs: add explanation

--- collectingUsingBasicCollectors/GroupingByOnlyByKeyMethodDefine.java
+++ collectingUsingBasicCollectors/GroupingByOnlyByKeyMethodDefine.java
@@ -10,6 +10,7 @@ public class GroupingByOnlyByKeyMethodDefine {
     /**
     * Le collecteur groupingBy() (The groupingBy() collector) indique à collect() qu'il doit regrouper tous les éléments du flux dans une Map.
     * La fonction détermine les clés de la Map. Chaque valeur de la Map est une liste de toutes les entrées qui correspondent à cette clé.
+    * By default It is List that hold value
      */
    static void specifyOnlyHowToGetKey(){
        var ohMy = Stream.of("lions", "tigers", "bears");
