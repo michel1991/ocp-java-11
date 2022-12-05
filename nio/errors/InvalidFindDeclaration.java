b19b41fc5af780f05f6dd3ad43d74876d3a0b174
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-05 23:45:31 +0100

    docs: add explanation

--- nio/errors/InvalidFindDeclaration.java
+++ nio/errors/InvalidFindDeclaration.java
@@ -2,16 +2,23 @@ package nio.errors;
 import java.nio.file.*;
 import java.io.*;
 
-/**
-* Assume the Path value sent to the NIO.2 Files.find() method exists as a directory.
-*  Which statements about the following code snippet are correct? (Choose all that apply.)
- */
 
 /**
-* This code does not compile for a number of reasons. First, the Files.find() method takes three arguments.
-* Without a search depth parameter, line n2 does not compile. Next, toRealPath() declares IOException, which is not handled by the lambda expression,
-* so line n3 does not compile. Finally, the result of the operation on line n3 creates a Stream<String>, not Stream<Path>, so the lambda expression
-* on line n4 does not compile.
+    Assume the Path value sent to the NIO.2 Files.find() method exists as a directory.
+    Which statements about the following code snippet are correct? (Choose all that apply.)
+
+    A. A list of files is printed.
+    B. A list of directories is printed.
+    C. An exception is printed at runtime.
+    D. Line n1 contains a compiler error.
+    E. Line n2 contains a compiler error.
+    F. Line n3 contains a compiler error.
+    G. Line n4 contains a compiler error.
+
+    This code does not compile for a number of reasons. First, the Files.find() method takes three arguments.
+    Without a search depth parameter, line n2 does not compile.
+    Next, toRealPath() declares IOException, which is not handled by the lambda expression, so line n3 does not compile.
+    Finally, the result of the operation on line n3 creates a Stream<String>, not Stream<Path>, so the lambda expression on line n4 does not compile.
  */
 public interface InvalidFindDeclaration {
     public static find(){
