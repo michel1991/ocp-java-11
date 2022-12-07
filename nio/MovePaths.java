e3c8a9da20d777ab0318f4eb697340333fcceaec
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-07 08:53:35 +0100

    code: add another method for move

--- nio/MovePaths.java
+++ nio/MovePaths.java
@@ -22,6 +22,27 @@ public class MovePaths {
         Files.move(p1, p2, StandardCopyOption.ATOMIC_MOVE);
     }
 
+    /**
+    *  Assume monkey.txt is a file that exists in the current working directory. Which statements about the following code snippet are correct? (Choose all that apply.)
+        A. If /animals/monkey.txt exists, then it will be overwritten at runtime.
+        B. If /animals exists as an empty directory, then /animals/monkey.txt will be the new location of the file.
+        C. If monkey.txt is a symbolic link, then the file it points to will be moved at runtime.
+        D. If the move is successful and another process is monitoring the file system, then it will not see an incomplete file at runtime.
+        E. The code will always throw an exception at runtime.
+        F. None of the above
+
+      The target path of the file after the move() operation is /animals, not /animals/monkey.txt, so options A and B are both incorrect.
+      Option B will actually throw an exception at runtime since /animals already exists and is a directory.
+      Next, the NOFOLLOW_LINKS option means that if the source is a symbolic link, the link itself and not the target will be copied at runtime, so option C is also incorrect.
+      The option ATOMIC_MOVE means that any process monitoring the file system will not see an incomplete file during the move, so option D is correct.
+      Option E is incorrect, since there are circumstances in which the operation would be allowed.
+      In particular, if /animals did not exist then the operation would complete successfully.
+
+     */
+    static void movePMonkeyToAnimals() throws Exception {
+        Files.move(Path.of("monkey.txt"), Paths.get("/animals"),StandardCopyOption.ATOMIC_MOVE, LinkOption.NOFOLLOW_LINKS);
+    }
+
     public static void main(String... args){
 
     }
