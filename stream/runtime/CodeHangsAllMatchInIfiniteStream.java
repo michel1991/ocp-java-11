6a70eb9403961d7602a31422be3ae55e9c465994
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-07 08:05:16 +0100

    code: add code hang with allMatch

--- stream/runtime/CodeHangsAllMatchInIfiniteStream.java
+++ stream/runtime/CodeHangsAllMatchInIfiniteStream.java
@@ -30,4 +30,26 @@ public class CodeHangsAllMatchInIfiniteStream {
        var b2 = stream2.allMatch(predicate);
        System.out.println(b1 + " " + b2);
    }
+
+    /**
+    *  Which of the following can fill in the blank so that the code prints out false?
+        A. allMatch
+        B. anyMatch
+        C. findAny
+        D. findFirst
+        E. noneMatch
+        F. None of the above
+
+      Options C and D do not compile because these methods do not take a Predicate parameter and do not return a boolean.
+      When working with streams, it is important to remember the behavior of the underlying functional interfaces.
+      Options B and E are incorrect. While the code compiles, it runs infinitely.
+      The stream has no way to know that a match won’t show up later.
+      Option A is correct because it is safe to return false as soon as one element passes through the stream that doesn’t match.
+
+     */
+    static void allMatchMeow(){
+        var s = Stream.generate(() -> "meow");
+        var match = s. allMatch(String::isEmpty); // .________________(
+        System.out.println(match);
+    }
 }
