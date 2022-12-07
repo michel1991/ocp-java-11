955109636ff76050c620e0ce16a8261d92e086bf
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-07 18:38:53 +0100

    explanation: add explanation

--- io/ClosingDefaultOutputStreamWithWrapping.java
+++ io/ClosingDefaultOutputStreamWithWrapping.java
@@ -2,9 +2,18 @@ package io;
 import java.io.*;
 
 /**
-* The variable w refers to System.out, which is closed at the end of the try-with-resources statement, but before println() is called.
-* Since PrintStream instances do not throw exceptions when closed, nothing is printed at runtime, and option D is correct.
-* If the stream were open, then it would print LX.
+    Assume in is a valid stream whose next bytes are LYNX. What is the result of calling the following method on the stream?
+    A. It prints L.
+    B. It prints LN.
+    C. It prints LX.
+    D. It does not print anything.
+    E. An exception is thrown.
+    F. The answer cannot be determined until runtime.
+    G. None of the above, as the method does not compile.
+
+    The variable w refers to System.out, which is closed at the end of the try-with-resources statement, but before println() is called.
+    Since PrintStream instances do not throw exceptions when closed, nothing is printed at runtime, and option D is correct.
+    If the stream were open, then it would print LX.
  */
 public class ClosingDefaultOutputStreamWithWrapping {
     public static void printData(InputStream in) throws IOException {
