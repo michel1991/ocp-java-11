3ae613592535ad256787f727f285d272ae6d8dd0
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-07 08:09:26 +0100

    code: choice correct variable to use in lambda

--- lambda/ChoiceCorrectEffectiveVariable.java
+++ lambda/ChoiceCorrectEffectiveVariable.java
@@ -24,6 +24,28 @@ public class ChoiceCorrectEffectiveVariable {
         chars = null;
     }
 
+    /**
+    *  Which of the following can be inserted without causing a compilation error? (Choose all that apply.)
+        A. char start = 'a';
+        B. char c = 'x';
+        C. chars = null;
+        D. end = '1';
+        E. None of the above
+
+       Lambdas are not allowed to redeclare local variables, making options A and B incorrect.
+       Option D is incorrect because setting end prevents it from being effectively final.
+       Lambdas are only allowed to reference effectively final variables.
+       Option C is tricky because it does compile but throws an exception and runtime.
+       Since the question only asks about compilation, option C is correct.
+     */
+    public void removeBefore(List<Character> chars) {
+        char end = 'z';
+        // INSERT LINE HERE
+        chars = null;
+        chars.removeIf(c -> {
+            char start = 'a'; return start <= c && c <= end; });
+    }
+
     public static void main(String... args){
         remove(new ArrayList<Character>());
     }
