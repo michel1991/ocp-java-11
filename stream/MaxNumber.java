9380ead89bdcf57b285fc87ecaa46bc7d0f39849
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-08 21:31:37 +0100

    docs: add explanation

--- stream/MaxNumber.java
+++ stream/MaxNumber.java
@@ -3,6 +3,22 @@ import java.util.stream.*;
 import java.util.*;
 
 public class MaxNumber{
+    /**
+    *  Which of the following are true? (Choose all that apply.)
+    *   A. magic(Stream.empty()); runs infinitely.
+        B. magic(Stream.empty()); throws an exception.
+        C. magic(Stream.iterate(1, x -> x++)); runs infinitely.
+        D. magic(Stream.iterate(1, x -> x++)); throws an exception.
+        E. magic(Stream.of(5, 10)); runs infinitely.
+        F. magic(Stream.of(5, 10)); throws an exception.
+        G. The method does not compile.
+
+      Calling get() on an empty Optional causes an exception to be thrown, making option B correct.
+      Option F is also correct because filter() makes the Optional empty before it calls get().
+      Option C is incorrect because the infinite stream is made finite by the intermediate limit() operation.
+      Options A and E are incorrect because the source streams are not infinite.
+      Therefore, the call to max() sees only three elements and terminates. For more information, see Chapter 4.
+     */
  private static void magic(Stream<Integer> s) {
     Optional o = s
       .filter(x -> x < 5)
