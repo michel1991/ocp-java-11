14f6a5edb10d5345ac84adbe44d2591296b204af
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-09 08:13:17 +0100

    code-errors:  attempting to assign weaker access privileges; was public

---
+++ interfaces/errors/ClownFishModifierAccess.java
@@ -0,0 +1,31 @@
+package interfaces.errors;
+
+/**
+* Which of the following statements about this program is correct?
+    A. It compiles and prints 14.
+    B. It compiles and prints 15.
+    C. The code will not compile because of line 4.
+    D. The code will not compile because of line 5.
+    E. The code will not compile because of line 6.
+    F. None of the above
+
+  The inherited interface method getNumOfGills(int) is implicitly public;
+  therefore, it must be declared public in any concrete class that implements the interface.
+  Since the method uses the default (package-private) modifier in the ClownFish class, line 6 does not compile, making option E the correct answer.
+  If the method declaration was corrected to include public on line 6, then the program would compile and print 15 at runtime, and option B would be the correct answer.
+
+ */
+interface Aquatic { // 1:
+    int getNumOfGills(int p); //  2:
+} //    3:
+ class ClownFish implements Aquatic { // 4:
+    String getNumOfGills() { return "14"; } //  5:
+    int getNumOfGills(int input) { return 15; } //   6:
+    public static void main() { // 7:
+        System.out.println(new ClownFish().getNumOfGills(-1)); //  8:
+    } } // 9:
+public class ClownFishModifierAccess {
+    public static void main(String[] args) {
+        ClownFish.main();
+    }
+}
