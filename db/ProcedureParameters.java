31ef57cfbf3e7fac47455ccbd61d5b6b262196a9
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-09 07:52:23 +0100

    code: require out parameter

--- db/ProcedureParameters.java
+++ db/ProcedureParameters.java
@@ -24,6 +24,28 @@ public class ProcedureParameters {
       }
   }
     
+    /**
+    *  Which of the following could be true of the following code? (Choose all that apply.)
+        A. The stored procedure can declare an IN or INOUT parameter.
+        B. The stored procedure can declare an INOUT or OUT parameter.
+        C. The stored procedure must declare an IN parameter.
+        D. The stored procedure must declare an INOUT parameter.
+        E. The stored procedure must declare an OUT parameter.
+
+      Since the code calls registerOutParameter(), we know the stored procedure cannot use an IN parameter.
+      Further, there is no setInt(), so it cannot be an INOUT parameter either.
+      Therefore, the stored procedure must use an OUT parameter, making option E the answer.
+     */
+    public static void requireOutParameter() throws SQLException{
+        var sql = "{call transform(?)}";
+        try (var conn = DriverManager.getConnection(
+                "jdbc:derby:zoo"); var cs = conn.prepareCall(sql)) {
+            cs.registerOutParameter(1, Types.INTEGER);
+            cs.execute();
+            System.out.println(cs.getInt(1));
+        }
+    }
+
     public static void main(String... args) throws SQLException{
       procedureParameter();
   }
