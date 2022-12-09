b982c824b805b3727279ca217978bada17c52125
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-09 17:35:14 +0100

    docs: add explanation

--- internationalization/errors/MethodGetOfPropertyHasNotDefaultValue.java
+++ internationalization/errors/MethodGetOfPropertyHasNotDefaultValue.java
@@ -2,6 +2,14 @@ package internationalization.errors;
 import java.util.*;
 
 /**
+   What is the output of the following method if props contains {veggies=brontosaurus, meat=velociraptor}?
+    A. brontosaurus none
+    B. brontosaurus null
+    C. none none
+    D. none null
+    E. The code does not compile.
+    
+    F. A runtime exception is thrown.
     The Properties class defines a get() method that does not allow for a default value.
      It also has a getProperty() method, which returns the default value if the key is not provided.
  */
