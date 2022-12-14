dce5e44059539fa8760c8ee0184fe15c9e273a77
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-14 18:13:58 +0100

    code-errors: an enum switch case label must be the unqualified name of an enumeration constant

---
+++ enums/errors/IceCreamAllPathToValueOfEnumInCase.java
@@ -0,0 +1,38 @@
+package enums.errors;
+
+/**
+*  What is the result of the following program?
+    A. v
+    B. vc
+    C. s
+    D. missing flavor
+    E. Exactly one line of code does not compile.
+    F. More than one line of code does not compile.
+    G. The code compiles but produces an exception at runtime.
+
+   When using an enum in a switch statement, the case statement must be made up of the enum values only.
+   If the enum name is used in the case statement value, then the code does not compile.
+   For example, VANILLA is acceptable but Flavors.VANILLA is not.
+   For this reason, the three case statements do not compile, making option F the correct answer.
+   If these three lines were corrected, then the code would compile and produce a NullPointerException at runtime.
+
+ */
+class IceCream {
+    enum Flavors {
+        CHOCOLATE, STRAWBERRY, VANILLA
+    }
+
+    public static void main(String[] args) {
+        Flavors STRAWBERRY = null;
+        switch (STRAWBERRY) {
+            case Flavors.VANILLA: System.out.print("v");
+            case Flavors.CHOCOLATE: System.out.print("c");
+            case Flavors.STRAWBERRY: System.out.print("s");
+            break;
+            default: System.out.println("missing flavor"); }
+    }
+}
+
+public class IceCreamAllPathToValueOfEnumInCase {
+
+}
