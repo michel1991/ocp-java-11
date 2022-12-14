4d879fcc369238c798556a10b15c65e086383dfc
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-14 14:25:35 +0100

    docs: add explanation

--- list/HashSetDisplayDataInUnknownOrder.java
+++ list/HashSetDisplayDataInUnknownOrder.java
@@ -2,21 +2,30 @@ package list;
 import java.util.*;
 
 /**
-* The code compiles fine. It allows any implementation of Number to be added.
-* Lines 5 and 8 succesfully autobox the primitives into an Integer and Long, respectively.
-* HashSet does not guarantee any iteration order, making options A and D correct.
+    Which of the following statements are true? (Choose all that apply.)
+    A. The code compiles successfully.
+    B. The output is 8675null309.
+    C. The output is 867586null309.
+    D. The output is indeterminate.
+    E. There is a compiler error on line 3.
+    F. There is a compiler error on line 9.
+    G. An exception is thrown.
+
+    The code compiles fine. It allows any implementation of Number to be added.
+    Lines 5 and 8 succesfully autobox the primitives into an Integer and Long, respectively.
+    HashSet does not guarantee any iteration order, making options A and D correct.
  */
 public class HashSetDisplayDataInUnknownOrder {
    public static void display(){
-       var numbers = new HashSet<Number>();
-       numbers.add(Integer.valueOf(86));
-       numbers.add(75);
-       numbers.add(Integer.valueOf(86));
-       numbers.add(null);
-       numbers.add(309L);
-       Iterator iter = numbers.iterator();
-       while (iter.hasNext())
-           System.out.println(iter.next());
+       var numbers = new HashSet<Number>(); // 3
+       numbers.add(Integer.valueOf(86));  // 4
+       numbers.add(75);   // 5
+       numbers.add(Integer.valueOf(86));  // 6
+       numbers.add(null);  // 7
+       numbers.add(309L);   // 8
+       Iterator iter = numbers.iterator(); // 9
+       while (iter.hasNext())  // 10
+           System.out.println(iter.next()); // 11
    }
 
     public static void main(String... args){
--- list/errors/FollowTypeReturnType.java
+++ list/errors/FollowTypeReturnType.java
@@ -4,12 +4,21 @@ import java.util.*;
 
 
 /**
- * The key to this question is keeping track of the types. Line 48 is a Map<Integer, Integer>.
- * Line 49 builds a List out of a Set of Entry objects, giving us List<Entry<Integer, Integer>>.
- * This causes a compile error on line 56 since we can’t multiply an Entry object by two.
- * Lines 51 through 54 are all of type List<Integer>. The first three are immutable, and the one on line 54 is mutable.
- * This means line 57 throws an UnsupportedOperationException since we attempt to modify the list.
- * Line 58 would work if we could get to it. Since there is one compiler error and one runtime error, options A and E are correct.
+  What is the result of the following code? (Choose all that apply.)
+    A. One line fails to compile.
+    B. Two lines fail to compile.
+    C. Three lines fail to compile.
+    D. The code compiles but throws an exception at runtime.
+    E. If any lines with compiler errors are removed, the code throws an exception at runtime.
+    F. If any lines with compiler errors are removed, the code prints [16, 32, 4].
+    G. The code compiles and prints [16, 32, 4] without any changes.
+
+     The key to this question is keeping track of the types. Line 48 is a Map<Integer, Integer>.
+     Line 49 builds a List out of a Set of Entry objects, giving us List<Entry<Integer, Integer>>.
+     This causes a compile error on line 56 since we can’t multiply an Entry object by two.
+     Lines 51 through 54 are all of type List<Integer>. The first three are immutable, and the one on line 54 is mutable.
+     This means line 57 throws an UnsupportedOperationException since we attempt to modify the list.
+     Line 58 would work if we could get to it. Since there is one compiler error and one runtime error, options A and E are correct.
  */
 public class FollowTypeReturnType {
   public static void main(String... args){
