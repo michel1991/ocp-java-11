c752a4d5de08bb9fa3b45e07793f277addba7116
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-15 19:46:14 +0100

    docs: add documentation

--- operations/errors/PromotedToLong.java
+++ operations/errors/PromotedToLong.java
@@ -2,10 +2,17 @@ package operations.errors;
 
 public class PromotedToLong {
     /**
-    * The first line contains a compilation error. The value 3 is cast to long.
-    * The 1 * 2 value is evaluated as int but promoted to long when added to the 3.
-    * Trying to store a long value in an int variable triggers a compiler error.
-    * The other lines do not contain any compilation errors, as they store smaller values in larger or same-size data types, with the third and fourth lines using casting to do so.
+    * How many lines of the following code contain compiler errors?
+        A. 0
+        B. 1
+        C. 2
+        D. 3
+        E. 4
+        
+    The first line contains a compilation error. The value 3 is cast to long.
+    The 1 * 2 value is evaluated as int but promoted to long when added to the 3.
+    Trying to store a long value in an int variable triggers a compiler error.
+    The other lines do not contain any compilation errors, as they store smaller values in larger or same-size data types, with the third and fourth lines using casting to do so.
      */
   public static void main(String... args){
       int note = 1 * 2 + (long)3;
