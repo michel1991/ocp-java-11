9cf14cb6d511f7f94e3ab02dbd8bb11834904310
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-15 14:51:27 +0100

    docs: add explanation

--- list/ArrayComparison.java
+++ list/ArrayComparison.java
@@ -4,13 +4,23 @@ import java.util.*;
 public class ArrayComparison {
 
     /**
-    * The array is allowed to use an anonymous initializer because it is in the same line as the declaration.
-    * The ArrayList uses the diamond operator. This specifies the type matches the one on the left without having to retype it.
-    * After adding the two elements, list contains [6, 8]. We then replace the element at index 1 with 9, resulting in [6, 9].
-    * Finally, we remove the element at index 0, leaving [9]and making option B correct.
-    * Option C is incorrect because arrays output something that looks like a reference that rather than a nicely printed list of values.
-    * Option E is correct because the compare() method returns 0 when the arrays are the same length and have the same elements.
-    * Option F is correct because the mismatch() method returns a -1 when the arrays are equivalent. For more information, see Chapter 5.
+       What is printed by the following code? (Choose all that apply.)
+        A. [8]
+        B. [9]
+        C. [Ljava.lang.String;@160bc7c0
+        D. C-1
+        E. C0
+        F. M-1
+        G. M0
+        H. The code does not compile.
+
+        The array is allowed to use an anonymous initializer because it is in the same line as the declaration.
+        The ArrayList uses the diamond operator. This specifies the type matches the one on the left without having to retype it.
+        After adding the two elements, list contains [6, 8]. We then replace the element at index 1 with 9, resulting in [6, 9].
+        Finally, we remove the element at index 0, leaving [9]and making option B correct.
+        Option C is incorrect because arrays output something that looks like a reference that rather than a nicely printed list of values.
+        Option E is correct because the compare() method returns 0 when the arrays are the same length and have the same elements.
+        Option F is correct because the mismatch() method returns a -1 when the arrays are equivalent. For more information, see Chapter 5.
      */
    static void withMismatch(){
        int[] array = {6,9,8};
