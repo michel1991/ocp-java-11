37d4b93a7252cc35a38f73dcd76191ea8d9edda5
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-15 06:34:21 +0100

    code: remove items into list

---
+++ list/RemoveItems.java
@@ -0,0 +1,35 @@
+package list;
+import java.util.*;
+import java.util.function.*;
+
+public class RemoveItems {
+    /**
+    *  What is true of the following code?
+    *   A. It outputs [2.718, 2.718].
+        B. It outputs [3.14, 3.14, 2.718, 2.718].
+        C. It outputs [12.0].
+        D. The code does not compile due to the Predicate.
+        E. The code does not compile due to the removeIf() line.
+        F. The code does not compile for another reason.
+
+     The Predicate correctly takes a Double value and returns a boolean. It is a complicated Predicate, but it does compile, ruling out option D.
+     First the Predicate rounds the number to the nearest int. On the right side of the ==, the Predicate converts the Double object to a double primitive.
+     Then it casts to an int, dropping any numbers after the decimal point.
+     Don’t worry if you haven’t seen doubleValue() before.
+     Remember that the exam may use APIs you haven’t learned as long as the APIs do what they sound like.
+      The removeIf() method removes all values that round down since the Predicate returns true for any values that have a rounded number equivalent to the number without the decimal value.
+      Therefore, option A is correct.
+     */
+   static void removeUsingComparisonWithDoubleValueMethod(){
+       List<Double> numbers = new ArrayList<>();
+       numbers.add(12.0);
+       numbers.add(3.14);
+       numbers.add(3.14);
+       numbers.add(2.718);
+       numbers.add(2.718);
+
+       Predicate<Double> pred = n -> Math.round(n) == (int) n.doubleValue();
+       numbers.removeIf(pred);
+       System.out.println(numbers);
+   }
+}
