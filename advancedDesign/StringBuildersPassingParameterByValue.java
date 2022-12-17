2d0fd9aa740da53f760d12f677c7a92129dbc08a
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-17 02:20:51 +0100

    code: passing paramter by value using string builder

--- advancedDesign/StringBuildersPassingParameterByValue.java
+++ advancedDesign/StringBuildersPassingParameterByValue.java
@@ -30,7 +30,52 @@ class StringBuilders {
     }
 }
 
+/**
+* What is the result of the following code?
+*   A. or-ixyginal stringmaybe
+    B. or-nal string
+    C. or-xyginal stringmaybe
+    D. original string
+    E. A different value
+    F. An exception is thrown.
+    G. The code does not compile.
+
+    There’s a lot going on in this question. The first thing to notice is that there is only one StringBuilder in use.
+    Both methods create a new StringBuilder, but they do not use it.
+   The code uses method chaining. In the trade() method, after the call to append(), we have "original stringmaybe".
+   That result is passed to the first insert() call, which inserts at index 2.
+    At this point, we have "orshiginal stringmaybe ".
+    That result is passed to the final insert(), which inserts xy at index 5, resulting in "orshixyginal stringmaybe".
+   Then we call the replace() method, which removes the characters from positions 2, 3, and 4 and substitutes a dash.
+   We finally have the result "or-xyginal stringmaybe", making option C correct.
+ */
+class TradeReplace{
+    private static StringBuilder trade(StringBuilder original) {
+        var b = new StringBuilder();
+        return original.append("maybe").insert(2, "sh")
+        .insert(5, "xy");
+    }
+    private static StringBuilder replace(StringBuilder original) {
+        var b = new StringBuilder();
+        return original.replace(2, 5, "-");
+    }
+
+    public static void main() {
+        System.out.println("Begin  TradeReplace ");
+        var sb = new StringBuilder("original string");
+        var trade = trade(sb);
+        var newSb = replace(sb);
+
+        System.out.println(newSb);
+
+        System.out.println("End TradeReplace ");
+    }
+}
+
 public class StringBuildersPassingParameterByValue {
+
+
+
     public static void main(String[] args) {
         StringBuilders.main();
     }
