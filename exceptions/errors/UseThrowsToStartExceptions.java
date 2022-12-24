9f2ba81ad7dd399e5607df92ac9339ce8eb9ffe9
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-24 17:46:27 +0100

    code-error: use invalid key word throws to start exception

---
+++ exceptions/errors/UseThrowsToStartExceptions.java
@@ -0,0 +1,31 @@
+package exceptions.errors;
+
+/**
+ * What is printed when running the following snippet?
+    A. 1
+    B. 3
+    C. 5
+    D. 7
+    E. The code does not compile.
+    F. An exception is thrown.
+
+    Method e() is incorrect. It should say throw new IllegalArgumentException() to compile.
+    If this change were made, option C would be correct.
+ */
+public class UseThrowsToStartExceptions {
+    public static void main(String[] args) {
+        int i = 0;
+        try {
+            i += 1;
+            e();
+            i += 2;
+        } catch (Exception e) {
+            i += 4;
+        }
+      System.out.print(i);
+    }
+
+    private static void e() {
+        throws new IllegalArgumentException();
+    }
+}
