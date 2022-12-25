79ef8560ca1925e66cf96ff9b62d73750861fcf6
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-25 23:07:53 +0100

    code: add cast operations

--- operations/ComplexCasting.java
+++ operations/ComplexCasting.java
@@ -45,7 +45,25 @@ public class ComplexCasting {
         System.out.println(sun+"-"+jupiter+"-"+mars);
     }
 
+    /**
+       Which exception will the following throw?
+        A. ArrayIndexOutOfBoundsException
+        B. ClassCastException
+        C. IllegalArgumentException
+        D. NumberFormatException
+        E. None of the above
+
+        The cast is fine because both types are the same. The last line refers to an invalid index of the array.
+        Remember that array indexes begin with zero.
+     */
+    static void castingArrayOfIntUsingObject(){
+        int[] nums = new int[] { 1, 4, 6};
+        Object p = nums;
+        int[] two = (int[]) p;
+        System.out.println(two[two.length]);
+    }
+
     public static void main(String... args){
       firstCasting();
-   }
+    }
 }
