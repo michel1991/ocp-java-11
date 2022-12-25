5fb72f29faa0aeb556baa5edfff40a6d0eccd05f
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-25 23:03:45 +0100

    docs: add documentation

--- annotations/AnnotationTypeUse.java
+++ annotations/AnnotationTypeUse.java
@@ -7,20 +7,29 @@ import java.lang.annotation.*;
 
 }
 
+/**
+ Given the following declaration for @Valid, which statements allow the following program to compile?
+    A. String a = @Valid "mail"
+    B. int b = @Valid 10
+    C. boolean c = @Valid true
+    D. char d = @Valid 'a'
+    E. long e = @Valid 39L
+    F. None of the above
+
+    Trick question! Annotations are applied to declarations, not values.
+    They can be applied to almost any type declaration in Java including classes,
+    interfaces, enums, methods, constructors, variables (member, static, and local),
+    parameters, cast operations, etc. They cannot be applied to a value or expression,
+     except as a cast operation. When used in a cast operation, a type is required such as int b = (@Valid int) 10.
+ */
 class Tester {
     void test() {
-        int age = (@Valid int) 1;
+        int age = (@Valid int) 1; //________________________________
     }
 }
 
 
-/**
-* Trick question! Annotations are applied to declarations, not values.
-* They can be applied to almost any type declaration in Java including classes,
-* interfaces, enums, methods, constructors, variables (member, static, and local),
-* parameters, cast operations, etc. They cannot be applied to a value or expression,
-* except as a cast operation. When used in a cast operation, a type is required such as int b = (@Valid int) 10.
- */
+
 public class AnnotationTypeUse {
    public static void main(String... args){
        new Tester().test();
