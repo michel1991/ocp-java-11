22f49c2644691d54c1c3a3ad2a25986a3a6910ce
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-26 11:30:54 +0100

    code: load resource bundle

--- internationalization/LoadCorrectDolphins.java
+++ internationalization/LoadCorrectDolphins.java
@@ -134,6 +134,29 @@ public class LoadCorrectDolphins {
 
     }
 
+    /**
+       Assume that all of the files mentioned in the answer choices exist and define the same keys.
+       Which one will be used to find the key in line 8?
+
+       A. Dolphins.properties
+        B. Dolphins_US.properties
+        C. Dolphins_fr.properties
+        D. Whales.properties
+        E. Whales_en_US.properties
+        F. The code does not compile.
+
+        Java will first look for the most specific match it can find, starting with Dolphins_en_US.properties.
+        Since it is not found, it drops the country and looks for Dolphins_en.properties.
+        Since no match is found, Java drops the language and goes on to Dolphin.properties.
+        For these reasons, option A is the correct answer.
+        Option B is incorrect because a locale requires a language code.
+     */
+    static void fiveExemple(){
+        Locale.setDefault(new Locale("en", "US")); // 6:
+        var b = ResourceBundle.getBundle("Dolphins"); // 7:
+        System.out.println(b.getString("name")); //   8:
+    }
+
   public static void main(String... args){
         firstExample();
         System.out.println();
