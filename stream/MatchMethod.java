ab1f0a32d1b5390aa596e88aadc2ace00bca179c
michel1991 <michelmvoumamvouma@gmail.com>
2022-12-27 16:04:26 +0100

    code: add none match method for revision

---
+++ stream/MatchMethod.java
@@ -0,0 +1,17 @@
+package stream;
+import java.util.*;
+import java.util.function.*;
+public class MatchMethod {
+    // aucun ne correspond au predicat
+    public static void noneMatchToPredicat(){
+        var s = Stream.of( "mm",  "mm",  "mm",  "");
+        var match = s
+                 .peek(System.out::println)
+                 .noneMatch(String::isEmpty);
+        System.out.println(match);
+    }
+
+    public static void main(String... args){
+        noneMatchWithEmpty();
+    }
+}
