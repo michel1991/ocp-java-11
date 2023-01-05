aeef3f65ea14995b7a4b8d9f29c572b5d0ad45cf
michel1991 <michelmvoumamvouma@gmail.com>
2023-01-05 17:08:38 +0100

    docs: add documentation

--- nio/NormalizeRelativePath.java
+++ nio/NormalizeRelativePath.java
@@ -4,11 +4,18 @@ import java.nio.file.*;
 public class NormalizeRelativePath {
 
     /**
-    * Assuming the current directory is /seals/harp/food, what is the result of executing the following code?
-    * The normalize() method does not convert a relative path into an absolute path; therefore,
-    * the path value after the first line is just the current directory symbol.
-    * The for() loop iterates the name values, but since there is only one entry,
-    * the loop terminates after a single iteration. Therefore, option B is correct.
+     Assuming the current directory is /seals/harp/food, what is the result of executing the following code?
+        A. 0
+        B. 1
+        C. 2
+        D. 3
+        E. 4
+        F. The code compiles but throws an exception at runtime.
+
+      The normalize() method does not convert a relative path into an absolute path; therefore,
+      the path value after the first line is just the current directory symbol.
+      The for() loop iterates the name values, but since there is only one entry,
+     the loop terminates after a single iteration. Therefore, option B is correct.
      */
    public static void countNumberOfPath(){
        final Path path = Paths.get(".").normalize();
