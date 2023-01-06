41fcd2e1e93897f63f9090e0b80b5c72390d4ad1
michel1991 <michelmvoumamvouma@gmail.com>
2023-01-06 16:15:09 +0100

    code: choice correct lambda

--- lambda/ChoiceFI.java
+++ lambda/ChoiceFI.java
@@ -1,5 +1,6 @@
 package lambda;
 import java.util.function.*;
+import java.util.*;
 public class ChoiceFI {
 
 
@@ -27,12 +28,36 @@ public class ChoiceFI {
       BiConsumer<String, String> m = (a, b) -> System.out.println( " m " + a.concat(" ").concat(b));
       BiConsumer<String, String>  n = (a, b) -> System.out.println(" n " + b.concat(" ").concat(a));
       x = String::new; //  6: ____________
-      y = m.andThen(n); //  7: ____________ y
-      z = a -> a + a; //  8: ____________
-      y.accept("michel", "stephane");
-  }
+        y = m.andThen(n); //  7: ____________ y
+        z = a -> a + a; //  8: ____________
+        y.accept("michel", "stephane");
+    }
 
     public static void main(String... args){
         firstSample(() -> "Denise", (a, b) ->System.out.println(" n " + a.concat(b)) ,(a) -> a);
+    }
+
+    /**
+     * Which functional interfaces complete the following code presuming variable r exists? (Choose all that apply.)
+        A. BinaryPredicate<Integer, Integer>
+        B. Comparable<Integer>
+        C. Comparator<Integer>
+        D. Consumer<Integer>
+        E. Predicate<Integer>
+        F. Runnable
+        G. Runnable<Integer>
+
+        Line 6 requires you to know that negate() is a convenience method on Predicate.
+        This makes option E correct. Line 7 takes zero parameters and doesn’t return anything,
+        making it a Runnable. Remember that Runnable doesn’t use generics.
+        This makes option F correct. Finally, line 8 takes two parameters and returns an int.
+        Option C is correct.
+        Comparable is there to mislead you since
+        it takes only one parameter in its single abstract method.
+     */
+    static void secondExample(Predicate<Integer> r){
+        Predicate<Integer> x = r.negate(); //  6:    ______
+        Runnable y = () -> System.out.println(); //     7:    ______
+       Comparator<Integer> z = (a, b) -> a - b;  //  8:    ______
     }
 }
