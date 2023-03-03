a9982b94d369ec838409ab08067adbad0005f58e
michel1991 <michelmvoumamvouma@gmail.com>
2023-03-03 14:38:03 +0100

    code: choice correct checked exception

---
+++ exceptions/ChoiceCorrectCheckedException.java
@@ -0,0 +1,29 @@
+package exceptions;
+import java.io.*;
+import java.sql.*;
+
+
+/**
+ * Which of the following exception classes must be handled or declared in the method in which they are thrown? (Choose three.)
+    A. Happy
+    B. Dopey
+    C. Sleepy
+    D. Sneezy
+    E. Doc
+    F. Grumpy
+
+    Since IOException and SQLException are checked exceptions, Happy and Grumpy are checked exceptions, respectively,
+    making options A and F correct. Since Dopey inherits Grumpy, option B is also a checked exception.
+    Options C and D are unchecked exceptions because those classes inherit RuntimeException.
+    Option E is also an unchecked exception because all Error classes are unchecked.
+ */
+class Happy extends IOException {}
+class Dopey extends Grumpy {}
+class Sleepy extends IllegalStateException {}
+class Sneezy extends UnsupportedOperationException {}
+class Doc extends AssertionError {}
+class Grumpy extends SQLException {}
+
+public class ChoiceCorrectCheckedException {
+
+}
