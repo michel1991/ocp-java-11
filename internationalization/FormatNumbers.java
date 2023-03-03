f9886fb2a427b6f75e609a63f97c0396193d4fce
michel1991 <michelmvoumamvouma@gmail.com>
2023-03-03 16:48:23 +0100

    code: format number to uk

--- internationalization/FormatNumbers.java
+++ internationalization/FormatNumbers.java
@@ -1,6 +1,8 @@
 package internationalization;
 import java.util.stream.*;
 import java.text.*;
+import java.util.*;
+import static java.util.Locale.Category;
 
 public class FormatNumbers {
     /**
@@ -25,6 +27,29 @@ public class FormatNumbers {
                 .collect(Collectors.joining("> <"));
                        System.out.println("<"+message+">");
    }
+
+    /**
+       For currency, the US uses the $ symbol, the UK uses the £ symbol, and Germany uses the € symbol.
+        Given this information, what is the expected output of the following code snippet?
+
+            A. $1.10
+            B. 1,10€
+            C. £1.10
+            D. The code does not compile.
+            E. An exception is thrown at runtime.
+            F. The output cannot be determined without knowing the locale of the system where it will be run.
+
+            The code compiles, so option D is incorrect.
+            In this sample, the default locale is set to US, while the default locale format is set to GERMANY.
+            Neither is used for formatting the value, as getCurrencyInstance() is called with UK as the locale.
+            For this reason, the £ symbol is used, making option C correct.
+     */
+    static void formatToUK(){
+        Locale.setDefault(Locale.US);
+        Locale.setDefault(Category.FORMAT, Locale.GERMANY);
+        System.out.print(NumberFormat.getCurrencyInstance(Locale.UK).format(1.1));
+
+    }
 
     public static void main(String... args){
         formatDoubleWithStream();
