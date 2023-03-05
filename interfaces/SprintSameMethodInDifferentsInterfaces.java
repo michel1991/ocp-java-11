4cc3d7ae730e89de748eb7ad3a8075ee136c5cf3
michel1991 <michelmvoumamvouma@gmail.com>
2023-03-05 20:49:52 +0100

    code: same method into both interface with covariant return type

---
+++ interfaces/SprintSameMethodInDifferentsInterfaces.java
@@ -0,0 +1,42 @@
+package interfaces;
+
+/**
+ * What is the output of the following application?
+    A. Walking and running!
+    B. Walking and jogging!
+    C. Sprinting!
+    D. The code does not compile.
+    E. The code compiles but prints an exception at runtime.
+    F. None of the above.
+
+    Having one class implement two interfaces that both define the same default method signature
+    leads to a compiler error unless the class overrides the default method.
+    In this case, the Sprint class overrides both walk() methods correctly;
+    therefore, the code compiles without issue, and option C is correct.
+    Note that the return types of the two default methods are different,
+    but the overridden method uses a return type that is covariant with both.
+ */
+interface Run {
+    default CharSequence walk() {
+        return "Walking and running!";
+    }
+}
+interface Jog {
+    default String walk() {
+        return "Walking and jogging!";
+    }
+}
+class Sprint implements Run, Jog {
+    public String walk() {
+        return "Sprinting!";
+    }
+    public static void main(String[] args) {
+        var s = new Sprint();
+        System.out.println(s.walk());
+    }
+}
+
+
+public class SprintSameMethodInDifferentsInterfaces {
+
+}
