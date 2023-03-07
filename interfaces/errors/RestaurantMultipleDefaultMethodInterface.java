ba8737c21b51661d10cc509497d176a163eb89a9
michel1991 <michelmvoumamvouma@gmail.com>
2023-03-07 20:27:28 +0100

    error-code: both define getHeight(), but with unrelated return types

---
+++ interfaces/errors/RestaurantMultipleDefaultMethodInterface.java
@@ -0,0 +1,30 @@
+package interfaces.errors;
+
+/**
+   Which is the first line to not compile?
+   A. Line m1
+    B. Line m2
+    C. Line m3
+    D. Line m4
+    E. None of the above
+
+    The interface declarations compile without issue.
+    When inheriting two default methods with the same signature,
+    the Tower class is required to override both methods
+    even if the class is marked abstract. For this reason,
+    line m3 is the first line that does not compile, and option C is correct.
+    Note that there is no possible overridden method
+    that can fulfill both inherited default methods since the return types are not covariant.
+ */
+interface Building {
+    default Double getHeight() { return 1.0; }         // m1
+}
+interface Office {
+    public default String getHeight() { return null; } // m2
+}
+abstract class Tower implements Building, Office {}   // m3
+class Restaurant extends Tower {}              // m4
+
+public class RestaurantMultipleDefaultMethodInterface {
+
+}
