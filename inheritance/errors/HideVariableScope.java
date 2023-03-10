9b3970a179ad27c2bc740d7ea8f5d3c84bcd72b2
michel1991 <michelmvoumamvouma@gmail.com>
2023-03-10 21:25:30 +0100

    refactor: make style

--- inheritance/errors/HideVariableScope.java
+++ inheritance/errors/HideVariableScope.java
@@ -2,16 +2,16 @@ package inheritance.errors;
 
 /**
 * Question: What is the result of the following code? (Choose all that apply.)
-* A. Nothing is output
-* B. ==
-* C. bird
-* D. sparrows
-* E. The code does not compile.
-*
-* This is one of those tricky questions that appears to ask about one thing but is really about something else.
-* The first if statement has a scope of line 6. The second if statement has a scope of lines 7 through 10.
-* The last if statement has a scope of lines 11 through 13. The problem is that s1 is declared in the second if statement and is not accessible after it ends.
-* This gives a compiler error on line 11 since there is no s1 variable in scope. And yes, the exam will try to trick you with nonstandard indentation.
+    A. Nothing is output
+    B. ==
+    C. bird
+    D. sparrows
+    E. The code does not compile.
+
+    This is one of those tricky questions that appears to ask about one thing but is really about something else.
+    The first if statement has a scope of line 6. The second if statement has a scope of lines 7 through 10.
+    The last if statement has a scope of lines 11 through 13. The problem is that s1 is declared in the second if statement and is not accessible after it ends.
+    This gives a compiler error on line 11 since there is no s1 variable in scope. And yes, the exam will try to trick you with nonstandard indentation.
  */
 class Sparrow extends Bird {
 
