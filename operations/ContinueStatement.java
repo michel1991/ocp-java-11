a12582c251217a6e302bb3346d04b4d9d1b6fb91
michel1991 <michelmvoumamvouma@gmail.com>
2023-03-17 18:04:07 +0100

    code: continues statement

---
+++ operations/ContinueStatement.java
@@ -0,0 +1,31 @@
+package operations;
+
+public class ContinueStatement {
+
+    /**
+     *  What is the output of the following code snippet?
+        A. 0
+        B. 1
+        C. 2
+        D. 3
+        E. The code does not compile.
+        F. None of the above.
+
+        On the first iteration of the loop, stops[++count] evaluates to stops[1],
+        while also setting count to 1.
+        Since stops[1] is Monroe and it has six characters, the break is reached, and the loop stops.
+        For this reason, 1 is printed, making option B correct.
+
+     */
+    static void firstExample(){
+        int count = 0;
+        var stops = new String[] { "Washington", "Monroe", "Jackson", "LaSalle" };
+        while (count < stops.length)
+            if (stops[++count].length() < 8)
+                break;
+        else continue;
+        System.out.println(count);
+
+    }
+  
+}
