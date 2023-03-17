f6c05d55ab7b0efe0c42e30166e71765d5cc6a2a
michel1991 <michelmvoumamvouma@gmail.com>
2023-03-17 17:25:03 +0100

    error-code: Flat map  flatMapToInt() requires a Function with a return value of IntStream

---
+++ stream/errors/FlatMapToIntRequiresFunction.java
@@ -0,0 +1,29 @@
+package stream.errors;
+import java.util.*;
+import java.util.stream.*;
+import java.util.function.*;
+
+public class FlatMapToIntRequiresFunction {
+
+    /**
+     * What is the output of the following code snippet?
+        A. 12124
+        B. 11224
+        C. 122
+        D. The code does not compile.
+        E. The code compiles but does not output anything at runtime.
+        F. None of the above.
+
+        The code does not compile because flatMapToInt() requires a Function
+        with a return value of IntStream, not Stream, making option D correct.
+     */
+ static void flatMap(){
+     var apples = List.of(1, 2);
+     var oranges = List.of(1, 2);
+     final var count = Stream.of(apples, oranges)
+                            .flatMapToInt(List::stream)
+                            .peek(System.out::print)
+                            .count();
+     System.out.print(count);
+ }
+}
