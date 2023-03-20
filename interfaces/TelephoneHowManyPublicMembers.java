24b1ee37ba193c69c248e3d52a92e24c1f7e8101
michel1991 <michelmvoumamvouma@gmail.com>
2023-03-20 06:31:38 +0100

    code: find how many public memebers on interfaces

---
+++ interfaces/TelephoneHowManyPublicMembers.java
@@ -0,0 +1,26 @@
+package interfaces;
+
+/**
+ *  How many of the following members of Telephone interface are public?
+    A. Zero.
+    B. One.
+    C. Two.
+    D. Three.
+    E. Four.
+    F. The code does not compile.
+
+    All four members of the Telephone interface are implicitly public,
+    making option E correct.
+    Only private and private static interface methods are not public, and they must be explicitly marked private.
+
+ */
+interface Telephone {
+    static int call() { return 1; }
+    default void dial() {}
+    long answer();
+    String home = "555-555-5555";
+}
+
+public class TelephoneHowManyPublicMembers {
+
+}
