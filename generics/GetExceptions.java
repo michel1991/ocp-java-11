8e6796a87ed98f54c723e780d91cbc648ea23634
michel1991 <michelmvoumamvouma@gmail.com>
2023-03-24 04:46:40 +0100

    code: Collections with generics exceptions

---
+++ generics/GetExceptions.java
@@ -0,0 +1,21 @@
+package generics;
+import java.util.*;
+import java.io.*;
+
+public class GetExceptions {
+    /**
+       Which of the following fills in the blank so this code compiles?
+        A. ?
+        B. ? extends Exception
+        C. ? super Exception
+        D. None of the above
+
+        Option C is correct because all the types
+        being added are of type Exception or direct subclasses.
+
+     */
+    public static void getExceptions(Collection<? super Exception> coll) { // getExceptions(Collection˂__________˃ coll)
+        coll.add(new RuntimeException());
+        coll.add(new Exception());
+    }
+}
