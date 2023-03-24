0196a2e4a66377e3ee058b10a0687f941daa97c4
michel1991 <michelmvoumamvouma@gmail.com>
2023-03-24 18:03:12 +0100

    error-code: array creation without size

---
+++ arrays/errors/ArrayCreationWithoutSize.java
@@ -0,0 +1,24 @@
+package arrays.errors;
+public class ArrayCreationWithoutSize {
+    /**
+      How many of the following are legal declarations?
+        A. None
+        B. One
+        C. Two
+        D. Three
+        E. Four
+
+        When creating an array object, a set of elements or size is required.
+        Therefore, lion and bear are incorrect.
+        Thebrackets containing the size are required to be after the type,
+        making ohMy incorrect. The only one that is correct is tiger, making the correct answer option B.
+
+     */
+  static void creation(){
+      float[] lion = new float[];
+      float[] tiger = new float[1];
+      float[] bear = new[] float;
+      float[] ohMy = new[1] float;
+
+  }
+}
