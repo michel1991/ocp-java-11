4a07f032a1b82ce068c129e22bd68aad7a92508b
michel1991 <michelmvoumamvouma@gmail.com>
2023-03-24 04:06:26 +0100

    error-code: method get of property object does not hold default value

--- internationalization/errors/MethodGetOfPropertyHasNotDefaultValue.java
+++ internationalization/errors/MethodGetOfPropertyHasNotDefaultValue.java
@@ -18,6 +18,36 @@ public class MethodGetOfPropertyHasNotDefaultValue {
         System.out.println(props.get("veggies", "none")+ " " + props.get("omni", "none"));
     }
 
+    /**
+      What is the output if the solveMystery() method is applied to a Properties object loaded from mystery.properties?
+
+       mystery.properties
+        mystery=bag
+        type=paper
+
+        A. bag ? trick
+        B. bag ?null
+        C. bag null null
+        D. bag null trick
+        E. The code does not compile.
+        F. An exception is thrown at runtime.
+
+        The first line of the method is correct,
+        asProperties inherits Map and has a get() method.
+        The get() method does not have an overloaded version that takes a default value,
+        though.For this reason, the second and third get() calls do not compile,
+        and option E is correct. If getProperty() were instead
+        used on the second and third call, then the output would be bag null trick.
+
+
+     */
+    void solveMystery(Properties props) {
+        var a = props.get("mystery");
+        var b = props.get("more", null);
+        var c = props.get("more", "trick");
+        System.out.print(a + " " + b + " " + c);
+    }
+
     public static void main(String... args){
 
     }
