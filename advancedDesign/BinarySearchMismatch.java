82f7f074413519338e5558376ffc5288888fba1a
michel1991 <michelmvoumamvouma@gmail.com>
2023-03-24 04:10:38 +0100

    code: binary search operations

--- advancedDesign/BinarySearchMismatch.java
+++ advancedDesign/BinarySearchMismatch.java
@@ -32,6 +32,34 @@ public class BinarySearchMismatch {
       System.out.println(search + " " + mismatch1 + " " + mismatch2);
   }
 
+    /**
+     *  What does the following output? (Choose two.)
+        A. -1
+        B. -2
+        C. -3
+        D. 0
+        E. 1
+        F. 2
+
+        The code sorts before calling the binarySearch() method,
+        so it meets the precondition for that method. The target string of "RedHat"
+        is not found in the sorted array. If it were found, it would be between the second and third elements.
+        The rule is to take the negative index of where it would be inserted and subtract 1.
+        It would need to be inserted as the third element.
+        Since indexes are zero-based, this is index 2.
+        We take the negative, which is -2, and subtract 1, giving -3.
+        The target string of "Mac" is the second element in the sorted array.
+        Since array indices begin with zero, the second position is index 1.
+        This makes the answer options C and E.
+     */
+    static void searchOS(){
+        var os = new String[] { "Mac", "Linux", "Windows" };
+        Arrays.sort(os);
+
+        System.out.println(Arrays.binarySearch(os, "RedHat"));
+        System.out.println(Arrays.binarySearch(os, "Mac"));
+    }
+
     public static void main(String... args){
         binarySearchMismatch();
     }
