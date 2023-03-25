7fbb678d2fafe7c54893154b824a3e66190b1edd
michel1991 <michelmvoumamvouma@gmail.com>
2023-03-25 16:05:25 +0100

    refactor: make indentation

--- advancedDesign/WormChoiceConceptNotImmutability.java
+++ advancedDesign/WormChoiceConceptNotImmutability.java
@@ -2,15 +2,16 @@ package advancedDesign;
 
 
 /**
-* Which of the following is the code an example of? (Choose all that apply.)
-*   A. Immutability
+  Which of the following is the code an example of? (Choose all that apply.)
+    A. Immutability
     B. Input validation
     C. Limiting accessibility
     D. Restricting extensibility
     E. None of the above
 
   Since the class is final, it restricts extensibility, making option D correct.
-  The private variable limits accessibility, making option C correct. This class does not do any validation, making option B incorrect.
+  The private variable limits accessibility, making option C correct.
+  This class does not do any validation, making option B incorrect.
   Option A is incorrect because there are setters, and the length field is not marked final.
  */
 final class Worm {
--- advancedDesign/WormChoiceWhichConcept.java
+++ advancedDesign/WormChoiceWhichConcept.java
@@ -1,15 +1,16 @@
 package advancedDesign;
 
 /**
-* Which of the following is the code an example of? (Choose all that apply.)
-*   A. Immutability
+  Which of the following is the code an example of? (Choose all that apply.)
+    A. Immutability
     B. Input validation
     C. Limiting accessibility
     D. Restricting extensibility
     E. None of the above
 
     Since the class is final, it restricts extensibility, making option D correct.
-    The private variable limits accessibility, making option C correct. Finally, option A is correct.
+    The private variable limits accessibility, making option C correct.
+    Finally, option A is correct.
     This is an immutable class since it’s not possible to change the state after construction.
     This class does not do any validation, making option B incorrect.
  */
