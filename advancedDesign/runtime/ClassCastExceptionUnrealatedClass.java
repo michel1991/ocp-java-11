72e555710d83b7ececc77dc906997b1717c12b93
michel1991 <michelmvoumamvouma@gmail.com>
2023-03-25 15:11:44 +0100

    refactor: move file into correct folder

---
+++ advancedDesign/runtime/ClassCastExceptionUnrealatedClass.java
@@ -0,0 +1,17 @@
+
+package advancedDesign.runtime;
+public class ClassCastExceptionUnrealatedClass {
+    /**
+       Line 5 tries to cast an Integer to a String.
+       Since String does not extend Integer, this is not allowed, and a ClassCastException is thrown,
+       making option C correct.
+       If line 5 were removed, then the code would instead produce a NullPointerException on line 7.
+       Since the program stops after line 5, though, line 7 is never reached.
+     */
+    public static void main(String... args){
+        Object obj = Integer.valueOf(3);
+         String str = (String) obj; // 5
+        obj = null;
+        System.out.println(obj.equals(null));
+    }
+}
