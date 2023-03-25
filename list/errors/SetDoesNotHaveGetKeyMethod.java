1d0ef1484328d9852b423492d29a3d971b763b0f
michel1991 <michelmvoumamvouma@gmail.com>
2023-03-25 19:58:07 +0100

    error-code:  Set does not have a getKey()

---
+++ list/errors/SetDoesNotHaveGetKeyMethod.java
@@ -0,0 +1,24 @@
+package list.errors;
+import java.util.*;
+
+public class SetDoesNotHaveGetKeyMethod {
+    /**
+      What does the following output?
+        A. peter
+        B. wendy
+        C. Does not compile due to line x1
+        D. Does not compile due to line x2
+        E. Does not compile due to another reason
+        F. Throws an exception at runtime
+
+        Line x1 returns a Set of map entries.
+        Set does not have a getKey() method, so line x2 does not compile, and option D is the answer.
+     */
+   static void add(){
+       var names = new HashMap<String, String>();
+       names.put("peter", "pan");
+       names.put("wendy", "darling");
+       var first = names.entrySet();        // line x1
+       System.out.println(first.getKey());  // line x2
+   }
+}
