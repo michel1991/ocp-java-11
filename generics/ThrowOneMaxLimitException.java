ad3748663a5944c62ff0fab9646907f6562a5e4e
michel1991 <michelmvoumamvouma@gmail.com>
2023-03-26 18:00:42 +0200

    code: choice correct list of exception according to generics

---
+++ generics/ThrowOneMaxLimitException.java
@@ -0,0 +1,23 @@
+package generics;
+import java.io.*;
+import java.util.*;
+
+/**
+ * Which of the following fills in the blank so this code compiles?
+    A. ?
+    B. ? extends RuntimeException
+    C. ? super RuntimeException
+    D. None of the above
+
+    Since the method does not have any declared exceptions,
+    it can only throw unchecked exceptions.
+    Option B is the only one that requires
+    the elements of coll to be RuntimeException or any subclasses.
+ */
+public class ThrowOneMaxLimitException {
+    public static void throwOne(Collection<? extends RuntimeException> coll) { // Collection<__________> coll
+        var iter = coll.iterator();
+        if (iter.hasNext())
+            throw iter.next();
+    }
+}
