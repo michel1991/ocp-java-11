9fa75a109bc13fca06b52aa2ef96f8a1ea64aa44
michel1991 <michelmvoumamvouma@gmail.com>
2023-03-26 18:45:16 +0200

    code:flat map operation

--- stream/FlatMapOperations.java
+++ stream/FlatMapOperations.java
@@ -74,6 +74,32 @@ public class FlatMapOperations {
                 System.out.println(result); // 32:
     }
 
+    /**
+      What does the following output?
+        A. [tire-][wheel-]
+        B. tire-wheel-
+        C. None of the above.
+        D. The code does not compile
+
+        The flatMap() method is used to turn a stream of collections
+        into a one-dimensional stream.
+        This means it gets rid of the empty list and flattens the other two.
+        Option A is incorrect because this is the output you’d get using the regular map() method.
+        Option B is correct because it flattens the elements.
+        Notice how it doesn’t matter that all three elements are different types of Collection implementations.
+     */
+    public static void printireUnderscoreTWheelUnderscoreT(){
+        Set<String> set = new HashSet<>();
+        set.add("tire-");
+        List<String> list = new LinkedList<>();
+        Deque<String> queue = new ArrayDeque<>();
+        queue.push("wheel-");
+        Stream.of(set, list, queue)
+           .flatMap(x -> x.stream())
+           .forEach(System.out::print);
+
+    }
+
     public static void main(String... args){
       printlnWithLimitFirstOperation();
       System.out.println(); 
