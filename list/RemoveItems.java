33c714191ee51db0138caec3a667efdef78265c0
michel1991 <michelmvoumamvouma@gmail.com>
2023-03-27 07:04:08 +0200

    code: list remove item

--- list/RemoveItems.java
+++ list/RemoveItems.java
@@ -68,4 +68,30 @@ public class RemoveItems {
         System.out.println(pennies); //  18:
 
     }
+
+    /**
+       What is the result of the following?
+        A. 1
+        B. 2
+        C. 3
+        D. None of the above
+
+        On a stream, the filter() method only keeps values matching the lambda.
+        The removeIf() does the reverse on a Collection and keeps
+        the elements that do not match. In this case, that is Austin and Boston, so option B is correct.
+     */
+    static void removeItemStringThatLengthIsLessThanTen(){
+        var list = new ArrayList<String>();
+        list.add("Austin");
+        list.add("Boston");
+        list.add("San Francisco");
+
+        list.removeIf(a -> a.length() > 10);
+        System.out.println(list.size());
+
+    }
+
+    public static void main(String... args){
+
+    }
 }
