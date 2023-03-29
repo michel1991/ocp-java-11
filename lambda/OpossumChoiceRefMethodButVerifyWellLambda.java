114fff16908f57704adf2cd072ebe18bf00fa811
michel1991 <michelmvoumamvouma@gmail.com>
2023-03-29 04:51:09 +0200

    code: choice correct ref method

---
+++ lambda/OpossumChoiceRefMethodButVerifyWellLambda.java
@@ -0,0 +1,33 @@
+package lambda;
+import java.util.function.*;
+
+/**
+   Which method reference can replace
+   the lambda in the first line of the main() method to produce the same output?
+
+    A. System:out:println
+    B. System::out:println
+    C. System::out::println
+    D. System.out::println
+    E. None of the above
+
+    The only one of these references to compile is option D.
+     However, the original code prints Carrying 1.
+     The version with a method reference would just print 1.
+     Option E is the answer because this is not the same output.
+ */
+interface Marsupial {
+    void carryInPouch(int size);
+}
+ class Opossum {
+    public static void main(String[] args) {
+        Marsupial mar = x -> System.out.println("Carrying " + x);
+
+        mar.carryInPouch(1);
+    }
+}
+public class OpossumChoiceRefMethodButVerifyWellLambda {
+    public static void main(String[] args) {
+        Opossum.main(args);
+    }
+}
