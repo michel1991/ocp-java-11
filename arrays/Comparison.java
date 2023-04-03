ea410c9b41c3af81ebb80ee8339a4c7094afa794
michel1991 <michelmvoumamvouma@gmail.com>
2023-04-04 01:19:09 +0200

    code: array comparison

---
+++ arrays/Comparison.java
@@ -0,0 +1,27 @@
+package arrays;
+import java.util.*;
+
+public class Comparison {
+    /**
+     * What is true about the output of the following code?
+
+        A. It is negative because ints has fewer elements.
+        B. It is 0 because the arrays can’t be compared.
+        C. It is positive because the first element is larger.
+        D. It is undefined.
+        E. The code does not compile.
+        
+     The Arrays.compare() method looks at each element in turn.
+     Since the first elements are different, we get the result of comparing them.
+     In this case, we get a positive number because 3 is larger than 2, and option C is correct.
+     */
+  static void compareWithDifferentSize(){
+      var ints = new int[] {3,1,4};
+      var others = new int[] {2,7,1,8};
+      System.out.println(Arrays.compare(ints, others));
+  }
+    
+    public static void main(String... args){
+        compareWithDifferentSize();
+    }
+}
