e51bc63731c1226f2e1e3db2b18db23c6c825e2f
michel1991 <michelmvoumamvouma@gmail.com>
2023-04-04 01:42:37 +0200

    code: arrays declaration

--- arrays/ArrayDeclaration.java
+++ arrays/ArrayDeclaration.java
@@ -24,4 +24,24 @@ public class ArrayDeclaration {
         Object[][][] cubbies = new Object[3][0][5];
         java.util.Date[] dates[] = new java.util.Date[2][];
    }
+
+    /**
+     * Which of these four array declarations produces a different array than the others?
+        A. int[][] nums = new int[2][1];
+        B. int[] nums[] = new int[2][1];
+        C. int[] nums[] = new int[][] { { 0 }, { 0 } };
+        D. int[] nums[] = new int[][] { { 0, 0 } };
+
+       Options A and B show that the brackets, [], can be before or after
+       the variable name and produce the same array.
+       Option C specifies the same array the long way with two arrays of length 1.
+       Option D is the answer because it is different than the others.
+       It instead specifies an array of length 1 where that element is of length 2.
+     */
+    static void differentArray(){
+        int[][] nums = new int[2][1];
+        int[] nums2[] = new int[2][1];
+        int[] nums3[] = new int[][] { { 0 }, { 0 } };
+        int[] nums4[] = new int[][] { { 0, 0 } };
+    }
 }
