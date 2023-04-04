9147ba90b274a3f86ce86a868ac40fed5720366d
michel1991 <michelmvoumamvouma@gmail.com>
2023-04-04 02:30:07 +0200

    code: load resource bundle

--- internationalization/en_properties/LoadResources.java
+++ internationalization/en_properties/LoadResources.java
@@ -96,6 +96,53 @@ public class LoadResources {
 
     }
 
+
+        /**
+           Assuming the key turquoise is in all five of the files referenced in the options,
+           which file will the following code use for the resource bundle?
+            A. Colors_en.properties
+            B. Colors_CN.properties
+            C. Colors.properties
+            D. Colors_default.properties
+            E. Colors_en_CN.properties
+            F. None of the above
+
+            Java starts out by looking for a properties file with the requested locale,
+           which in this case is the zh_CN language and country.
+           It doesn’t find it, so it moves onto the locale with just a language code zh, which it also does not find.
+            It then moves on to the default locale en_US, but there is still no match.
+           It drops the country code and does find a match with en, making option A correct
+         */
+    static void loadWithTurquoiseKey(){
+        Locale loc = new Locale("zh", "CN");
+        Locale.setDefault(new Locale("en", "US"));
+        ResourceBundle rb = ResourceBundle.getBundle("Colors", loc);
+        rb.getString("turquoise");
+    }
+
+    /**
+        Assuming the key green is in all five of
+        the files referenced in the options,
+        which file will the following code use for the resource bundle?
+
+        A. Colors_default.properties
+        B. Colors.properties
+        C. Colors_en.properties
+        D. Colors_US.properties
+        E. Colors_en_US.properties
+        F. None of the above
+
+        Java starts out by looking for a properties file with the requested locale,
+         which in this case is the fr language.
+         It doesn’t find it, so it moves onto the default localeen_US,
+         * which it does find, making option E correct.
+     */
+    static void loadGrrenKey(){
+        Locale.setDefault(new Locale("en", "US"));
+        var rb = ResourceBundle.getBundle("Colors", new Locale("fr"));
+        System.out.print(rb.getString("green"));
+    }
+
     public static void main(String... args){
         load();
     }
