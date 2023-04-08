dc03927378331372fbbc1f0bf54d7ddbb7ae55c9
michel1991 <michelmvoumamvouma@gmail.com>
2023-04-08 22:42:43 +0200

    code: stream conversion

--- stream/mapping/StreamConversion.java
+++ stream/mapping/StreamConversion.java
@@ -21,7 +21,28 @@ public class StreamConversion {
        IntStream is = stream.mapToInt(Integer::parseInt);
   }
 
-    public static void main(String... args){
+   static void mapAndMapToInt(){
 
+   }
+
+   /**
+    * Which of the following can fill in the blank to have the code print 44?
+         A. Only map
+         B. Only mapToInt
+         C. Only mapToObject
+         D. Both map and mapToInt
+         E. Both map and mapToObject
+         F. map, mapToInt, and mapToObject
+
+         The map() method can fill in the blank.
+         The lambda converts a String to an int, and Java uses autoboxing to turn that into an Integer.
+         The mapToInt() method can also fill in the blank, and Java doesn’t even need to autobox.
+         There isn’t a mapToObject() in the stream API. Note there is a similarly named mapToObj() method on IntStream.
+         Since both map() and mapToInt() work here, option D is correct.
+
+    */
+    public static void main(String... args){
+      var stream = Stream.of("base", "ball");
+      stream.mapToInt(s -> s.length()).forEach(System.out::print);
    }
 }
