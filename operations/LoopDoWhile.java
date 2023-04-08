a39f97d051a7b0e9d5098f1d05c3c8721e1117eb
michel1991 <michelmvoumamvouma@gmail.com>
2023-04-08 23:18:54 +0200

    code: do while loop

---
+++ operations/LoopDoWhile.java
@@ -0,0 +1,27 @@
+package operations;
+public class LoopDoWhile {
+    /**
+      What is the output of the following code snippet?
+     
+        A. done
+        B. inflate-done
+        C. The code does not compile.
+        D. This is an infinite loop.
+        E. None of the above.
+        
+        On the first iteration of the loop, the if statement executes and prints inflate-.
+        Then the loop condition is checked.
+        The variable balloonInflated is true, so the loop condition is false, and the loop completes and done is printed.
+     */
+   static void ballonInflated(){
+       boolean balloonInflated = false;
+       do {
+           if (!balloonInflated) {
+               balloonInflated = true;
+               System.out.print("inflate-");
+           }
+       } while (! balloonInflated);
+       System.out.println("done");
+       
+   }
+}
