dbd5a6fced3ad30c4fadc55393813a848c4d3279
michel1991 <michelmvoumamvouma@gmail.com>
2023-04-08 11:18:17 +0200

    refactor: move class into correct folder

--- io/serialization/runtime/ZebraSerializableMemberFieldObject.java
+++ io/serialization/runtime/ZebraSerializableMemberFieldObject.java
@@ -1,10 +1,12 @@
-package io.runtime;
+package io.serialization.runtime;
 import java.io.*;
 import java.util.*;
 
 /**
-    Which of the following fields will be null after an instance of the class created on line 15 is serialized
+    Which of the following fields will be null after
+    an instance of the class created on line 15 is serialized
     and then deserialized using ObjectOutputStream and ObjectInputStream? (Choose all that apply.)
+
     A. name
     B. stripes
     C. age
@@ -13,15 +15,15 @@ import java.util.*;
     F. The code does not compile.
     G. The code compiles but throws an exception at runtime.
 
-    * The code compiles, so option F is incorrect. To be serializable,
-    * a class must implement the Serializable interface, which Zebra does.
-    * It must also contain instance members that either are marked transient or are serializable.
-    * The instance member stripes is of type Object, which is not serializable.
-    * If Object implemented Serializable, then all objects would be serializable by default,
-    * defeating the purpose of having the Serializable interface. Therefore, the Zebra class is not serializable,
-    * with the program throwing an exception at runtime if serialized and making option G correct.
-    * If stripes were removed from the class, then options A and C would be the correct answers,
-    * as name and age are both marked transient.
+    The code compiles, so option F is incorrect. To be serializable,
+    a class must implement the Serializable interface, which Zebra does.
+    It must also contain instance members that either are marked transient or are serializable.
+    The instance member stripes is of type Object, which is not serializable.
+    If Object implemented Serializable, then all objects would be serializable by default,
+    defeating the purpose of having the Serializable interface. Therefore, the Zebra class is not serializable,
+    with the program throwing an exception at runtime if serialized and making option G correct.
+    If stripes were removed from the class, then options A and C would be the correct answers,
+    as name and age are both marked transient.
  */
 class Zebra implements Serializable { //3
     private transient String name = "George";  //4
