6f43f9d6095a5358118ecf09c76681ec5313057c
michel1991 <michelmvoumamvouma@gmail.com>
2023-04-13 12:53:53 +0200

    refactor: remove static class

--- creatingThreadsWithConcurrencyApi/introducingTheSingleThreadExecutor/ZooInfo.java
+++ creatingThreadsWithConcurrencyApi/introducingTheSingleThreadExecutor/ZooInfo.java
@@ -2,32 +2,27 @@ package creatingThreadsWithConcurrencyApi.introducingTheSingleThreadExecutor;
 import java.util.concurrent.*;
 
 class ZooInfo{
-  static void basicExample(){
+    public static void main(String[] args){
       ExecutorService  service = null;
-       Runnable task1 = () -> System.out.println("Printing zoo inventory");
-       Runnable task2 = () -> {
-         for(int i = 0; i < 3; i++)
-           System.out.println("Printing record:"+i);
+      Runnable task1 = () -> System.out.println("Printing zoo inventory");
+      Runnable task2 = () -> {
+          for(int i = 0; i < 3; i++)
+              System.out.println("Printing record:"+i);
       };
 
       try{
-            service = Executors.newSingleThreadExecutor();
-             System.out.println("begin");
-             service.execute(task1);
-             service.execute(task2);
-             service.execute(task1);
-             System.out.println("end");
+          service = Executors.newSingleThreadExecutor();
+          System.out.println("begin");
+          service.execute(task1);
+          service.execute(task2);
+          service.execute(task1);
+          System.out.println("end");
       } finally{
           if(service != null){
               service.shutdown();
-	      System.out.println("call finally");
-           }
+              System.out.println("call finally main thread ");
+          }
       }
-  }
-  
-
-    public static void main(String[] args){
-         basicExample();  
    }
  
 }
