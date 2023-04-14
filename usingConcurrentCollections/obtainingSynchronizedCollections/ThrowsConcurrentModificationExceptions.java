d7c0d535925e69255ca08b3bb04a2222f84e9ff4
michel1991 <michelmvoumamvouma@gmail.com>
2023-04-14 14:36:16 +0200

    runtime-error-obtaining-synchronized-collections: list throw concurrent modification exception

---
+++ usingConcurrentCollections/obtainingSynchronizedCollections/ThrowsConcurrentModificationExceptions.java
@@ -0,0 +1,13 @@
+package usingConcurrentCollections.obtainingSynchronizedCollections;
+import java.util.*;
+public class ThrowsConcurrentModificationExceptions {
+    public static void main(String[] args){
+        var foodData = new HashMap<String, Object>();
+        foodData.put("penguin", 1);
+        foodData.put("flamingo", 2);
+
+        var synFoodData = Collections.synchronizedMap(foodData);
+        for(String key : synFoodData.keySet() )
+            synFoodData.remove(key);
+    }
+}
