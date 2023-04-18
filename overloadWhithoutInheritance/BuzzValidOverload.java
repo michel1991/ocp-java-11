f5527931cd1ee2b7767e166651a04037275a68ba
michel1991 <michelmvoumamvouma@gmail.com>
2023-04-18 20:37:43 +0200

    style: format responses answer

--- overloadWhithoutInheritance/BuzzValidOverload.java
+++ overloadWhithoutInheritance/BuzzValidOverload.java
@@ -9,11 +9,13 @@ package overloadWhithoutInheritance;
     E. public void buzz(boolean softly) { }
     F. public void buzzLouder() { }
 
-  Options B and C are incorrect because they have the same name and empty parameter list as the original.
+  Options B and C are incorrect because they have
+  the same name and empty parameter list as the original.
   One of these must differ to be an overload.
   Option F is incorrect because the method name is different than the original.
   It is a perfectly fine method, but it isn’t an overload.
-  Options A, D, and E are correct overloads because the method name is the same and the method parameter list is different.
+  Options A, D, and E are correct overloads because
+  the method name is the same and the method parameter list is different.
   Anything else is allowed to vary. Remember that Java is case sensitive.
  */
 public class BuzzValidOverload {
--- overloadWhithoutInheritance/VarargsInMethod.java
+++ overloadWhithoutInheritance/VarargsInMethod.java
@@ -11,11 +11,14 @@ public class VarargsInMethod {
         E. public void moreE(String[] values, ...int nums) {}
         F. public void moreG(String[] values, int[] nums) {}
 
-        Options A and B are correct because the single varargs parameter is the last parameter declared.
+        Options A and B are correct because the single varargs parameter
+        is the last parameter declared.
         Option F is correct because it doesn’t use any varargs parameters.
         Option C is incorrect because the varargs parameter is not last.
-        Option D is incorrect because two varargs parameters are not allowed in the same method.
-        Option E is incorrect because the ... for a varargs must be after the type, not before it.
+        Option D is incorrect because two varargs
+        parameters are not allowed in the same method.
+        Option E is incorrect because the ... for
+        a varargs must be after the type, not before it.
 
      */
     public void moreA(int... nums) {
