91ba767e4b0d0364622cc354a2dcf7b64bbb7ac5
michel1991 <michelmvoumamvouma@gmail.com>
2023-04-20 06:18:36 +0200

    code: add main method, when second size of array is not define it is null

--- arrays/ArrayDeclaration.java
+++ arrays/ArrayDeclaration.java
@@ -1,4 +1,5 @@
 package arrays;
+import java.util.*;
 public class ArrayDeclaration {
     /**
     *   Which of these array declarations is not legal? (Choose all that apply.)
@@ -11,7 +12,8 @@ public class ArrayDeclaration {
 
        Option C uses the variable name as if it were a type, which is clearly illegal.
        Options E and F don’t specify any size.
-       Although it is legal to leave out the size for later dimensions of a multidimensional array, the first one is required.
+       Although it is legal to leave out the size
+       for later dimensions of a multidimensional array, the first one is required.
        Option A declares a legal 2D array. Option B declares a legal 3D array.
        Option D declares a legal 2D array.
        Remember that it is normal to see on the exam types you might not have learned.
@@ -20,9 +22,13 @@ public class ArrayDeclaration {
 
      */
    static void firstExample(){
+        System.out.println("start first exemple");
         int[][] scores = new int[5][];
+        System.out.println(Arrays.toString(scores[0])); // size of second table are null
         Object[][][] cubbies = new Object[3][0][5];
         java.util.Date[] dates[] = new java.util.Date[2][];
+        System.out.println("end start first exemple");
+        System.out.println();
    }
 
     /**
@@ -43,5 +49,9 @@ public class ArrayDeclaration {
         int[] nums2[] = new int[2][1];
         int[] nums3[] = new int[][] { { 0 }, { 0 } };
         int[] nums4[] = new int[][] { { 0, 0 } };
+    }
+
+    public static void main(String... zrgs){
+        firstExample();
     }
 }
