d2e1c7cb6bd9b142fb2e8fe3c398b1c209098951
michel1991 <michelmvoumamvouma@gmail.com>
2023-04-20 20:31:04 +0200

    code: sort a list

---
+++ compare/SortOnly.java
@@ -0,0 +1,28 @@
+package compare;
+import java.util.*;
+
+public class SortOnly {
+    
+    /**
+     *  What is the output of the following?
+     *  A. [3, three, THREE]
+        B. [3, THREE, three]
+        C. [three, THREE, 3]
+        D. [THREE, three, 3]
+        E. None of the above
+
+     Unfortunately, you do have to memorize two facts about sort order.
+     First, numbers sort before letters.
+     Second, uppercase sorts before lowercase.
+     Since, the first value is 3 and the last is three, option B is correct.
+     */
+  static void ordersIsNumberUpperCaseLowerCase(){
+      var threes = Arrays.asList("3", "three", "THREE");
+      Collections.sort(threes);
+      System.out.println(threes);
+  }
+    
+    public static void main(String[] args) {
+        ordersIsNumberUpperCaseLowerCase();
+    }
+}
