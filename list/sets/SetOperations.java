36b92f8f92ec57c9cca19807b9e834a3631a068d
michel1991 <michelmvoumamvouma@gmail.com>
2023-04-25 17:59:36 +0200

    code: add to set

--- list/sets/SetOperations.java
+++ list/sets/SetOperations.java
@@ -25,7 +25,38 @@ public class SetOperations {
 
   }
 
+    /**
+     * What is the result of the following?
+        A. 2 3
+        B. 2 56
+        C. 3 3
+        D. 3 56
+        E. None of the above
+
+        First the code creates an ArrayList of three elements.
+        Then the list is transformed into a TreeSet.
+        Since sets are not allowed to have duplicates,
+        the set only has two elements. Remember that a TreeSet is sorted,
+        which means that the first element in the TreeSet is 3.
+        Therefore, option A is correct.
+     */
+    public static void addToSet(){
+        System.out.println(" Begin Add To Set " );
+        var list = new ArrayList<Integer>();
+        list.add(56);
+        list.add(56);
+        list.add(3);
+
+        var set = new TreeSet<Integer>(list);
+        System.out.print(set.size());
+        System.out.print(" " );
+        System.out.print(set.iterator().next());
+        System.out.println();
+        System.out.println(" End Add To Set " );
+    }
+
     public static void main(String... args){
         addToHashSet();
+        addToSet();
     }
 }
