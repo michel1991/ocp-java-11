01d59d196ad6af2df26a146dfe78f6ef955b528e
michel1991 <michelmvoumamvouma@gmail.com>
2023-04-25 08:04:35 +0200

    refactor: identation of line answer

--- nio/cleanPathAndNotCleanPath/ResolveDoesNotNormalize.java
+++ nio/cleanPathAndNotCleanPath/ResolveDoesNotNormalize.java
@@ -5,9 +5,13 @@ import java.nio.file.*;
 public class ResolveDoesNotNormalize {
 
     /**
-        The code compiles and runs without issue, so option E is incorrect. For this question, you have to remember two things.
-        First, the resolve() method does not normalize any path symbols, so options A and B are not correct. Second,
-         calling resolve() with an absolute path as a parameter returns the absolute path, so option C is correct, and option D is incorrect.
+        The code compiles and runs without issue, so option E is incorrect.
+        For this question, you have to remember two things.
+        First, the resolve() method does not normalize any path symbols,
+        so options A and B are not correct.
+        Second, calling resolve() with an absolute path
+        as a parameter returns the absolute path,
+        so option C is correct, and option D is incorrect.
      */
     static void firstExample(){
         var path1 = Path.of("/pets/../cat.txt");
@@ -27,7 +31,8 @@ public class ResolveDoesNotNormalize {
 
         First, p2 is an absolute path, which means that p1.resolve(p2) just returns p2.
         For this reason, options B and C are incorrect.
-        Since p1 is a relative path, it is appended onto p2, making option D correct and option A incorrect.
+        Since p1 is a relative path, it is appended onto p2,
+        making option D correct and option A incorrect.
         Option A would be correct if normalize() was applied.
      */
     static void secondExample(){
