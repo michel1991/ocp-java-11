ca194069146d2c6a8e21c1d9417bdb6c90fdb1e1
michel1991 <michelmvoumamvouma@gmail.com>
2023-05-04 16:58:57 +0200

    style: format answer

--- stream/matchMethods/AllMatch.java
+++ stream/matchMethods/AllMatch.java
@@ -12,17 +12,23 @@ public class AllMatch {
         E. noneMatch
         F. None of the above
 
-    Options C and D do not compile because these methods do not take a Predicate parameter and do not return a boolean.
-    When working with streams, it is important to remember the behavior of the underlying functional interfaces.
+    Options C and D do not compile because these methods do not
+    take a Predicate parameter and do not return a boolean.
+    When working with streams, it is important to remember
+    the behavior of the underlying functional interfaces.
     Options B and E are incorrect. While the code compiles, it runs infinitely.
     The stream has no way to know that a match won’t show up later.
-    Option A is correct because it is safe to return false as soon as one element passes through the stream that doesn’t match.
+    Option A is correct because it is safe to return
+    false as soon as one element passes through the stream that doesn’t match.
 
-    *
      */
    public static void runInfinitely(){
        var s = Stream.generate(() -> "meow");
        var match = s.allMatch(String::isEmpty);  // .________________
        System.out.println(match);
    }
+    
+    public static void main(String... args){
+        runInfinitely();
+    }
 }
