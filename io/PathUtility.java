793811f6d21606b192df9a98d8d60eca9e458320
michel1991 <michelmvoumamvouma@gmail.com>
2023-05-04 14:31:04 +0200

    code: give path of resource according to package name of learn and name of resource folder

--- io/PathUtility.java
+++ io/PathUtility.java
@@ -14,6 +14,20 @@ public class PathUtility {
        
        return  pathToResourceFile;
    }
+   
+   public static StringBuilder getResourcePath(String packageName, String resourceFolder){
+      String rootPath = System.getProperty("user.dir");
+      String fileSeparator = System.getProperty("file.separator");
+      StringBuilder pathToResourceFile = new StringBuilder(rootPath);
+      pathToResourceFile
+                         .append(fileSeparator)
+                         .append(packageName)
+                         .append(fileSeparator)
+                         .append(resourceFolder)
+       ;
+
+      return  pathToResourceFile;
+   }
 
     public static String getFileSeparator(){
        return System.getProperty("file.separator");
