c7712105b1e2fbc3e48db45a46a66d1be5aee095
michel1991 <michelmvoumamvouma@gmail.com>
2023-06-09 15:30:32 +0200

    refactor: move method into correct class

--- internationalization/en_properties/LoadResources.java
+++ internationalization/en_properties/LoadResources.java
@@ -1,40 +1,7 @@
 package internationalization.en_properties;
 import java.util.*;
 public class LoadResources {
-    /**
-    * Suppose that we have the following property files and code. Which bundle is used on lines 8 and 9, respectively?
-        Dolphins.properties
-        name=The Dolphin
-        age=0
-
-        Dolphins_de.properties
-        name=Dolly
-        age=4
-
-        Dolphins_en.properties
-        name=Dolly
-
-        A. Dolphins.properties and Dolphins.properties are used.
-        B. Dolphins.properties and Dolphins_en.properties are used.
-        C. Dolphins_en.properties and Dolphins.properties are used.
-        D. Dolphins_en.properties and Dolphins_en.properties are used.
-        E. Dolphins_de.properties and Dolphins_en.properties are used.
-        F. The code does not compile.
-
-       Java will use Dolphins_en.properties as the matching resource bundle on line 7.
-       Since there is no match for French, the default locale is used.
-       Line 8 finds a matching key in this file. Line 9 does not find a match in that file; therefore, it has to look higher up in the hierarchy.
-       For more information, see Chapter 5.
-     */
-    static void load(){
-        Locale fr = new Locale("fr"); //   5:
-        Locale.setDefault(new Locale("en", "US")); //   6:
-        var b = ResourceBundle.getBundle("internationalization.en_properties.Dolphins", fr); //  7:
-        b.getString("name"); //  8:
-        b.getString("age"); //  9:
-
-        System.out.println(" name : " +   b.getString("name") + " age : " +  b.getString("age"));
-    }
+    
 
     
 
@@ -115,7 +82,5 @@ public class LoadResources {
         System.out.print(rb.getString("green"));
     }
 
-    public static void main(String... args){
-        load();
-    }
+   
 }
