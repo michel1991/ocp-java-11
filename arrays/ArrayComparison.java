ef4803c3339ee7a9cf4dfd32dc04b8b1f166552f
michel1991 <michelmvoumamvouma@gmail.com>
2023-06-12 23:47:15 +0200

    refactor: move file into correct directories

--- arrays/ArrayComparison.java
+++ arrays/ArrayComparison.java
@@ -1,4 +1,4 @@
-package list.comparison;
+package arrays;
 import java.util.*;
 
 public class ArrayComparison {
@@ -15,12 +15,17 @@ public class ArrayComparison {
         H. The code does not compile.
 
         The array is allowed to use an anonymous initializer because it is in the same line as the declaration.
-        The ArrayList uses the diamond operator. This specifies the type matches the one on the left without having to retype it.
-        After adding the two elements, list contains [6, 8]. We then replace the element at index 1 with 9, resulting in [6, 9].
-        Finally, we remove the element at index 0, leaving [9]and making option B correct.
-        Option C is incorrect because arrays output something that looks like a reference that rather than a nicely printed list of values.
-        Option E is correct because the compare() method returns 0 when the arrays are the same length and have the same elements.
-        Option F is correct because the mismatch() method returns a -1 when the arrays are equivalent. For more information, see Chapter 5.
+        The ArrayList uses the diamond operator.
+        This specifies the type matches the one on the left without having to retype it.
+        After adding the two elements, list contains [6, 8].
+        We then replace the element at index 1 with 9, resulting in [6, 9].
+        Finally, we remove the element at index 0, leaving [9] and making option B correct.
+        Option C is incorrect because arrays output something that looks
+        like a reference that rather than a nicely printed list of values.
+        Option E is correct because the compare() method returns 0
+        when the arrays are the same length and have the same elements.
+        Option F is correct because the mismatch() method returns a -1
+        when the arrays are equivalent. For more information, see Chapter 5.
      */
    static void withMismatch(){
        int[] array = {6,9,8};
