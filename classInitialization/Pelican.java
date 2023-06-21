8c6c7e477c966119e54baa19d60ff2237b4343db
michel1991 <michelmvoumamvouma@gmail.com>
2023-06-22 00:08:58 +0200

    style: make indentation

--- classInitialization/Pelican.java
+++ classInitialization/Pelican.java
@@ -9,17 +9,19 @@ package classInitialization;
     E. The code contains a compilation error.
     F. None of the above
 
-    The code compiles without issue. The question is making sure you know that superclass constructors
+    The code compiles without issue.
+    The question is making sure you know that superclass constructors
     are called in the same manner in abstract classes as they are in nonabstract classes.
     Line 9 calls the constructor on line 6.
     The compiler automatically inserts super() as the first line of the constructor defined on line 6.
-    The program then calls the constructor on line 3 and prints Wow-. Control then returns to line 6,
-    and Oh- is printed.
+    The program then calls the constructor on line 3 and prints Wow-.
+    Control then returns to line 6, and Oh- is printed.
     Finally, the method call on line 10 uses the version of fly() in the Pelican class,
     since it is marked private and the reference type of var is resolved as Pelican.
     The final output is Wow-Oh-Pelican, making option D the correct answer.
     Remember that private methods cannot be overridden.
-    If the reference type of chirp was Bird, then the code would not compile as it would not be accessible outside the class.
+    If the reference type of chirp was Bird,
+    then the code would not compile as it would not be accessible outside the class.
  */
 abstract class Bird { // 1:
     private final void fly() { System.out.println("Bird"); } //   2:
