a193fb92998a1b66aa331dd00d5f2e416acf7f2e
michel1991 <michelmvoumamvouma@gmail.com>
2023-08-05 11:34:29 +0200

    error: cannot overloading generic method in child class due to erasure of the type

---
+++ overridingAGenericMethod/reviewOfOverloadingAGenericMethod/Anteater.java
@@ -0,0 +1,8 @@
+package overridingAGenericMethod.reviewOfOverloadingAGenericMethod;
+import java.util.*;
+
+public class Anteater extends LongTailAnimal{
+  protected void chew(List<Double> input){
+
+  }
+}
---
+++ overridingAGenericMethod/reviewOfOverloadingAGenericMethod/LongTailAnimal.java
@@ -0,0 +1,9 @@
+package overridingAGenericMethod.reviewOfOverloadingAGenericMethod;
+import java.util.*;
+
+public class LongTailAnimal{
+  protected void chew(List<Object> input){
+
+  }
+
+}
