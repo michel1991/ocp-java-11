b47d7344ba8a4f4cd9ae9f03108aa1a576dffe0c
michel1991 <michelmvoumamvouma@gmail.com>
2023-09-13 04:07:36 +0200

    code: add static method

--- internationalization/numberFormat/DecimalFormatWrapper.java
+++ internationalization/numberFormat/DecimalFormatWrapper.java
@@ -46,17 +46,23 @@ public class DecimalFormatWrapper {
         F. #,##.#
 
         Options B and D correctly print the same string value in the specified format.
-        Option A is incorrect because ˂06.92˃ is printed instead of <06.9>.
+        Option A is incorrect because <06.92> is printed instead of <06.9>.
         Options C and E are incorrect, because (among other things) commas are
         printed as part of both of the first two values.
         Option F is incorrect because <2.1><6.9> is printed instead of <02.1><06.9>
      */
     static void choicePattern(){
         // String pattern = "_______________";
-        String pattern = " ##,00.#"; // #,00.#
+        String pattern = "##,00.#"; // #,00.#
+      System.out.println("For what values of pattern will the following print <02.1><06.9><10,00>?");
         var message = DoubleStream.of(2.1, 6.923, 1000)
                 .mapToObj(v -> new DecimalFormat(pattern).format(v))
                  .collect(Collectors.joining("><"));
               System.out.print("<" + message + ">");
+       System.out.println("For what values of pattern will the following print <02.1><06.9><10,00>?");
+    }
+
+    public static void main(String... args){
+        choicePattern();
     }
 }
