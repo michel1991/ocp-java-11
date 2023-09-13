24d4367485804ed77cb9254987586bffd8943337
michel1991 <michelmvoumamvouma@gmail.com>
2023-09-13 06:25:23 +0200

    refactor: move file

--- inheritance/errors/breakMultipleRule/OverrideReturnTypeIsNotConvariant.java
+++ inheritance/errors/breakMultipleRule/OverrideReturnTypeIsNotConvariant.java
@@ -1,4 +1,4 @@
-package inheritance.errors;
+package inheritance.errors.breakMultipleRule;
 
 /**
 *
@@ -9,14 +9,18 @@ package inheritance.errors;
     D. The IsSea declaration compiles because it is not required to implement any of the methods it inherits.
     E. The Ocean declaration compiles.
     F. The variable color is inherited by the Ocean class.
-    *
+    
     The HasWater and IsSalty interfaces compile, making options A and B correct.
     Option C is incorrect, as an interface can extend multiple interfaces.
-    That said, the IsSalty interface still does not compile, but the reason is that the return types of Boolean and boolean are not covariant;
-    in other words, one is not a subtype of the other. For this reason, option D is also incorrect.
-    Even though IsSalty is an abstract type, the compiler can detect no concrete method could ever override both inherited abstract methods simultaneously.
+    That said, the IsSalty interface still does not compile, but the reason
+    is that the return types of Boolean and boolean are not covariant;
+    in other words, one is not a subtype of the other.
+    For this reason, option D is also incorrect.
+    Even though IsSalty is an abstract type, the compiler
+    can detect no concrete method could ever override both inherited abstract methods simultaneously.
     Option E is incorrect, as a class cannot be marked both abstract and final.
-    Finally, option F is correct, as interface variables are implicitly public and inherited in the classes that implement the interface.
+    Finally, option F is correct, as interface variables are implicitly
+    public and inherited in the classes that implement the interface.
  */
 interface HasWater {
     public boolean isBlue();
