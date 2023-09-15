6b025d5746d015bdad1a45c5feae680614a5ffca
michel1991 <michelmvoumamvouma@gmail.com>
2023-09-15 02:54:51 +0200

    refactor: move file

--- inheritance/multipleRules/Arthropod.java
+++ inheritance/multipleRules/Arthropod.java
@@ -1,4 +1,4 @@
-package inheritance;
+package inheritance.multipleRules;
 /**
 * What is the output of the following code?
 *   A. SpiderSpiderArthropod
@@ -11,12 +11,17 @@ package inheritance;
 
    The code compiles and runs without issue, so options E and F are incorrect.
     The Arthropod class defines two overloaded versions of the printName() method.
-    The printName() method that takes an int value on line 5 is correctly overridden in the Spider class on line 9.
-    Remember, an overridden method can have a broader access modifier, and protected access is broader than package-private access.
-    Because of polymorphism, the overridden method replaces the method on all calls, even if an Arthropod reference variable is used, as is done in the main() method.
+    The printName() method that takes an int value on line 5
+    is correctly overridden in the Spider class on line 9.
+    Remember, an overridden method can have a broader access modifier,
+    and protected access is broader than package-private access.
+    Because of polymorphism, the overridden method replaces the method on all calls,
+    even if an Arthropod reference variable is used, as is done in the main() method.
     For these reasons, the overridden method is called on lines 14 and 15, printing Spider twice.
-    Note that the short value is automatically cast to the larger type of int, which then uses the overridden method.
-    Line 16 calls the overloaded method in the Arthropod class, as the long value 5L does not match the overridden method, resulting in Arthropod being printed.
+    Note that the short value is automatically cast to the larger type of int,
+    which then uses the overridden method.
+    Line 16 calls the overloaded method in the Arthropod class,
+    as the long value 5L does not match the overridden method, resulting in Arthropod being printed.
     Therefore, option A is the correct answer.
  */
 public class Arthropod {
