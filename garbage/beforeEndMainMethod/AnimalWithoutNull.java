303b647115745395525f5587ca3cae843787cf05
michel1991 <michelmvoumamvouma@gmail.com>
2023-09-23 09:53:46 +0200

    refactor: move files

--- garbage/beforeEndMainMethod/AnimalWithoutNull.java
+++ garbage/beforeEndMainMethod/AnimalWithoutNull.java
@@ -1,17 +1,21 @@
-package garbage;
+package garbage.beforeEndMainMethod;
 
 public class AnimalWithoutNull {
 
 
    /**
-    * How many of the String objects are eligible for garbage collection right before the end of the main() method?
+     How many of the String objects are eligible for
+      garbage collection right before the end of the main() method?
+      
         A. None
         B. One
         C. Two
         D. Three
         E. None of the above
 
-       All three references point to the String object "lion". This makes the other two String objects eligible for garbage collection and makes option C correct.
+       All three references point to the String object "lion".
+       This makes the other two String objects eligible
+       for garbage collection and makes option C correct.
     */
     public static void main(String[] ohMy) {
         String animal1 = new String("lion");
