ce34127fdbfb5ee0e469e86796fbba216cd821bb
michel1991 <michelmvoumamvouma@gmail.com>
2023-09-23 11:19:04 +0200

    refactor: move files

--- garbage/beforeEndMainMethod/Chicken.java
+++ garbage/beforeEndMainMethod/Chicken.java
@@ -1,7 +1,7 @@
-package garbage;
+package garbage.beforeEndMainMethod;
 
 /**
- Suppose you have the following code. Which of the images
+  Suppose you have the following code. Which of the images
   best represents the state of the references c1, c2, and c3, right before
   the end of the main() method, assuming garbage collection hasn’t run? In the diagrams,
    each box represents a Chicken object with a number of eggs. (See chicken images)
@@ -13,12 +13,14 @@ package garbage;
     E. The code does not compile.
     F. None of the above.
 
-*
+
     Three instances of Chicken are created on lines 8–10. On line 11,
     the value of eggs in the first two instances is set to 2,
-    while the third instance has a value of 3. On line 12,
-    the original instance that was pointed to by c1 (with an eggs value of 2)
+    while the third instance has a value of 3.
+    
+    On line 12, the original instance that was pointed to by c1 (with an eggs value of 2)
     is dereferenced and eligible for garbage collection.
+    
     The c1 and c2 variables now both point to the same instance with an egg value of 2.
     Finally, on line 13, the eggs value for c3 is changed from 3 to null.
  */
