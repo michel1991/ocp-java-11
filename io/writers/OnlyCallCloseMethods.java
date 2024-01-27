c235519cf70d8f615adc9b1c4935ae6f0aa5b835
michel1991 <michelmvoumamvouma@gmail.com>
2024-01-27 14:33:28 +0100

    refactor: rename file

--- io/writers/OnlyCallCloseMethods.java
+++ io/writers/OnlyCallCloseMethods.java
@@ -4,11 +4,15 @@ package io.writers;
   Which of the following statements are correct?
   
     A. It will throw an exception if  text.txt does not exist.
+    
     B. It will create text.txt file in the filesystem if it does not exist.
+    
     C. It will not throw an exception if text.txt does not exist and it will not create a file either because nothing is being written to the file.
          It will create an empty file.
+         
     D. It will throw an exception if //1 is uncommented and if text.txt does not exist.
      If the file does not exist, it will be created and data will be written to it.
+     
     E. It will throw an exception if text.txt already exists.
         If the file already exists, it will be overwritten with a new file.
         To append to the existing file, the following constructor should be used.
@@ -17,7 +21,7 @@ package io.writers;
         Correct(E)
  */
 import java.io.*;
-public class Test{
+public class OnlyCallCloseMethods{
 public static void main(String[] args)  throws Exception {
        var fw = new FileWriter("text.txt");
        // fw.write("hello"); //1
