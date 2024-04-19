7faefd79b723b71a077605aad4f5d5c546737642
michel1991 <michelmvoumamvouma@gmail.com>
2024-04-19 06:05:18 +0200

    style: reduce line

--- collections/sorts/Book.java
+++ collections/sorts/Book.java
@@ -28,11 +28,15 @@ import java.util.*;
     
    A. 
     [non-fiction:Uri, non-fiction:Becoming, fiction:The Outsider]
-    The thenComparing method adds on a Comparator to the existing Comparator. This additional Comparator is used only when two objects are determined to be equal by the first Comparator.
-    Thus, in the given question, the books will be first sorted by their genre and title (i.e. if the genre of two books is same, then they will be sorted as per their title). This means the list will be ordered as :
+    The thenComparing method adds on a Comparator to the existing Comparator.
+    This additional Comparator is used only when two objects are determined to be equal by the first Comparator.
+    Thus, in the given question, the books will be first sorted by their genre and title
+    (i.e. if the genre of two books is same, then they will be sorted as per their title). This means the list will be ordered as :
     [fiction:The Outsider, non-fiction:Becoming, non-fiction:Uri]
     
-    Next, the reversed method generates a Comparator that reverses the order of the Comparator on which it is invoked. Thus, whatever is order of the list produced by Book::getGenre).thenComparing(Book::getTitle), that order is reversed. Therefore, the final order will be :
+    Next, the reversed method generates a Comparator that reverses the order of the Comparator on which it is invoked.
+    Thus, whatever is order of the list produced by Book::getGenre).thenComparing(Book::getTitle), that order is reversed.
+    Therefore, the final order will be :
     [non-fiction:Uri, non-fiction:Becoming, fiction:The Outsider]
     
    B.  [non-fiction:Becoming, non-fiction:Uri, fiction:The Outsider]
@@ -64,8 +68,10 @@ import java.util.*;
 public class Book {
     private String title;
     private String genre;
-    public Book(String title, String genre){
+    private int age;
+    public Book(String title, String genre, int age){
         this.title = title; this.genre = genre;
+        this.age = age;
     }
     //accessors and toString code not shown
     public String getTitle(){
@@ -76,11 +82,26 @@ public class Book {
         return genre;
     }
     
+    public int getAge(){
+        return age;
+    }
+
+    public String toString(){
+      var builder =  new StringBuilder("{ title=");
+      builder
+          .append(title)
+          .append(", genre=")
+          .append(genre)
+          .append(" }")
+      ;
+      return builder.toString();
+    }
+
     public static void main(String[] args) {
         var books = new ArrayList<Book>(List.of(
-                new Book("The Outsider", "fiction"),
-                new Book("Becoming", "non-fiction"),
-                new Book("Uri", "non-fiction")));
+                new Book("The Outsider", "fiction", 1),
+                new Book("Becoming", "non-fiction", 7),
+                new Book("Uri", "non-fiction", 9)));
 
         books.sort(Comparator.comparing(Book::getGenre)
          .thenComparing(Book::getTitle).reversed());
