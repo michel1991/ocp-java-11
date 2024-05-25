92f6e89cb83e5210c89464836e142163cc83991f
michel1991 <michelmvoumamvouma@gmail.com>
2024-05-25 20:43:12 +0200

    error: var is not allowed in a compound declaration

---
+++ advancedDesign/errors/vars/Discounter.java
@@ -0,0 +1,51 @@
+package advancedDesign.errors.vars;
+
+/**
+  What will the following code print when compiled and run?
+  
+    A. 3000
+    B. 3000.0
+    C. compilation error at //3
+         There are two problems here - 1. 'var' is not allowed in a compound declaration.
+         In other words, you can define only one variable using var.
+         2. Variables declared with var must be assigned a value in the declaration itself because without the value,
+         the compiler will not be able to infer the type of the variable.
+    D. compilation error at //4
+    E. compilation error at //5
+    F. Exception at run time.
+
+    ---------------------------------------------
+    Remember that static and instance variables are automatically assigned a value even if
+     you don't initialize them explicitly but local variables must be initialized explicitly before they are used.
+
+    Now, observe that the calc method declares local variables coupon, offset,
+    and base but does not assign them a value. Even though at run time, we know that since percent is 0 and is thus < 10,
+    a value will be assigned to these variables, the compiler doesn't know this because the compiler
+    doesn't take values of "variables" into consideration while determining the flow of control.
+    It only considers the values of compile time constants. Therefore, as far as the compiler is concerned,
+    coupon, offset, and base may remain uninitialized before they are used.
+
+    Having uninitialized variables itself is not a problem. So, if you change 'var' to 'int',
+    there will be no compilation error at //3. However, using them before they are initialized
+    is a problem and therefore the compiler will still flag an error at //5.
+
+    Had percent been defined as final ( static final double percent = 0; ), the code would work and print 3000.0.
+     Correct(C)
+
+ */
+public class Discounter {
+    static double percent; //1
+    int offset = 10, base= 50; //2
+    public static double calc(double value) {
+        var coupon, offset, base; //3
+        if(percent <10){ //4
+            coupon = 15;
+            offset = 20;
+            base = 10;
+        }
+        return coupon*offset*base*value/100; //5
+    }
+    public static void main(String[] args) {
+        System.out.println(calc(100));
+    }
+}
