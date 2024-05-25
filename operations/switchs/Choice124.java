8dcb7b73eed3bf1e3afb326f553858605ea6c12e
michel1991 <michelmvoumamvouma@gmail.com>
2024-05-25 16:32:06 +0200

    code: switch operation example

---
+++ operations/switchs/Choice124.java
@@ -0,0 +1,79 @@
+package operations.switchs;
+
+/**
+  Which of the following four constructs are valid?
+   1.
+      switch(5)
+      {
+            default :
+      }
+      
+   2.
+      
+           switch(5)
+        {
+            default : break;
+        }
+        
+    3. switch(8);
+   
+    4.
+      var x = 0;
+        switch(x){
+      }
+      
+    A. 1, 3
+    B. 1, 2, 3
+    C. 3, 4
+    D. 1, 2, 4
+        Code 3 is invalid because a switch statement must have a body. The body may even be empty as shown in Code 4.
+    E. All are valid.
+
+   ---------------------------------------------------------
+
+     Rules for a switch statement
+
+    1. Only String, byte, char, short, int, (and their wrapper classes Byte, Character, Short, and Integer),
+and enums can be used as types of a switch variable. String is allowed since Java 7.
+    2. The case constants must be assignable to the switch variable.
+For example, if your switch variable is of class String, your case labels must use Strings as well.
+    3. The switch variable must be big enough to hold all the case constants.
+For example, if the switch variable is of type char, then none of the case constants can be greater
+than 65535 because a char's range is from 0 to 65535. For example, the following will not compile because 200
+cannot be assigned to the variable b, which can only hold values from -128 to 127.
+
+    byte b = 10;
+    switch(b){
+        case 100 : System.out.print(100); //OK
+        case 200 :  System.out.print(200); //will not compile
+    }
+    
+    4. All case labels should be COMPILE TIME CONSTANTS. This means, you can have literals such as 'a', 5, 10, or a final variable,
+the value of which is computed at compile time such as int I = 2*3; For example, the following is valid:
+  
+    int x = 10;
+    final int I = 2*3;
+    switch(x){ 
+    case I : System.out.println(x); //this is valid because I is a compile time constant
+    }
+    
+    5. No two of the case constant expressions associated with a switch statement may have the same value.
+    6. The default label is optional and at most one default label may be associated with the same switch statement.
+    
+  
+     Correct D)
+
+ */
+public class Choice124 {
+    public static void main(String... args){
+        switch(5)
+        {
+                default :
+        }
+
+        
+        var x = 0;
+        switch(x){
+        }
+    }
+}
