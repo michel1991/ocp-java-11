5abe6eb3bfd6171c0dd5e47748562173e76f4541
michel1991 <michelmvoumamvouma@gmail.com>
2024-05-31 01:36:51 +0200

    docs: add explanation

--- security/guidelines/immutable/Person.java
+++ security/guidelines/immutable/Person.java
@@ -26,26 +26,26 @@ import java.io.*;
     What changes should be made to make this class immutable?
     
     A. Remove the call to clone() and assign the dob parameter directly to the dob field.
-       ---------------------------
+       ---------------------------------------------------------------------------------------
         cloning an input argument is important if the object referred to by the argument is mutable.
         Guideline 6-3 / MUTABLE-3: Create safe copies of mutable and subclassable input values
         
    B.
      validateDob method should be made private.
-     --------------------
+     --------------------------------------------------------------------------------------
     Generally, a constructor should not invoke a method that can be overridden because
     this gives the subclass an opportunity to mess with the creation logic of the instance.
     
     Guideline 7-4 / OBJECT-4: Prevent constructors from calling methods that can be overridden
     
-   C. erson class should be made final.
-      --------------------
+   C. Person class should be made final.
+      -----------------------------------------------------------------------------------
         Ideally, as per Guideline 6-1 / MUTABLE-1, an immutable class should not be subclassable.
         In this case, since the given class has a public constructor, this class should be made final.
         
    D.
        name and dob fields should be made final.
-       ---------------------------
+       ---------------------------------------------------------------------------------------------
         Ideally, as per Guideline 6-1 / MUTABLE-1, fields of immutable classes should be declared final.
         However, in this case, since there are no setter methods and the fields are private,
         it may be argued that the class is immutable even if the fields are not final.
