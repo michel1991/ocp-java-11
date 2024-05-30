bb30d25f0509d1216c78e86f8a03fa2162b74372
michel1991 <michelmvoumamvouma@gmail.com>
2024-05-31 01:45:12 +0200

    refactor: move file and add explanation

---
+++ security/guidelines/immutable/PersonCloneDobField.java
@@ -0,0 +1,56 @@
+package security.guidelines.immutable;
+/**
+ Gven:
+
+    public class Person{
+    private String name;
+    private java.util.Date dob;
+    public Person(String name, java.util.Date dob){
+        this.name = name;
+        this.dob = dob;
+    }
+    
+        public  String getName(){ return name; }
+        public  java.util.Date getDob(){ return dob; }
+    }
+    
+    
+    What, if anything, is wrong with the above code from secure coding guidlines perspective?
+    
+    A. There is nothing wrong.
+    B. setter methods for name and dob should be added.
+    C. name and dob should be set using setter methods instead of the constructor.
+    D. The constructor should clone name and dob parameters before assigning them to the instance fields.
+        ----------------------------------------------------------------
+        Only dob needs to be cloned because name is immutable already.
+    
+        Guideline 6-3 / MUTABLE-3: Create safe copies of mutable and subclassable input value
+        
+    E. The getDob method should return a clone of dob.
+        Date is not an immutable class. So, anyone having a reference to the same Date object can potentially change it.
+        This may cause serious problem with the Person instance.
+        For example, a rogue code might change the Date object pointed to by dob to a future date.
+        
+        Therefore, the constructor should clone the passed Date object and the getter method should return a clone of the dob field.
+        
+        String (and other immutable classes) fields do not have this issue because they are immutable.
+        So, even if other code gets access to such objects, it cannot change them.
+        
+        This is one of the reasons, the new java.time.LocalDate class is preferred instead of java.util.Date class.
+        
+        Guideline 6-2 / MUTABLE-2: Create copies of mutable output values
+    
+   
+     Correct(E)
+ */
+public class PersonCloneDobField {
+    private String name;
+    private java.util.Date dob;
+    public Person(String name, java.util.Date dob){
+        this.name = name;
+        this.dob = dob;
+    }
+    
+    public  String getName(){ return name; }
+    public  java.util.Date getDob(){ return dob; }
+}
--- security/guidelines/manyRules/Person.java
+++ security/guidelines/manyRules/Person.java
@@ -1,56 +1,76 @@
 package security.guidelines.manyRules;
-/**
- Gven:
+import java.io.*;
 
-    public class Person{
-    private String name;
-    private java.util.Date dob;
-    public Person(String name, java.util.Date dob){
-        this.name = name;
-        this.dob = dob;
+/**
+   Given:
+
+    class Person{
+        private String name;
+        private java.util.Date dob;
+
+        public Person(String name, java.util.Date dob){
+            this.name = name;
+            this.dob = (java.util.Date) dob.clone();
+            if(!validateDob(this.dob))
+                                    throw new IllegalArgumentException("dob cannot be in future");
+        }
+
+        protected boolean validateDob(java.util.Date dob){
+                return !dob.after(new java.util.Date());
+        }
+    
+        //getter methods for fields not shown
+    
     }
     
-        public  String getName(){ return name; }
-        public  java.util.Date getDob(){ return dob; }
-    }
+    What changes should be made to make this class immutable?
     
-    
-    What, if anything, is wrong with the above code from secure coding guidlines perspective?
-    
-    A. There is nothing wrong.
-    B. setter methods for name and dob should be added.
-    C. name and dob should be set using setter methods instead of the constructor.
-    D. The constructor should clone name and dob parameters before assigning them to the instance fields.
-        ---------------
-        Only dob needs to be cloned because name is immutable already.
-    
-        Guideline 6-3 / MUTABLE-3: Create safe copies of mutable and subclassable input value
+    A. Remove the call to clone() and assign the dob parameter directly to the dob field.
+       ---------------------------------------------------------------------------------------
+        cloning an input argument is important if the object referred to by the argument is mutable.
+        Guideline 6-3 / MUTABLE-3: Create safe copies of mutable and subclassable input values
         
-    E. The getDob method should return a clone of dob.
-        Date is not an immutable class. So, anyone having a reference to the same Date object can potentially change it.
-        This may cause serious problem with the Person instance.
-        For example, a rogue code might change the Date object pointed to by dob to a future date.
+   B.
+     validateDob method should be made private.
+     --------------------------------------------------------------------------------------
+    Generally, a constructor should not invoke a method that can be overridden because
+    this gives the subclass an opportunity to mess with the creation logic of the instance.
+
+    Guideline 7-4 / OBJECT-4: Prevent constructors from calling methods that can be overridden
+
+   C. Person class should be made final.
+      -----------------------------------------------------------------------------------
+        Ideally, as per Guideline 6-1 / MUTABLE-1, an immutable class should not be subclassable.
+        In this case, since the given class has a public constructor, this class should be made final.
         
-        Therefore, the constructor should clone the passed Date object and the getter method should return a clone of the dob field.
+   D.
+       name and dob fields should be made final.
+       ---------------------------------------------------------------------------------------------
+        Ideally, as per Guideline 6-1 / MUTABLE-1, fields of immutable classes should be declared final.
+        However, in this case, since there are no setter methods and the fields are private,
+        it may be argued that the class is immutable even if the fields are not final.
         
-        String (and other immutable classes) fields do not have this issue because they are immutable.
-        So, even if other code gets access to such objects, it cannot change them.
-        
-        This is one of the reasons, the new java.time.LocalDate class is preferred instead of java.util.Date class.
-        
-        Guideline 6-2 / MUTABLE-2: Create copies of mutable output values
-    
-   
-     Correct(E)
+        In such cases, it is not possible to determine the correct answer because
+        it depends on what the question writer at Oracle thinks is the right anwer.
+        It is best to use your judgement to select the correct options based on the number of correct options that you have to select
+
+     Correct(B, C)
+
  */
 public class Person {
     private String name;
     private java.util.Date dob;
+
     public Person(String name, java.util.Date dob){
-        this.name = name;
-        this.dob = dob;
+    this.name = name;
+    this.dob = (java.util.Date) dob.clone();
+    if(!validateDob(this.dob))
+                throw new IllegalArgumentException("dob cannot be in future");
     }
-    
-    public  String getName(){ return name; }
-    public  java.util.Date getDob(){ return dob; }
+
+    protected boolean validateDob(java.util.Date dob){
+        return !dob.after(new java.util.Date());
+    }
+
+    //getter methods for fields not shown
 }
