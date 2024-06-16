20cedaed53fbb876caecf52da8385b7f122b54f2
michel1991 <michelmvoumamvouma@gmail.com>
2024-06-16 13:39:03 +0200

    code: reference method

---
+++ lambda/lambdaAndReference/MyProcessorTestClass.java
@@ -0,0 +1,73 @@
+package lambda.lambdaAndReference;
+import java.util.function.*;
+import java.util.function.*;
+import java.util.*;
+
+/**
+ Given:
+    class MyProcessor{
+        public void process(){
+            System.out.println("Processing ");
+        }
+    }
+    public class TestClass {
+        
+        public static void main(String[] args) {
+            process(()->new MyProcessor()); //1 REPLACE THIS LINE OF CODE
+        }
+    
+        public static void process(Supplier<MyProcessor> s){
+            s.get().process();
+        }
+    }
+    
+    Which of the following options correctly replaces the line marked //1 with code that uses method reference?
+    
+    A. TestClass.process(MyProcessor::new);
+        This is the correct syntax for referring to a constructor.
+    B. TestClass.process(new::MyProcessor);
+    C. TestClass.process(MyProcessor::new());
+    D. TestClass.process(new::MyProcessor());
+    E. TestClass.process(MyProcessor()::new);
+    
+    ---------------------
+    Observe that the TestClass's static method process takes Supplier<MyProcessor> as an argument.
+    The lambda expression //()->new MyProcessor()// creates a Supplier with the implementation
+    of its functional method named get that just returns a new MyProcessor object. If it makes it easier to understand,
+    you can think of this lambda expression in terms of an anonymous class like this:
+        new Supplier(){
+           public MyProcessor get(){
+               return new MyProcessor(); //the body of a lambda expression becomes
+        //the body of the functional method of the functional interface.
+
+           }
+        }
+
+        The expression new MyProcessor() in the lambda expression can be replaced by MyProcessor::new.
+    Correct(A)
+    
+
+ */
+
+class MyProcessor{
+    public void process(){
+        System.out.println("Processing ");
+    }
+}
+ class TestClass {
+    
+    public static void main(String[] args) {
+        // process(()->new MyProcessor()); //1 REPLACE THIS LINE OF CODE
+        TestClass.process(MyProcessor::new);
+    }
+
+    public static void process(Supplier<MyProcessor> s){
+        s.get().process();
+    }
+}
+
+public class MyProcessorTestClass {
+    public static void main(String[] args) {
+        
+    }
+}
