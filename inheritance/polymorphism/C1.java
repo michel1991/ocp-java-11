34bb100543b6ae809118151d104a2761b920db54
michel1991 <michelmvoumamvouma@gmail.com>
2024-07-09 05:51:53 +0200

    code: polymorphism

---
+++ inheritance/polymorphism/C1.java
@@ -0,0 +1,43 @@
+package inheritance.tricky.polymorphism;
+/**
+   Given that classes B1 and B2 extend class A and classes C1, C2 are subclasses of B1.
+   Further, assume that method public void m1(){ ... } is defined in all of these classes EXCEPT B1 and C1.
+   
+   Assume that "objectOfXX" means a variable that points to an object of class XX.
+   So, objectOfC1 means a reference variable that is pointing to an object of class C1.
+
+    Which of the following statements are correct?
+    
+    A. objectOfC1.m1(); will cause a compilation error.
+        C1 will inherit B1's m1() which in turn inherits m1() from A.
+        
+    B. objectOfC2.m1(); will cause A's m1() to be called.
+         C2 has m1(), so its m1() will override A's m1().
+         
+    C. objectOfC1.m1(); will cause A's m1() to be called.
+        C1 will inherit B1's m1() which in turn inherits m1() from A.
+        
+    D. objectOfB1.m1(); will cause an exception at runtime.
+         B1 will inherit m1() from A. So this is valid.
+         
+    E. objectOfB2.m1(); will cause an exception at runtime.
+        B2 overrides m1() of A. So there will be no exception.
+        
+    Correct(C)
+
+ */
+class A{
+    public void m1(){ }
+  }
+  
+  class B1 extends A{
+  }
+  class B2 extends A{
+    public void m1(){ }
+  }
+  
+  class C1 extends B1{
+  }
+  class C2 extends B1{
+    public void m1(){ }
+  }
