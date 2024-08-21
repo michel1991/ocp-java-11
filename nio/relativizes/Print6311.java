e3824a7da27da3f349dcd141dc5686b5a0d70d19
michel1991 <michelmvoumamvouma@gmail.com>
2024-08-21 06:16:09 +0200

    fix: add correct path for linux

--- nio/relativizes/Print6311.java
+++ nio/relativizes/Print6311.java
@@ -68,11 +68,11 @@ Traduit avec DeepL.com (version gratuite)
  */
 public class Print6311 {
     public static void main(String... arg){
-        Path p1 = Paths.get("photos\\..\\beaches\\.\\calangute\\a.txt");
+        //Path p1 = Paths.get("photos\\..\\beaches\\.\\calangute\\a.txt");
+        Path p1 = Paths.get("photos/../beaches/./calangute/a.txt"); // correct are  Path p1 = Paths.get("photos\\..\\beaches\\.\\calangute\\a.txt");
         Path p2 = p1.normalize();
         Path p3 = p1.relativize(p2);
         Path p4 = p2.relativize(p1);
-
         System.out.println(
                 p1.getNameCount()+" "+p2.getNameCount()+" "+
                 p3.getNameCount()+" "+p4.getNameCount());
